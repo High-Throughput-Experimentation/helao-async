52e5725ce747855e8cf12d7dc0aef50b1eb2b4c1
Matthias Richter <matthias.h.richter@gmail.com>
2021-08-03 15:56:15 -0700

    Update nidaqmx_server.py

--- helao/library/server/action/nidaqmx_server.py
+++ helao/library/server/action/nidaqmx_server.py
@@ -14,163 +14,166 @@
 
 from importlib import import_module
 
-from helao.core.server import Action, makeActServ
-from helao.library.driver.nidaqmx_driver import cNIMAX
+from fastapi import Request
+from typing import Optional, List, Union
+
+
+from helao.core.server import Action, makeActServ, setupAct
+from helao.library.driver.nidaqmx_driver import cNIMAX, pumpitems
 
 
 def makeApp(confPrefix, servKey):
 
     config = import_module(f"helao.config.{confPrefix}").config
-    C = config["servers"]
-    S = C[servKey]
 
     app = makeActServ(
-        config, servKey, servKey, "NIdaqmx server", version=2.0, driver_class=cNIMAX
+        config,
+        servKey,
+        servKey,
+        "NIdaqmx server",
+        version=2.0,
+        driver_class=cNIMAX,
     )
 
+
     @app.post(f"/{servKey}/run_task_GasFlowValves")
-    async def run_task_GasFlowValves(valves: str, on: bool = True, action_params=""):
+    async def run_task_GasFlowValves(
+                                    request: Request, 
+                                    valves: Optional[List[int]] = [],
+                                    on: Optional[bool] = True,
+                                    action_dict: dict = {}
+                                    ):
         """Provide list of Valves (number) separated by ,"""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_task_GasFlowValves",
-                "parameters": {"valves": valves, "ON": on},
-            },
-            data=await app.driver.run_task_GasFlowValves(valves, on),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"GasFlowValves": await app.driver.run_task_GasFlowValves(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_Master_Cell_Select")
     async def run_task_Master_Cell_Select(
-        cells: str, on: bool = True, action_params=""
-    ):
+                                         request: Request, 
+                                         cells: Optional[List[int]] = [], 
+                                         on: Optional[bool] = True,
+                                         action_dict: dict = {}
+                                         ):
         """Provide list of Cells separated by ,"""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_task_Master_Cell_Select",
-                "parameters": {"cells": cells, "ON": on},
-            },
-            data=await app.driver.run_task_Master_Cell_Select(cells, on),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"Master_Cell": await app.driver.run_task_Master_Cell_Select(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_Active_Cells_Selection")
     async def run_task_Active_Cells_Selection(
-        cells: str, on: bool = True, action_params=""
-    ):
+                                             request: Request,
+                                             cells: Optional[List[int]] = [], 
+                                             on: Optional[bool] = True,
+                                             action_dict: dict = {}
+                                             ):
         """Provide list of Cells (number) separated by ,"""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_task_Active_Cells_Selection",
-                "parameters": {"cells": cells, "ON": on},
-            },
-            data=await app.driver.run_task_Active_Cells_Selection(cells, on),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"Active_Cells": await app.driver.run_task_Active_Cells_Selection(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_Pumps")
     async def run_task_Pumps(
-        pumps: pumpitems = "PeriPump", on: bool = True, action_params=""
-    ):
-        """Provide list of Pumps separated by ,"""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_Pumps",
-                "parameters": {"pumps": pumps, "ON": on},
-            },
-            data=await app.driver.run_task_Pumps(pumps, on),
-        )
-        return retc
+                            request: Request, 
+                            pumps: Optional[pumpitems] = "PeriPump",
+                            on: Optional[bool] = True,
+                            action_dict: dict = {}
+                            ):
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"pumps": await app.driver.run_task_Pumps(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_FSWBCD")
-    async def run_task_FSWBCD(BCDs: str, on: bool = True, action_params=""):
-        """Provide list of Pumps separated by ,"""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_FSWBCD",
-                "parameters": {"BCD": BCDs, "ON": on},
-            },
-            data=await app.driver.run_task_FSWBCD(BCDs, on),
-        )
-        return retc
+    async def run_task_FSWBCD(
+                             request: Request,
+                             BCDs: Optional[str] = "",
+                             on: Optional[bool] = True,
+                             action_dict: dict = {}
+                             ):
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"FSWBCD": await app.driver.run_task_FSWBCD(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_FSW_error")
-    async def run_task_FSW_error(action_params=""):
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={"command": "run_task_FSW_error", "parameters": {},},
-            data=await app.driver.run_task_getFSW("Error"),
-        )
-        return retc
+    async def run_task_FSW_error(
+                                request: Request, 
+                                action_dict: dict = {}
+                                ):
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"FSW_error": await app.driver.run_task_getFSW("Error")})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/run_task_FSW_done")
-    async def run_task_FSW_done(action_params=""):
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={"command": "run_task_FSW_done", "parameters": {},},
-            data=await app.driver.run_task_getFSW("Done"),
-        )
-        return retc
+    async def run_task_FSW_done(
+                                request: Request, 
+                                action_dict: dict = {}
+                               ):
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"FSW_done": await app.driver.run_task_getFSW("Done")()})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
 
-    # @app.post(f"/{servKey}/run_task_RSH_TTL_handshake")
-    # async def run_task_RSH_TTL_handshake(action_params = ''):
-    #     await stat.set_run()
-    #     retc = return_class(
-    #         measurement_type="NImax_command",
-    #         parameters={
-    #                     "command": "run_RSH_TTL_handshake",
-    #                     "parameters": {
-    #                         },
-    #                     },
-    #         data=await app.driver.run_task_RSH_TTL_handshake(),
-    #     )
-    #     await stat.set_idle()
-    #     return retc
 
     @app.post(f"/{servKey}/run_task_Cell_IV")
-    async def run_task_Cell_IV(on: bool = True, tstep: float = 1.0, action_params=""):
+    async def run_task_Cell_IV(
+                              request: Request, 
+                              on: Optional[bool] = True, 
+                              tstep: Optional[float] = 1.0,
+                              action_dict: dict = {},
+                              ):
         """Get the current/voltage measurement for each cell.
         Only active cells are plotted in visualizer."""
-        retc = return_class(
-            measurement_type="NImax_command",
-            parameters={
-                "command": "run_task_Cell_IV",
-                "parameters": {"tstep": tstep, "ON": on},
-            },
-            data=await app.driver.run_task_Cell_IV(on, tstep),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        A.save_data = True
+        active_dict = await app.driver.run_task_Cell_IV(A)
+        return active_dict
+
 
     @app.post(f"/{servKey}/stop")
-    async def stop(action_params=""):
+    async def stop(
+                  request: Request, 
+                  action_dict: dict = {}
+                  ):
         """Stops measurement in a controlled way."""
-        retc = return_class(
-            measurement_type="gamry_command",
-            parameters={"command": "stop"},
-            data=await app.driver.stop(),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"stop_result": await app.driver.stop()})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
+
 
     @app.post(f"/{servKey}/estop")
-    async def estop(switch: bool = True, action_params=""):
+    async def estop(
+                   request: Request, 
+                   switch: Optional[bool] = True,
+                   action_dict: dict = {}
+                   ):
         """Same as stop, but also sets estop flag."""
-        retc = return_class(
-            measurement_type="gamry_command",
-            parameters={"command": "estop"},
-            data=await app.driver.estop(switch),
-        )
-        return retc
+        A = await setupAct(action_dict, request, locals())
+        active = await app.base.contain_action(A)
+        await active.enqueue_data({"estop_result": await app.driver.estop(**A.action_params)})
+        finished_act = await active.finish()
+        return finished_act.as_dict()
 
-    @app.websocket(f"/{servKey}/ws_data_settings")
-    async def websocket_data_setings(websocket: WebSocket):
-        await wsdatasettings.send(
-            websocket, app.driver.qsettings, "NImax_data_settings"
-        )
 
     @app.post("/shutdown")
     def post_shutdown():
