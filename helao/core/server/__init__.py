079e91ece48321ae4952b8f110f6958efa678eb4
onepunchdan <onepunchdan@users.noreply.github.com>
2021-10-06 13:11:43 -0700

    importable make_process_serv()

--- helao/core/server/__init__.py
+++ helao/core/server/__init__.py
@@ -4,6 +4,7 @@ from .base import Base
 from .dispatcher import async_private_dispatcher, async_process_dispatcher
 from .import_sequences import import_sequences
 from .make_orch_serv import makeOrchServ
+from .make_process_serv import make_process_serv
 from .make_vis_serv import makeVisServ
 from .orch import Orch
 from .process_start_condition import process_start_condition
