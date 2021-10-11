c725fcc94efb46d9478f932b9c39dcc1c100e04e
onepunchdan <onepunchdan@users.noreply.github.com>
2021-10-11 10:19:12 -0700

    breakout helao.core into separate repo

--- helao/core/data/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-from .legacy import HTELegacyAPI
-from .sample import LiquidSampleAPI
-from .sample import GasSampleAPI
-from .sample import AssemblySampleAPI
-from .sample import UnifiedSampleDataAPI
-from .sample import OldLiquidSampleAPI
--- helao/core/server/version.py
+++
@@ -1,5 +0,0 @@
-
-__all__ = ["hlo_version"]
-
-# version number, gets written into every prc/prg and hlo file
-hlo_version = "2021.09.20"
