3f2c77a579f08c4311268304aad786d698862723
onepunchdan <onepunchdan@users.noreply.github.com>
2022-05-02 15:49:51 -0700

    Rename dll to spec, add device num to shutdown

--- helao/driver/spec/spectral_products.py
+++ helao/driver/spec/spectral_products.py
@@ -4,6 +4,7 @@ import os
 import ctypes
 
 from helaocore.server.base import Base
+from helao.driver.io.enum import TriggerType
 
 
 class SM303:
@@ -22,25 +23,25 @@ class SM303:
         assert len(self.wl_cal) == len(self.px_cal)
         self.n_cal = len(self.wl_cal)
         if os.path.exists(self.lib_path):
-            self.dll = ctypes.CDLL(self.lib_path)
+            self.spec = ctypes.CDLL(self.lib_path)
             self.setup_sm303()
         else:
             self.base.print_message("SMdbUSBm.dll not found.", error=True)
-            self.dll = None
+            self.spec = None
 
     def setup_sm303(self):
-        self.model = ctypes.c_short(self.dll.spGetModel())
-        self.dll.spTestAllChannels()
-        self.dll.spSetupGivenChannel(self.dev_num)
-        self.dll.spInitGivenChannel(self.model, self.dev_num)
-        self.dll.spSetTEC(ctypes.c_long(1), self.dev_num)
+        self.model = ctypes.c_short(self.spec.spGetModel())
+        self.spec.spTestAllChannels()
+        self.spec.spSetupGivenChannel(self.dev_num)
+        self.spec.spInitGivenChannel(self.model, self.dev_num)
+        self.spec.spSetTEC(ctypes.c_long(1), self.dev_num)
         self.c_wl_cal = (ctypes.c_double * self.n_cal)()
         self.c_px_cal = (ctypes.c_double * self.n_cal)()
         self.c_fitcoeffs = (ctypes.c_double * self.n_cal)()
         for i, (wl, px) in enumerate(zip(self.wl_cal, self.px_cal)):
             self.c_wl_cal[i] = wl / 10
             self.c_px_cal[i] = px
-        self.dll.spPolyFit(
+        self.spec.spPolyFit(
             ctypes.byref(self.c_px_cal),
             ctypes.byref(self.c_wl_cal),
             ctypes.c_short(self.n_cal),
@@ -49,7 +50,7 @@ class SM303:
         )
         self.c_wl = (ctypes.c_double * self.n_pixels)()
         for i in range(self.n_pixels):
-            self.dll.spPolyCalc(
+            self.spec.spPolyCalc(
                 ctypes.byref(self.c_fitcoeffs),
                 ctypes.c_short(3),  # polynomial order
                 ctypes.c_double(i + 1),
@@ -60,14 +61,14 @@ class SM303:
             f"Calibrated wavelength range: {min(self.pxwl)}, {max(self.pxwl)} over {self.n_pixels} detector pixels."
         )
         self.wl_saved = (ctypes.c_double * 1024)()
-        self.dll.spGetWLTable(ctypes.byref(self.wl_saved), self.dev_num)
+        self.spec.spGetWLTable(ctypes.byref(self.wl_saved), self.dev_num)
 
     def measure_spec(self, int_time: float):
         # minimum int_time for SM303 is 7.0 msec
         self.int_time = ctypes.c_double(int_time)
-        self.dll.spSetTrgEx(10, self.dev_num)
-        self.dll.spSetDblIntEx(self.int_time, self.dev_num)
-        self.dll.spReadDataEx(ctypes.byref(self.data), self.dev_num)
+        self.spec.spSetTrgEx(10, self.dev_num)
+        self.spec.spSetDblIntEx(self.int_time, self.dev_num)
+        self.spec.spReadDataEx(ctypes.byref(self.data), self.dev_num)
         data = [self.data[i] for i in range(1056)]
         data = data[10:1034]
         return data
@@ -75,9 +76,9 @@ class SM303:
     def measure_spec_adv(self, int_time: float, n_avg: int = 1, fft: int = 0):
         # minimum int_time for SM303 is 7.0 msec
         self.int_time = ctypes.c_double(int_time)
-        self.dll.spSetTrgEx(10, self.dev_num)
-        self.dll.spSetDblIntEx(self.int_time, self.dev_num)
-        self.dll.spReadDataAdvEx(
+        self.spec.spSetTrgEx(10, self.dev_num)
+        self.spec.spSetDblIntEx(self.int_time, self.dev_num)
+        self.spec.spReadDataAdvEx(
             ctypes.byref(self.data),
             ctypes.c_short(n_avg),
             ctypes.c_short(fft),
@@ -89,6 +90,34 @@ class SM303:
         data = data[10:1034]
         return data
 
+    def set_external_trigger(self, edge_mode: TriggerType = TriggerType.fallingedge):
+        """Set external trigger mode.
+
+        Notes:
+            SM303 has max 'waiting time' of 7ms, ADC time of 4ms, min USB tx
+            time of 2ms in addition to the min integration time of 7ms. 
+
+            Trigger signal time must be at least 13ms.
+            Galil IO appears to have 1ms toggle resolutionself.
+
+            TODO: setup external trigger mode and integration time,
+            SPEC server should switch over to usb context and listen for data,
+            Galil IO or PSTAT should send SPEC server a finish signal
+
+        """
+        if edge_mode == TriggerType.fallingedge:
+            trigger = ctypes.c_short(0)
+        elif edge_mode == TriggerType.risingedge:
+            trigger = ctypes.c_short(1)
+        self.spec.spSetExtEdgeMode(trigger, self.dev_num)
+        self.spec.spSetTrgEx(ctypes.c_short(12), self.dev_num)  # 12=SP_TRIGGER_EXTERNAL
+
+    def unset_external_trigger(self):
+        self.spec.spSetTrgEx(ctypes.c_short(10), self.dev_num)  # 10=SP_TRIGGER_OFF
+
     def shutdown(self):
-        self.dll.spCloseGivenChannel(self.dev_num)
-        self.dll.spSetTEC(ctypes.c_long(0), self.dev_num)
+        self.base.print_message("shutting down SM303")
+        self.unset_external_trigger()
+        self.spec.spSetTEC(ctypes.c_long(0), self.dev_num)
+        self.spec.spCloseGivenChannel(self.dev_num)
+        return {"shutdown"}
