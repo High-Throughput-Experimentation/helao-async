de5b8f62075ecef1dc2c9647dbbf8544236a1ce4
kkanatcit <103452480+kkanatcit@users.noreply.github.com>
2022-05-05 15:57:09 -0700

    added move routines

--- helao/sequence/SDC_seq.py
+++ helao/sequence/SDC_seq.py
@@ -12,6 +12,8 @@ __all__ = [
     "SDC_background",
     "SDC_CP",
     "SDC_CP_led",
+    "SDC_movetosample",
+    "SDC_move",
 
 ]
 
@@ -780,3 +782,46 @@ def SDC_CP_led(
 
     return pl.experiment_plan_list  # returns complete experiment list
 
+def SDC_movetosample(
+    sequence_version: int = 1,
+    plate_id: int = 1,
+    plate_sample_no: int = 1,
+
+):
+
+    pl = ExperimentPlanMaker()
+
+
+    pl.add_experiment(
+        "SDC_slave_movetosample",
+        {
+#            "solid_custom_position": "cell1_we",
+            "solid_plate_id": plate_id,
+            "solid_sample_no": plate_sample_no,
+        },
+    )
+
+    pl.add_experiment("SDC_slave_shutdown", {})
+
+    return pl.experiment_plan_list  # returns complete experiment list
+
+def SDC_move(
+    sequence_version: int = 1,
+    move_x_mm: float = 1.0,
+    move_y_mm: float = 1.0,
+):
+
+    pl = ExperimentPlanMaker()
+
+
+    pl.add_experiment(
+        "SDC_slave_move",
+        {
+            "x_mm": move_x_mm,
+            "y_mm": move_y_mm,
+        },
+    )
+
+    pl.add_experiment("SDC_slave_shutdown", {})
+
+    return pl.experiment_plan_list  # returns complete experiment list
