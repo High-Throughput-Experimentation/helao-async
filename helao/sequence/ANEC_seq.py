cb7e0916b8e4d466abf22032ee83edd5518cf145
yul69-cell <64283970+yul69-cell@users.noreply.github.com>
2022-05-16 13:57:44 -0700

    update ANEC_photo_CA seq

--- helao/sequence/ANEC_seq.py
+++ helao/sequence/ANEC_seq.py
@@ -1,6 +1,6 @@
 """Sequence library for ANEC"""
 
-__all__ = ["ANEC_sample_ready", "ANEC_series_CA", "ANEC_cleanup_disengage","ANEC_repeat_CA", "ANEC_repeat_CV", "ANEC_CA_pretreat", "ANEC_photo_CA", "ANEC_gasonly_CA", "GC_Archiveliquid_analysis", "HPLC_Archiveliquid_analysis"]
+__all__ = ["ANEC_sample_ready", "ANEC_series_CA", "ANEC_photo_CA", "ANEC_cleanup_disengage","ANEC_repeat_CA", "ANEC_repeat_CV", "ANEC_CA_pretreat", "ANEC_gasonly_CA", "GC_Archiveliquid_analysis", "HPLC_Archiveliquid_analysis"]
 
 from typing import List
 from typing import Optional
@@ -201,7 +201,108 @@ def ANEC_series_CA(
     
     return epm.experiment_plan_list
 
+def ANEC_photo_CA(
+    sequence_version: int = 2,
+    num_repeats: int = 1,
+    solid_plate_id: int = 4534,
+    solid_sample_no: int = 1,
+    reservoir_liquid_sample_no: int = 1,
+    volume_ul_cell_liquid: float = 1000,
+    WE_potential__V: List[float] = [-0.9, -1.0, -1.1, -1.2, -1.3],
+    WE_versus: str = "ref",
+    ref_type: str = "leakless",
+    pH: float = 6.8,
+    CA_duration_sec: List[float] = [900, 900, 900, 900, 900],
+    SampleRate: float = 0.01,
+    IErange: str = "auto",
+    ref_offset__V: float = 0.0,
+    LED_source_nm: int = 450,
+    toolGC: str = "HS 2",
+    toolarchive: str = "LS 3",
+    volume_ul_GC: int = 300,
+    volume_ul_archive: int = 500,
+    wash1: bool = True,
+    wash2: bool = True,
+    wash3: bool = True,
+    wash4: bool = False,
+):
+    """Repeat CA and aliquot sampling at the cell1_we position.
 
+    Flush and fill cell, run CA, and drain.
+
+    (1) Fill cell with liquid for 90 seconds
+    (2) Equilibrate for 15 seconds
+    (3) run CA
+    (4) mix product
+    (5) Drain cell and purge with CO2 for 60 seconds
+
+    Args:
+        exp (Experiment): Active experiment object supplied by Orchestrator
+        toolGC (str): PAL tool string enumeration (see pal_driver.PALTools)
+        volume_ul_GC: GC injection volume
+
+
+
+    """
+
+    epm = ExperimentPlanMaker()
+
+    # housekeeping
+    epm.add_experiment("ANEC_slave_unload_cell", {})
+
+    #epm.add_experiment("ANEC_slave_normal_state", {})
+
+    epm.add_experiment(
+        "ANEC_slave_load_solid",
+        {"solid_plate_id": solid_plate_id, "solid_sample_no": solid_sample_no},
+    )
+
+    for cycle, (potential, time) in enumerate(zip(WE_potential__V, CA_duration_sec)):
+        print(f" ... cycle {cycle} potential:", potential, f" ... cycle {cycle} duration:", time)
+
+        epm.add_experiment(
+            "ANEC_slave_flush_fill_cell",
+            {
+                "liquid_flush_time": 90,
+                "co2_purge_time": 15,
+                "equilibration_time": 1.0,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
+                "volume_ul_cell_liquid": volume_ul_cell_liquid,
+            },
+        )
+
+        epm.add_experiment(
+            "ANEC_slave_photo_CA",
+            {
+                "WE_potential__V": potential,
+                "WE_versus": WE_versus,
+                "ref_type": ref_type,
+                "pH": pH,
+                "ref_offset__V": ref_offset__V,
+                "CA_duration_sec": time,
+                "SampleRate": SampleRate,
+                "IErange": IErange,
+            },
+        )
+
+        epm.add_experiment(
+            "ANEC_slave_aliquot",
+            {
+                "toolGC": toolGC,
+                "toolarchive": toolarchive,
+                "volume_ul_GC": volume_ul_GC,
+                "volume_ul_archive": volume_ul_archive,
+                "wash1": wash1,
+                "wash2": wash2,
+                "wash3": wash3,
+                "wash4": wash4,
+            },
+        )
+
+        epm.add_experiment("ANEC_slave_drain_cell", {"drain_time": 50.0})
+    
+    epm.add_experiment("ANEC_slave_alloff", {})
+    return epm.experiment_plan_list
 
 def ANEC_cleanup_disengage(
     sequence_version: int = 1
@@ -487,105 +588,7 @@ def ANEC_gasonly_CA(
 
     return epm.experiment_plan_list
 
-def ANEC_photo_CA(
-    sequence_version: int = 1,
-    num_repeats: int = 1,
-    solid_plate_id: int = 4534,
-    solid_sample_no: int = 1,
-    reservoir_liquid_sample_no: int = 1,
-    volume_ul_cell_liquid: float = 1000,
-    WE_potential__V: float = 0.0,
-    WE_versus: str = "ref",
-    ref_type: str = "leakless",
-    pH: float = 6.8,
-    CA_duration_sec: float = 0.1,
-    SampleRate: float = 0.01,
-    IErange: str = "auto",
-    ref_offset__V: float = 0.0,
-    toolGC: str = "HS 2",
-    toolarchive: str = "LS 3",
-    volume_ul_GC: int = 300,
-    volume_ul_archive: int = 500,
-    wash1: bool = True,
-    wash2: bool = True,
-    wash3: bool = True,
-    wash4: bool = False,
-):
-    """Repeat CA and aliquot sampling at the cell1_we position.
 
-    Flush and fill cell, run CA, and drain.
-
-    (1) Fill cell with liquid for 90 seconds
-    (2) Equilibrate for 15 seconds
-    (3) run CA
-    (4) mix product
-    (5) Drain cell and purge with CO2 for 60 seconds
-
-    Args:
-        exp (Experiment): Active experiment object supplied by Orchestrator
-        toolGC (str): PAL tool string enumeration (see pal_driver.PALTools)
-        volume_ul_GC: GC injection volume
-
-
-
-    """
-
-    epm = ExperimentPlanMaker()
-
-    # housekeeping
-    epm.add_experiment("ANEC_slave_unload_cell", {})
-
-    #epm.add_experiment("ANEC_slave_normal_state", {})
-
-    epm.add_experiment(
-        "ANEC_slave_load_solid",
-        {"solid_plate_id": solid_plate_id, "solid_sample_no": solid_sample_no},
-    )
-
-    for _ in range(num_repeats):
-
-        epm.add_experiment(
-            "ANEC_slave_flush_fill_cell",
-            {
-                "liquid_flush_time": 80,
-                "co2_purge_time": 15,
-                "equilibration_time": 1.0,
-                "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
-                "volume_ul_cell_liquid": volume_ul_cell_liquid,
-            },
-        )
-
-        epm.add_experiment(
-            "ANEC_slave_photo_CA",
-            {
-                "WE_potential__V": WE_potential__V,
-                "WE_versus": WE_versus,
-                "ref_type": ref_type,
-                "pH": pH,
-                "ref_offset__V": ref_offset__V,
-                "CA_duration_sec": CA_duration_sec,
-                "SampleRate": SampleRate,
-                "IErange": IErange,
-            },
-        )
-
-        epm.add_experiment(
-            "ANEC_slave_aliquot",
-            {
-                "toolGC": toolGC,
-                "toolarchive": toolarchive,
-                "volume_ul_GC": volume_ul_GC,
-                "volume_ul_archive": volume_ul_archive,
-                "wash1": wash1,
-                "wash2": wash2,
-                "wash3": wash3,
-                "wash4": wash4,
-            },
-        )
-
-        epm.add_experiment("ANEC_slave_drain_cell", {"drain_time": 50.0})
-
-    return epm.experiment_plan_list
 
 def ANEC_repeat_CV(
     sequence_version: int = 1,
