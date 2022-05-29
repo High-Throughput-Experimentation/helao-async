95bb65397ccfb8b17d6c0afdc4addbd2e3f5b82a
kkanatcit <103452480+kkanatcit@users.noreply.github.com>
2022-05-29 13:28:35 -0700

    added CA CV CP spectrometer sequences

--- helao/sequence/ECHE_seq.py
+++ helao/sequence/ECHE_seq.py
@@ -12,7 +12,9 @@ __all__ = [
     "ECHE_background",
     "ECHE_CP",
     "ECHE_CP_led",
-#    "ECHE_CV_led_secondtrigger",
+    "ECHE_CV_led_spectrometer",
+    "ECHE_CA_led_spectrometer",
+    "ECHE_CP_led_spectrometer",
     "ECHE_movetosample",
     "ECHE_move",
 
@@ -854,98 +856,263 @@ def ECHE_move(
 
     return pl.experiment_plan_list  # returns complete experiment list
 
-# def ECHE_CV_led_secondtrigger(
-#     sequence_version: int = 1,
-#     plate_id: int = 1,
-#     plate_sample_no_list: list = [2],
-#     reservoir_electrolyte: Electrolyte = "SLF10",
-#     reservoir_liquid_sample_no: int = 1,
-#     reservoir_bubbler_gas: str = "O2",
-#     ph: float = 9.53,
-#     droplet_size_cm2: float = .071,  #3mm diameter droplet
-#     liquid_volume_ml: float = 1.0,
-#     ref_vs_nhe: float = 0.21,
-#     CV_Vinit_vsRHE: float = 1.23,
-#     CV_Vapex1_vsRHE: float = 0.73,
-#     CV_Vapex2_vsRHE: float = 1.73,
-#     CV_Vfinal_vsRHE: float = 1.73,
-#     CV_scanrate_voltsec: float = 0.02,
-#     CV_samplerate_mV: float = 1,
-#     CV_cycles: int = 1,
-#     IErange: str = "auto",
-#     led_typenamesdate: list = ["led"],
-#     led_wavelengths_nm: list = [0],
-#     led_intensities_mw: list = [0],
-#     led_number_CV: int = 1,
-#     toggle_onCV_ms: float = 2000,
-#     toggle_offCV_ms: float = 1000,
-#     toggle_offsetCV_ms: float = 0,
-#     toggle_duration_ms: float = -1,
-#     toggle_two_onCV_ms: float = 100,
-#     toggle_two_offCV_ms: float = 500,
-#     toggle_two_offsetCV_ms: float = 0,
-#     toggle_two_duration_ms: float = -1,
+def ECHE_CV_led_spectrometer(
+    sequence_version: int = 1,
+    plate_id: int = 1,
+    plate_sample_no_list: list = [2],
+    reservoir_electrolyte: Electrolyte = "SLF10",
+    reservoir_liquid_sample_no: int = 1,
+    reservoir_bubbler_gas: str = "O2",
+    ph: float = 9.53,
+    droplet_size_cm2: float = .071,  #3mm diameter droplet
+    liquid_volume_ml: float = 1.0,
+    ref_vs_nhe: float = 0.21,
+    CV_Vinit_vsRHE: float = 1.23,
+    CV_Vapex1_vsRHE: float = 0.73,
+    CV_Vapex2_vsRHE: float = 1.73,
+    CV_Vfinal_vsRHE: float = 1.73,
+    CV_scanrate_voltsec: float = 0.02,
+    CV_samplerate_mV: float = 1,
+    CV_cycles: int = 1,
+    IErange: str = "auto",
+    led_typenamesdate: list = ["led"],
+    led_wavelengths_nm: list = [0],
+    led_intensities_mw: list = [0],
+    led_number_CV: int = 1,
+    toggle_onCV_ms: float = 2000,
+    toggle_offCV_ms: float = 1000,
+    toggle_offsetCV_ms: float = 0,
+    toggle_duration_ms: float = -1,
+    toggle_two_on_ms: float = 100,
+    toggle_two_off_ms: float = 500,
+    toggle_two_offset_ms: float = 0,
+    toggle_two_duration_ms: float = -1,
 
     
-# ):
+):
 
-#     pl = ExperimentPlanMaker()
+    pl = ExperimentPlanMaker()
 
-#     # (1) house keeping
-#     pl.add_experiment("ECHE_slave_unloadall_customs", {})
+    # (1) house keeping
+    pl.add_experiment("ECHE_slave_unloadall_customs", {})
 
-#     for plate_sample in plate_sample_no_list:
+    for plate_sample in plate_sample_no_list:
 
-#         pl.add_experiment(
-#             "ECHE_slave_startup",
-#             {
-#                 "solid_custom_position": "cell1_we",
-#                 "solid_plate_id": plate_id,
-#                 "solid_sample_no": plate_sample,
-#                 "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
-#                 "reservoir_bubbler_gas":     reservoir_bubbler_gas,
-#                 "liquid_volume_ml": liquid_volume_ml,
-#             },
-#         )
+        pl.add_experiment(
+            "ECHE_slave_startup",
+            {
+                "solid_custom_position": "cell1_we",
+                "solid_plate_id": plate_id,
+                "solid_sample_no": plate_sample,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
+                "reservoir_bubbler_gas":     reservoir_bubbler_gas,
+                "liquid_volume_ml": liquid_volume_ml,
+            },
+        )
         
-#         # CV1
-#         pl.add_experiment(
-#             "ECHE_slave_CV_led_secondtrigger",
-#             {
-#                 "Vinit_vsRHE": CV_Vinit_vsRHE,
-#                 "Vapex1_vsRHE": CV_Vapex1_vsRHE,
-#                 "Vapex2_vsRHE": CV_Vapex2_vsRHE,
-#                 "Vfinal_vsRHE": CV_Vfinal_vsRHE,
-#                 "scanrate_voltsec": CV_scanrate_voltsec,
-#                 "samplerate_sec": CV_samplerate_mV / (CV_scanrate_voltsec * 1000),
-#                 "cycles": CV_cycles,
-#                 "IErange": IErange,
-#                 "ph": ph,
-#                 "reservoir_liquid_sample_no": reservoir_liquid_sample_no, #currently liquid sample database number
-#                 "reservoir_bubbler_gas": reservoir_bubbler_gas,
-#                 "droplet_size_cm2": droplet_size_cm2,
-#                 "reference_electrode_type": "NHE",
-#                 "ref_vs_nhe": ref_vs_nhe,
-#                 "led": led_typenamesdate[led_number_CV],
-#                 "wavelength_nm": led_wavelengths_nm[led_number_CV],
-#                 "wavelength_intensity_mw": led_intensities_mw[led_number_CV],
-#                 "wavelength_intensity_date": led_typenamesdate[5],
-#                 "led_side_illumination": led_typenamesdate[0],
-#                 "toggle_on_ms": toggle_onCV_ms,
-#                 "toggle_off_ms": toggle_offCV_ms,
-#                 "toggle_offset_ms": toggle_offsetCV_ms,
-#                 "toggle_duration_ms": toggle_duration_ms,
-#                 "toggle_two_on_ms": toggle_two_onCV_ms,
-#                 "toggle_two_off_ms": toggle_two_offCV_ms,
-#                 "toggle_two_offset_ms": toggle_two_offsetCV_ms,
-#                 "toggle_two_duration_ms": toggle_two_duration_ms,
+        # CV1
+        pl.add_experiment(
+            "ECHE_slave_CV_led_secondtrigger",
+            {
+                "Vinit_vsRHE": CV_Vinit_vsRHE,
+                "Vapex1_vsRHE": CV_Vapex1_vsRHE,
+                "Vapex2_vsRHE": CV_Vapex2_vsRHE,
+                "Vfinal_vsRHE": CV_Vfinal_vsRHE,
+                "scanrate_voltsec": CV_scanrate_voltsec,
+                "samplerate_sec": CV_samplerate_mV / (CV_scanrate_voltsec * 1000),
+                "cycles": CV_cycles,
+                "IErange": IErange,
+                "ph": ph,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no, #currently liquid sample database number
+                "reservoir_bubbler_gas": reservoir_bubbler_gas,
+                "droplet_size_cm2": droplet_size_cm2,
+                "reference_electrode_type": "NHE",
+                "ref_vs_nhe": ref_vs_nhe,
+                "led": led_typenamesdate[led_number_CV],
+                "wavelength_nm": led_wavelengths_nm[led_number_CV],
+                "wavelength_intensity_mw": led_intensities_mw[led_number_CV],
+                "wavelength_intensity_date": led_typenamesdate[5],
+                "led_side_illumination": led_typenamesdate[0],
+                "toggle_on_ms": toggle_onCV_ms,
+                "toggle_off_ms": toggle_offCV_ms,
+                "toggle_offset_ms": toggle_offsetCV_ms,
+                "toggle_duration_ms": toggle_duration_ms,
+                "toggle_two_on_ms": toggle_two_on_ms,
+                "toggle_two_off_ms": toggle_two_off_ms,
+                "toggle_two_offset_ms": toggle_two_offset_ms,
+                "toggle_two_duration_ms": toggle_two_duration_ms,
 
 
 
-#             },
-#         )
+            },
+        )
 
-#         pl.add_experiment("ECHE_slave_shutdown", {})
+        pl.add_experiment("ECHE_slave_shutdown", {})
 
-#     return pl.experiment_plan_list  # returns complete experiment list
+    return pl.experiment_plan_list  # returns complete experiment list
+
+def ECHE_CA_led_spectrometer(
+    sequence_version: int = 1,
+    plate_id: int = 1,
+    plate_sample_no_list: list = [2],
+    reservoir_electrolyte: Electrolyte = "SLF10",
+    reservoir_liquid_sample_no: int = 1,
+    reservoir_bubbler_gas: str = "O2",
+    ph: float = 9.53,
+    droplet_size_cm2: float = .071,  #3mm diameter droplet
+    liquid_volume_ml: float = 1.0,
+    ref_vs_nhe: float = 0.21,
+    CA_potential_vsRHE: float = 1.23,
+    CA_duration_sec: float = 15,
+    CA_samplerate_sec: float = 0.05,
+    IErange: str = "auto",
+    led_typenamesdate: list = ["led"],
+    led_wavelengths_nm: list = [0],
+    led_intensities_mw: list = [0],
+    led_number_CA: int = 1,
+    toggle_onCA_ms: float = 500,
+    toggle_offCA_ms: float = 500,
+    toggle_offsetCA_ms: float = 0,
+    toggle_durationCA_ms: float = -1,
+    toggle_two_on_ms: float = 100,
+    toggle_two_off_ms: float = 500,
+    toggle_two_offset_ms: float = 0,
+    toggle_two_duration_ms: float = -1,
+):
+
+    pl = ExperimentPlanMaker()
+
+    # (1) house keeping
+    pl.add_experiment("ECHE_slave_unloadall_customs", {})
+
+    for plate_sample in plate_sample_no_list:
+
+        pl.add_experiment(
+            "ECHE_slave_startup",
+            {
+                "solid_custom_position": "cell1_we",
+                "solid_plate_id": plate_id,
+                "solid_sample_no": plate_sample,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
+                "reservoir_bubbler_gas":     reservoir_bubbler_gas,
+                "liquid_volume_ml": liquid_volume_ml,
+            },
+        )
+        # CA1
+        pl.add_experiment(
+            "ECHE_slave_CA_led_secondtrigger",
+            {
+                "CA_potential_vsRHE": CA_potential_vsRHE,
+                "ph": ph,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no, #currently liquid sample database number
+                "reservoir_bubbler_gas": reservoir_bubbler_gas,
+                "droplet_size_cm2": droplet_size_cm2,
+                "reference_electrode_type": "NHE",
+                "ref_vs_nhe": ref_vs_nhe,
+                "samplerate_sec": CA_samplerate_sec,
+                "CA_duration_sec": CA_duration_sec,
+                "IErange": IErange,
+                "led": led_typenamesdate[led_number_CA],
+                "wavelength_nm": led_wavelengths_nm[led_number_CA],
+                "wavelength_intensity_mw": led_intensities_mw[led_number_CA],
+                "wavelength_intensity_date": led_typenamesdate[5],
+                "led_side_illumination": led_typenamesdate[0],
+                "toggle_on_ms": toggle_onCA_ms,
+                "toggle_off_ms": toggle_offCA_ms,
+                "toggle_offset_ms": toggle_offsetCA_ms,
+                "toggle_duration_ms": toggle_durationCA_ms,
+                "toggle_two_on_ms": toggle_two_on_ms,
+                "toggle_two_off_ms": toggle_two_off_ms,
+                "toggle_two_offset_ms": toggle_two_offset_ms,
+                "toggle_two_duration_ms": toggle_two_duration_ms,
+            },
+        )
+
+        pl.add_experiment("ECHE_slave_shutdown", {})
+
+    return pl.experiment_plan_list  # returns complete experiment list
+
+def ECHE_CP_led_spectrometer(
+    sequence_version: int = 1,
+    plate_id: int = 1,
+    plate_sample_no_list: list = [2],
+    reservoir_electrolyte: Electrolyte = "SLF10",
+    reservoir_liquid_sample_no: int = 1,
+    reservoir_bubbler_gas: str = "O2",
+    ph: float = 9.53,
+    droplet_size_cm2: float = .071,  #3mm diameter droplet
+    liquid_volume_ml: float = 1.0,
+    reference_electrode_type: str = "NHE",
+    ref_vs_nhe: float = 0.21,
+    CP_current: float = .000001,
+    CP_duration_sec: float = 15,
+    CP_samplerate_sec: float = 0.05,
+    IErange: str = "auto",
+    led_number_CP: int = 1,
+    led_typenamesdate: list = ["led"],
+    led_wavelengths_nm: list = [0],
+    led_intensities_mw: list = [0],
+    toggle_onCP_ms: float = 500,
+    toggle_offCP_ms: float = 500,
+    toggle_offsetCP_ms: float = 0,
+    toggle_durationCP_ms: float = -1,
+    toggle_two_on_ms: float = 100,
+    toggle_two_off_ms: float = 500,
+    toggle_two_offset_ms: float = 0,
+    toggle_two_duration_ms: float = -1,
+
+):
+
+    pl = ExperimentPlanMaker()
+
+    # (1) house keeping
+    pl.add_experiment("ECHE_slave_unloadall_customs", {})
+
+    for plate_sample in plate_sample_no_list:
+
+        pl.add_experiment(
+            "ECHE_slave_startup",
+            {
+                "solid_custom_position": "cell1_we",
+                "solid_plate_id": plate_id,
+                "solid_sample_no": plate_sample,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
+                "reservoir_bubbler_gas":     reservoir_bubbler_gas,
+                "liquid_volume_ml": liquid_volume_ml,
+            },
+        )
+        # CP1
+        pl.add_experiment(
+            "ECHE_slave_CP_led_secondtrigger",
+            {
+                "CP_current": CP_current,
+                "ph": ph,
+                "reservoir_liquid_sample_no": reservoir_liquid_sample_no, #currently liquid sample database number
+                "reservoir_bubbler_gas": reservoir_bubbler_gas,
+                "droplet_size_cm2": droplet_size_cm2,
+                "reference_electrode_type": "NHE",
+                "ref_vs_nhe": ref_vs_nhe,
+                "samplerate_sec": CP_samplerate_sec,
+                "CP_duration_sec": CP_duration_sec,
+                "IErange": IErange,
+                "led": led_typenamesdate[led_number_CP],
+                "wavelength_nm": led_wavelengths_nm[led_number_CP],
+                "wavelength_intensity_mw": led_intensities_mw[led_number_CP],
+                "wavelength_intensity_date": led_typenamesdate[5],
+                "led_side_illumination": led_typenamesdate[0],
+                "toggle_on_ms": toggle_onCP_ms,
+                "toggle_off_ms": toggle_offCP_ms,
+                "toggle_offset_ms": toggle_offsetCP_ms,
+                "toggle_duration_ms": toggle_durationCP_ms,
+                "toggle_two_on_ms": toggle_two_on_ms,
+                "toggle_two_off_ms": toggle_two_off_ms,
+                "toggle_two_offset_ms": toggle_two_offset_ms,
+                "toggle_two_duration_ms": toggle_two_duration_ms,
+            },
+        )
+
+        pl.add_experiment("ECHE_slave_shutdown", {})
+
+    return pl.experiment_plan_list  # returns complete experiment list
+
 
