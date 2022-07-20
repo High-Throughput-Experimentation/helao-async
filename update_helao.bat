50786e80e421e730dc7930af69973e20d7a94851
Dan Guevarra <onepunchdan@users.noreply.github.com>
2022-07-20 10:11:35 -0700

    remove ENDLOCAL, fetch before pull

--- update_helao.bat
+++ update_helao.bat
@@ -1,9 +1,10 @@
 @echo off
 call conda activate base
 cd C:\INST_hlo\CODE\helao-core
+git fetch
 git reset --hard
 git pull
 cd C:\INST_hlo\CODE\helao-async
+git fetch
 git reset --hard
 git pull
-ENDLOCAL
