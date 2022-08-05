936cc9ed91644e67b443d610ddae80227b6b28ad
Dan Guevarra <onepunchdan@users.noreply.github.com>
2022-08-05 16:29:23 -0700

    feat: new spec_type and reference_mode enums

--- helao/drivers/spec/enum.py
+++ helao/drivers/spec/enum.py
@@ -5,3 +5,12 @@ class SpecTrigType(int, Enum):
     off = 10
     internal = 11
     external = 12
+
+class SpecType(str, Enum):
+    T = "T"
+    R = "R"
+    
+class ReferenceMode(str, Enum):
+    internal = "internal"  # measure nearest references to starting and ending samples
+    builtin = "builtin"  # measure reference position defined in config
+    blank = "blank"  # measure starting and ending samples w/auto-stop for plate swap
