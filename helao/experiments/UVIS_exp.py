013fb5ba061e099821ab82226f3e0e3261dd347a
Dan Guevarra <onepunchdan@users.noreply.github.com>
2022-08-05 16:37:01 -0700

    feat: UVIS exp and sequences w/ref measurement

--- helao/experiments/UVIS_exp.py
+++ helao/experiments/UVIS_exp.py
@@ -9,63 +9,47 @@ __all__ = [
     "UVIS_sub_startup",
     "UVIS_sub_shutdown",
     "UVIS_sub_movetosample",
-    "UVIS_sub_move",
-    "UVIS_sub_spectrometer_T",
-    "UVIS_sub_spectrometer_R",
+    "UVIS_sub_relmove",
+    "UVIS_sub_measure",
 ]
 
 
-from typing import Optional #, List, Union
+from typing import Optional  # , List, Union
 from socket import gethostname
 
 from helaocore.models.action_start_condition import ActionStartCondition
-from helaocore.models.sample import SolidSample #, LiquidSample
-from helaocore.models.machine import MachineModel
+from helaocore.models.sample import SolidSample  # , LiquidSample
+from helaocore.models.machine import MachineModel as MM
 from helaocore.models.process_contrib import ProcessContrib
+from helaocore.models.run_use import RunUse
+
 # from helaocore.models.electrolyte import Electrolyte
 
-from helao.helpers.premodels import Experiment, ActionPlanMaker #, Action
+from helao.helpers.premodels import Experiment, ActionPlanMaker  # , Action
 from helao.drivers.motion.enum import MoveModes, TransformationModes
 from helao.drivers.io.enum import TriggerType
+from helao.drivers.spec.enum import SpecType
+
 # from helao.drivers.robot.enum import PALtools, Spacingmethod
 
 
 EXPERIMENTS = __all__
 
-# PSTAT_server = MachineModel(server_name="PSTAT", machine_name=gethostname()).json_dict()
-
-MOTOR_server = MachineModel(server_name="MOTOR", machine_name=gethostname()).json_dict()
-IO_server = MachineModel(server_name="IO", machine_name=gethostname()).json_dict()
-SPEC_T_server = MachineModel(
-    server_name="SPEC_T", machine_name=gethostname()
-).json_dict()
-SPEC_R_server = MachineModel(
-    server_name="SPEC_R", machine_name=gethostname()
-).json_dict()
-
-ORCH_server = MachineModel(server_name="ORCH", machine_name=gethostname()).json_dict()
-PAL_server = MachineModel(server_name="PAL", machine_name=gethostname()).json_dict()
+MOTOR_server = MM(server_name="MOTOR", machine_name=gethostname()).json_dict()
+IO_server = MM(server_name="IO", machine_name=gethostname()).json_dict()
+SPEC_T_server = MM(server_name="SPEC_T", machine_name=gethostname()).json_dict()
+SPEC_R_server = MM(server_name="SPEC_R", machine_name=gethostname()).json_dict()
+ORCH_server = MM(server_name="ORCH", machine_name=gethostname()).json_dict()
+PAL_server = MM(server_name="PAL", machine_name=gethostname()).json_dict()
 
 toggle_triggertype = TriggerType.fallingedge
 
 
 def UVIS_sub_unloadall_customs(experiment: Experiment):
-    """last functionality test: -"""
-
-    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    apm.add_action(
-        {
-            "action_server": PAL_server,
-            "action_name": "archive_custom_unloadall",
-            "action_params": {
-                "destroy_liquid": True,
-            },
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-        }
-    )
-
-    return apm.action_list  # returns complete action list to orch
+    """Clear samples from measurement position."""
+    apm = ActionPlanMaker()
+    apm.add(PAL_server, "archive_custom_unloadall", {"destroy_liquid": True})
+    return apm.action_list
 
 
 def UVIS_sub_load_solid(
@@ -75,28 +59,20 @@ def UVIS_sub_load_solid(
     solid_plate_id: Optional[int] = 4534,
     solid_sample_no: Optional[int] = 1,
 ):
-    """last functionality test: -"""
-
+    """Load solid sample onto measurement position."""
     apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    apm.add_action(
+    apm.add(
+        PAL_server,
+        "archive_custom_load",
         {
-            "action_server": PAL_server,
-            "action_name": "archive_custom_load",
-            "action_params": {
-                "custom": apm.pars.solid_custom_position,
-                "load_sample_in": SolidSample(
-                    **{
-                        "sample_no": apm.pars.solid_sample_no,
-                        "plate_id": apm.pars.solid_plate_id,
-                        "machine_name": "legacy",
-                    }
-                ).dict(),
-            },
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-        }
+            "custom": apm.pars.solid_custom_position,
+            "load_sample_in": SolidSample(
+                sample_no=apm.pars.solid_sample_no,
+                plate_id=apm.pars.solid_plate_id,
+                machine_name="legacy",
+            ),
+        },
     )
-
     return apm.action_list  # returns complete action list to orch
 
 
@@ -107,69 +83,51 @@ def UVIS_sub_startup(
     solid_plate_id: Optional[int] = 4534,
     solid_sample_no: Optional[int] = 1,
 ):
-    """Sub experiment
-    last functionality test: -"""
 
     apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    # unload all samples from custom positions
     apm.add_action_list(UVIS_sub_unloadall_customs(experiment=experiment))
 
     # load new requested solid samples
-    apm.add_action_list(
-        UVIS_sub_load_solid(
-            experiment=experiment,
-            solid_custom_position=apm.pars.solid_custom_position,
-            solid_plate_id=apm.pars.solid_plate_id,
-            solid_sample_no=apm.pars.solid_sample_no,
-        )
+    apm.add(
+        PAL_server,
+        "archive_custom_load",
+        {
+            "custom": apm.pars.solid_custom_position,
+            "load_sample_in": SolidSample(
+                sample_no=apm.pars.solid_sample_no,
+                plate_id=apm.pars.solid_plate_id,
+                machine_name="legacy",
+            ),
+        },
     )
-
     # get sample plate coordinates
-    apm.add_action(
+    apm.add(
+        MOTOR_server,
+        "solid_get_samples_xy",
         {
-            "action_server": MOTOR_server,
-            "action_name": "solid_get_samples_xy",
-            "action_params": {
-                "plate_id": apm.pars.solid_plate_id,
-                "sample_no": apm.pars.solid_sample_no,
-            },
-            "to_global_params": [
-                "_platexy"
-            ],  # save new liquid_sample_no of eche cell to globals
-            "start_condition": ActionStartCondition.wait_for_all,
-        }
+            "plate_id": apm.pars.solid_plate_id,
+            "sample_no": apm.pars.solid_sample_no,
+        },
+        to_global_params=["_platexy"],  # save new liquid_sample_no of cell to globals
     )
-
     # move to position
-    apm.add_action(
+    apm.add(
+        MOTOR_server,
+        "move",
         {
-            "action_server": MOTOR_server,
-            "action_name": "move",
-            "action_params": {
-                # "d_mm": [apm.pars.x_mm, apm.pars.y_mm],
-                "axis": ["x", "y"],
-                "mode": MoveModes.absolute,
-                "transformation": TransformationModes.platexy,
-            },
-            "from_global_params": {"_platexy": "d_mm"},
-            "start_condition": ActionStartCondition.wait_for_all,
-        }
+            "axis": ["x", "y"],
+            "mode": MoveModes.absolute,
+            "transformation": TransformationModes.platexy,
+        },
+        from_global_params={"_platexy": "d_mm"},
     )
-
     return apm.action_list  # returns complete action list to orch
 
 
 def UVIS_sub_shutdown(experiment: Experiment):
-    """Sub experiment
-
-    last functionality test: -"""
-
     apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
     # unload all samples from custom positions
     apm.add_action_list(UVIS_sub_unloadall_customs(experiment=experiment))
-
     return apm.action_list  # returns complete action list to orch
 
 
@@ -179,43 +137,29 @@ def UVIS_sub_movetosample(
     solid_plate_id: Optional[int] = 4534,
     solid_sample_no: Optional[int] = 1,
 ):
-    """Sub experiment
-    last functionality test: -"""
-
     apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    # get sample plate coordinates
-    apm.add_action(
+    apm.add(
+        MOTOR_server,
+        "solid_get_samples_xy",
         {
-            "action_server": MOTOR_server,
-            "action_name": "solid_get_samples_xy",
-            "action_params": {
-                "plate_id": apm.pars.solid_plate_id,
-                "sample_no": apm.pars.solid_sample_no,
-            },
-            "to_global_params": [
-                "_platexy"
-            ],  # save new liquid_sample_no of eche cell to globals
-            "start_condition": ActionStartCondition.wait_for_all,
-        }
+            "plate_id": apm.pars.solid_plate_id,
+            "sample_no": apm.pars.solid_sample_no,
+        },
+        to_global_params=[
+            "_platexy"
+        ],  # save new liquid_sample_no of eche cell to globals
     )
-
     # move to position
-    apm.add_action(
+    apm.add(
+        MOTOR_server,
+        "move",
         {
-            "action_server": MOTOR_server,
-            "action_name": "move",
-            "action_params": {
-                # "d_mm": [apm.pars.x_mm, apm.pars.y_mm],
-                "axis": ["x", "y"],
-                "mode": MoveModes.absolute,
-                "transformation": TransformationModes.platexy,
-            },
-            "from_global_params": {"_platexy": "d_mm"},
-            "start_condition": ActionStartCondition.wait_for_all,
-        }
+            "axis": ["x", "y"],
+            "mode": MoveModes.absolute,
+            "transformation": TransformationModes.platexy,
+        },
+        from_global_params={"_platexy": "d_mm"},
     )
-
     return apm.action_list  # returns complete action list to orch
 
 
@@ -225,129 +169,167 @@ def UVIS_sub_relmove(
     offset_x_mm: float = 1.0,
     offset_y_mm: float = 1.0,
 ):
-    """Sub experiment
-    last functionality test: -"""
+    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
+    apm.add(
+        MOTOR_server,
+        "move",
+        {
+            "d_mm": [apm.pars.offset_x_mm, apm.pars.offset_y_mm],
+            "axis": ["x", "y"],
+            "mode": MoveModes.relative,
+            "transformation": TransformationModes.platexy,
+        },
+    )
+    return apm.action_list  # returns complete action list to orch
 
+
+def UVIS_sub_measure(
+    experiment: Experiment,
+    experiment_version: int = 1,
+    spec_type: Optional[SpecType] = "T",
+    spec_n_avg: Optional[int] = 1,
+    spec_int_time_ms: Optional[int] = 35,
+    duration_sec: Optional[float] = -1,
+    toggle_source: Optional[str] = "doric_wled",  # this could be a shutter
+    illumination_wavelength: Optional[float] = -1,
+    illumination_intensity: Optional[float] = -1,
+    illumination_intensity_date: Optional[str] = "n/a",
+    technique_name: Optional[str] = "T_UVVIS",
+    run_use: Optional[RunUse] = "data",
+    comment: Optional[str] = "",
+):
     apm = ActionPlanMaker()  # exposes function parameters via apm.pars
 
+    # query loaded sample in cell1_we position
+    apm.add(
+        PAL_server,
+        "archive_custom_query_sample",
+        {"custom": "cell1_we"},
+        to_global_params=["_fast_samples_in"],
+    )
+
+    # depending on run_use
+    apm.add(
+        IO_server,
+        "set_digital_out",
+        {
+            "do_item": apm.pars.toggle_source,
+            "on": False if apm.pars.run_use == "ref_dark" else True,
+        },
+    )
+
+    # setup spectrometer data collection
+    apm.add(
+        SPEC_T_server if apm.pars.spec_type == SpecType.T else SPEC_R_server,
+        "acquire_spec",
+        {
+            "int_time_ms": apm.pars.spec_int_time_ms,
+            "n_avg": apm.pars.spec_n_avg,
+            "duration_sec": apm.pars.duration_sec,
+        },
+        from_global_params={"_fast_samples_in": "fast_samples_in"},
+        run_use=apm.pars.run_use,
+        technique_name=apm.pars.technique_name,
+        process_finish=True,
+        process_contrib=[
+            ProcessContrib.files,
+            ProcessContrib.samples_in,
+            ProcessContrib.samples_out,
+            ProcessContrib.run_use,
+        ],
+    )
+
+    #  turn off illumination source or close shutter
+    apm.add(
+        IO_server,
+        "set_digital_out",
+        {"do_item": apm.pars.toggle_source, "on": False},
+    )
+
+    return apm.action_list  # returns complete action list to orch
+
+
+def UVIS_sub_setup_ref(
+    experiment: Experiment,
+    experiment_version: int = 1,
+    reference_sample_type: str = "internal",
+    solid_custom_position: Optional[str] = "cell1_we",
+    solid_plate_id: int = 1,
+    solid_sample_no: int = 2,
+    specref_code: int = 4,
+):
+    """Determine initial and final reference measurements and move to position."""
+    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
+    if reference_sample_type == "internal":
+        apm.add(
+            MOTOR_server,
+            "solid_get_nearest_specref",
+            {
+                "plate_id": apm.pars.solid_plate_id,
+                "sample_no": apm.pars.solid_sample_no,
+                "specref_code": apm.pars.specref_code
+            },
+            to_global_params=["_refno", "_refxy"]
+        )
+        apm.add(
+            PAL_server,
+            "archive_custom_load_solid",
+            {
+                "custom": apm.pars.solid_custom_position,
+                "plate_id": apm.pars.solid_plate_id,
+            },
+            from_global_params={"_refno": "sample_no"}
+        )
+    elif reference_sample_type == "builtin":
+        apm.add(
+            MOTOR_server,
+            "solid_get_builtin_specref", {},
+            to_global_params=["_refno", "_refxy"]
+        )
+        apm.add(
+            PAL_server,
+            "archive_custom_load_solid",
+            {
+                "custom": apm.pars.solid_custom_position,
+                "plate_id": apm.pars.solid_plate_id,
+            },
+            from_global_params={"_refno": "sample_no"}
+        )
+    elif reference_sample_type == "blank":
+        apm.add(
+            ORCH_server,
+            "interrupt"
+        )
+        apm.add(
+            PAL_server,
+            "archive_custom_load",
+            {
+                "custom": apm.pars.solid_custom_position,
+                "load_sample_in": SolidSample(
+                    sample_no=apm.pars.solid_sample_no,
+                    plate_id=apm.pars.solid_plate_id,
+                    machine_name="legacy",
+                ),
+            },
+        )
+        apm.add(
+            MOTOR_server,
+            "solid_get_samples_xy",
+            {
+                "plate_id": apm.pars.solid_plate_id,
+                "sample_no": apm.pars.solid_sample_no,
+            },
+            to_global_params={"_platexy": "_refxy"},
+        )
     # move to position
-    apm.add_action(
+    apm.add(
+        MOTOR_server,
+        "move",
         {
-            "action_server": MOTOR_server,
-            "action_name": "move",
-            "action_params": {
-                "d_mm": [apm.pars.offset_x_mm, apm.pars.offset_y_mm],
-                "axis": ["x", "y"],
-                "mode": MoveModes.relative,
-                "transformation": TransformationModes.platexy,
-            },
-            #            "from_global_params": {"_platexy": "d_mm"},
-            "start_condition": ActionStartCondition.wait_for_all,
-        }
-    )
-
-    return apm.action_list  # returns complete action list to orch
-
-
-def UVIS_sub_spectrometer_T(
-    experiment: Experiment,
-    experiment_version: int = 1,
-    toggle_illum_time: Optional[float] = -1,
-    spec_n_avg: Optional[int] = 1,
-    spec_int_time: Optional[int] = 35,
-    comment: Optional[str] = "",
-):
-    """last functionality test: -"""
-
-    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    # get sample for gamry
-    apm.add_action(
-        {
-            "action_server": PAL_server,
-            "action_name": "archive_custom_query_sample",
-            "action_params": {
-                "custom": "cell1_we",
-            },
-            "to_global_params": [
-                "_fast_samples_in"
-            ],  # save new liquid_sample_no of eche cell to globals
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-        }
-    )
-
-    # setup spectrometer data collection
-    apm.add_action(
-        {
-            "action_server": SPEC_T_server,
-            "action_name": "acquire_spec",
-            "action_params": {
-                "int_time": apm.pars.spec_int_time,
-                #                "n_avg": apm.pars.spec_n_avg,
-                #                "duration": apm.pars.toggle_illum_time,
-            },
-            "from_global_params": {"_fast_samples_in": "fast_samples_in"},
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-            "technique_name": "T_UVVIS",
-            "process_finish": True,
-            "process_contrib": [
-                ProcessContrib.files,
-                ProcessContrib.samples_in,
-                ProcessContrib.samples_out,
-            ],
+            "axis": ["x", "y"],
+            "mode": MoveModes.absolute,
+            "transformation": TransformationModes.platexy,
         },
+        from_global_params={"_refxy": "d_mm"},
     )
-
-    return apm.action_list  # returns complete action list to orch
-
-
-def UVIS_sub_spectrometer_R(
-    experiment: Experiment,
-    experiment_version: int = 1,
-    toggle_illum_time: Optional[float] = -1,
-    spec_n_avg: Optional[int] = 1,
-    spec_int_time: Optional[int] = 35,
-    comment: Optional[str] = "",
-):
-    """last functionality test: -"""
-
-    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
-
-    # get sample for gamry
-    apm.add_action(
-        {
-            "action_server": PAL_server,
-            "action_name": "archive_custom_query_sample",
-            "action_params": {
-                "custom": "cell1_we",
-            },
-            "to_global_params": [
-                "_fast_samples_in"
-            ],  # save new liquid_sample_no of eche cell to globals
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-        }
-    )
-
-    # setup spectrometer data collection
-    apm.add_action(
-        {
-            "action_server": SPEC_R_server,
-            "action_name": "acquire_spec",
-            "action_params": {
-                "int_time": apm.pars.spec_int_time,
-                #                "n_avg": apm.pars.spec_n_avg,
-                #                "duration": apm.pars.toggle_illum_time,
-            },
-            "from_global_params": {"_fast_samples_in": "fast_samples_in"},
-            "start_condition": ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
-            "technique_name": "R_UVVIS",
-            "process_finish": True,
-            "process_contrib": [
-                ProcessContrib.files,
-                ProcessContrib.samples_in,
-                ProcessContrib.samples_out,
-            ],
-        },
-    )
-
     return apm.action_list  # returns complete action list to orch
--- helao/sequences/UVIS_seq.py
+++ helao/sequences/UVIS_seq.py
@@ -5,8 +5,6 @@ Sequence library for ECHE
 __all__ = [
     "UVIS_TR",
     "UVIS_T"
-    # "UVIS_movetosample",
-    # "UVIS_move",
 ]
 
 
@@ -17,115 +15,135 @@ from helaocore.models.electrolyte import Electrolyte
 SEQUENCES = __all__
 
 
-# def UVIS_movetosample(
-#     sequence_version: int = 1,
-#     plate_id: int = 1,
-#     plate_sample_no: int = 1,
-# ):
-
-#     pl = ExperimentPlanMaker()
-#     pl.add_experiment(
-#         "UVIS_sub_movetosample",
-#         {
-#             #            "solid_custom_position": "cell1_we",
-#             "solid_plate_id": plate_id,
-#             "solid_sample_no": plate_sample_no,
-#         },
-#     )
-#     pl.add_experiment("UVIS_sub_shutdown", {})
-#     return pl.experiment_plan_list  # returns complete experiment list
-
-
-# def UVIS_move(
-#     sequence_version: int = 1,
-#     move_x_mm: float = 1.0,
-#     move_y_mm: float = 1.0,
-# ):
-#     """ Move by offset b
-#     """
-
-#     pl = ExperimentPlanMaker()
-#     pl.add_experiment(
-#         "UVIS_sub_move",
-#         {
-#             "x_mm": move_x_mm,
-#             "y_mm": move_y_mm,
-#         },
-#     )
-#     pl.add_experiment("UVIS_sub_shutdown", {})
-#     return pl.experiment_plan_list  # returns complete experiment list
-
-
-def UVIS_TR(
-    sequence_version: int = 2,
-    plate_id: int = 1,
-    plate_sample_no_list: list = [2],
-    toggle_illum_time: float = -1,
-    Spec_n_avg: int = 1,
-    Spec_integration_time_ms: int = 35,
-):
-    pl = ExperimentPlanMaker()
-    pl.add_experiment("UVIS_sub_unloadall_customs", {})
-
-    for plate_sample in plate_sample_no_list:
-        pl.add_experiment(
-            "UVIS_sub_startup",
-            {
-                "solid_custom_position": "cell1_we",
-                "solid_plate_id": plate_id,
-                "solid_sample_no": plate_sample,
-            },
-        )
-        pl.add_experiment(
-            "UVIS_sub_spectrometer_T",
-            {
-                "toggle_illum_time": toggle_illum_time,
-                "spec_n_avg": Spec_n_avg,
-                "spec_int_time": Spec_integration_time_ms,
-            },
-        )
-        pl.add_experiment(
-            "UVIS_sub_spectrometer_R",
-            {
-                "toggle_illum_time": toggle_illum_time,
-                "spec_n_avg": Spec_n_avg,
-                "spec_int_time": Spec_integration_time_ms,
-            },
-        )
-        pl.add_experiment("UVIS_sub_shutdown", {})
-
-    return pl.experiment_plan_list  # returns complete experiment list
-
-
 def UVIS_T(
     sequence_version: int = 2,
     plate_id: int = 1,
     plate_sample_no_list: list = [2],
-    toggle_illum_time: float = -1,
-    Spec_n_avg: int = 1,
-    Spec_integration_time_ms: int = 35,
+    reference_mode: str = "internal",
+    custom_position: str = "cell1_we",
+    spec_n_avg: int = 1,
+    spec_int_time_ms: int = 35,
+    duration_sec: float = -1,
+    led_type: str = "front",
+    led_date: str = "n/a",
+    led_names: list = ["doric_wled"],
+    led_wavelengths_nm: list = [-1],
+    led_intensities_mw: list = [-1],
 ):
-    pl = ExperimentPlanMaker()
-    pl.add_experiment("UVIS_sub_unloadall_customs", {})
+    epm = ExperimentPlanMaker()
+    epm.add_experiment("UVIS_sub_unloadall_customs", {})
+    epm.add_experiment(
+        "UVIS_sub_setup_ref",
+        {
+            "reference_mode": reference_mode,
+            "solid_custom_position": custom_position,
+            "solid_plate_id": plate_id,
+            "solid_sample_no": plate_sample_no_list[0]
+        }
+    )
+    # dark ref
+    epm.add_experiment(
+        "UVIS_sub_measure",  
+        {
+            "spec_type": "T",
+            "spec_int_time": spec_integration_time_ms,
+            "spec_n_avg": spec_n_avg,
+            "duration_sec": toggle_illum_time_sec,
+            "toggle_source": led_names[0],
+            "illumination_wavelength": led_wavelengths_nm[0],
+            "illumination_intensity": led_intensities_mw[0],
+            "illumination_intensity_date": led_date,
+            "technique_name": "T_UVVIS",
+            "run_use": "ref_dark",
+        },
+    )
+    # light ref
+    epm.add_experiment(
+        "UVIS_sub_measure",  
+        {
+            "spec_type": "T",
+            "spec_int_time": spec_integration_time_ms,
+            "spec_n_avg": spec_n_avg,
+            "duration_sec": toggle_illum_time_sec,
+            "toggle_source": led_names[0],
+            "illumination_wavelength": led_wavelengths_nm[0],
+            "illumination_intensity": led_intensities_mw[0],
+            "illumination_intensity_date": led_date,
+            "technique_name": "T_UVVIS",
+            "run_use": "ref_light",
+        },
+    )
 
     for plate_sample in plate_sample_no_list:
-        pl.add_experiment(
-            "UVIS_sub_startup",
+        epm.add_experiment("UVIS_sub_unloadall_customs", {})
+        epm.add_experiment(
+            "UVIS_sub_startup",  # move to solid sample, assign to cell position
             {
-                "solid_custom_position": "cell1_we",
+                "solid_custom_position": custom_position,
                 "solid_plate_id": plate_id,
                 "solid_sample_no": plate_sample,
             },
         )
-        pl.add_experiment(
-            "UVIS_sub_spectrometer_T",
+        # perform transmission spec
+        epm.add_experiment(
+            "UVIS_sub_measure",  
             {
-                "toggle_illum_time": toggle_illum_time,
-                "spec_n_avg": Spec_n_avg,
-                "spec_int_time": Spec_integration_time_ms,
+                "spec_type": "T",
+                "spec_int_time": spec_integration_time_ms,
+                "spec_n_avg": spec_n_avg,
+                "duration_sec": toggle_illum_time_sec,
+                "toggle_source": led_names[0],
+                "illumination_wavelength": led_wavelengths_nm[0],
+                "illumination_intensity": led_intensities_mw[0],
+                "illumination_intensity_date": led_date,
+                "technique_name": "T_UVVIS",
+                "run_use": "data",
             },
         )
-        pl.add_experiment("UVIS_sub_shutdown", {})
 
-    return pl.experiment_plan_list  # returns complete experiment list
+    epm.add_experiment("UVIS_sub_unloadall_customs", {})
+    epm.add_experiment(
+        "UVIS_sub_setup_ref",
+        {
+            "reference_mode": reference_mode,
+            "solid_custom_position": custom_position,
+            "solid_plate_id": plate_id,
+            "solid_sample_no": plate_sample_no_list[-1]
+        }
+    )
+    # dark ref
+    epm.add_experiment(
+        "UVIS_sub_measure",  
+        {
+            "spec_type": "T",
+            "spec_int_time": spec_integration_time_ms,
+            "spec_n_avg": spec_n_avg,
+            "duration_sec": toggle_illum_time_sec,
+            "toggle_source": led_names[0],
+            "illumination_wavelength": led_wavelengths_nm[0],
+            "illumination_intensity": led_intensities_mw[0],
+            "illumination_intensity_date": led_date,
+            "technique_name": "T_UVVIS",
+            "run_use": "ref_dark",
+        },
+    )
+    # light ref
+    epm.add_experiment(
+        "UVIS_sub_measure",  
+        {
+            "spec_type": "T",
+            "spec_int_time": spec_integration_time_ms,
+            "spec_n_avg": spec_n_avg,
+            "duration_sec": toggle_illum_time_sec,
+            "toggle_source": led_names[0],
+            "illumination_wavelength": led_wavelengths_nm[0],
+            "illumination_intensity": led_intensities_mw[0],
+            "illumination_intensity_date": led_date,
+            "technique_name": "T_UVVIS",
+            "run_use": "ref_light",
+        },
+    )
+    epm.add_experiment("UVIS_sub_shutdown", {})
+
+    return epm.experiment_plan_list  # returns complete experiment list
 
