e8cb9bd53bc317395486c8d0da4787be4f6c374c
Dan Guevarra <onepunchdan@users.noreply.github.com>
2022-12-05 15:30:44 -0800

    feat: initial infuse/withdraw endpoints

--- helao/servers/action/syringe_server.py
+++ helao/servers/action/syringe_server.py
@@ -9,9 +9,10 @@ __all__ = ["makeApp"]
 from typing import Optional, List, Union
 from fastapi import Body
 import numpy as np
+import asyncio
 
 
-from helao.drivers.pump.legato_driver import KDS100
+from helao.drivers.pump.legato_driver import KDS100, PumpExec
 from helao.servers.base import makeActionServ
 from helao.helpers.make_str_enum import make_str_enum
 from helao.helpers.premodels import Action
@@ -48,12 +49,36 @@ def makeApp(confPrefix, servKey, helao_root):
         finished_action = await active.finish()
         return finished_action.as_dict()
 
-    @app.post(f"/start_polling")
+    @app.post("/start_polling")
     async def start_polling():
         await app.driver.start_polling()
 
-    @app.post(f"/stop_polling")
+    @app.post("/stop_polling")
     async def stop_polling():
         await app.driver.stop_polling()
+
+    @app.post(f"/{servKey}/infuse")
+    async def infuse(
+        action: Optional[Action] = Body({}, embed=True),
+        action_version: int = 1,
+        rate_uL_sec: int = 0,
+        volume_uL: int = 0,
+    ):
+        active = await app.base.setup_and_contain_action()
+        active.executor = PumpExec(1, active)
+        active_action_dict = active.start_executor()
+        return active_action_dict
+
+    @app.post(f"/{servKey}/withdraw")
+    async def withdraw(
+        action: Optional[Action] = Body({}, embed=True),
+        action_version: int = 1,
+        rate_uL_sec: int = 0,
+        volume_uL: int = 0,
+    ):
+        active = await app.base.setup_and_contain_action()
+        active.executor = PumpExec(0, active)
+        active_action_dict = active.start_executor()
+        return active_action_dict
 
     return app
