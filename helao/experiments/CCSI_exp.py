1fa78bd90f0d25b9784b0372e5fb95bd21856221
Kevin Kan <kkan@caltech.edu>
2023-01-10 12:56:59 -0800

    fix typo

--- helao/experiments/CCSI_exp.py
+++ helao/experiments/CCSI_exp.py
@@ -619,7 +619,7 @@ def CCSI_sub_liquidfill_syringes(
     apm.add(SOLUTIONPUMP_server, "withdraw", {"rate_uL_sec": apm.pars.Syringe_rate_ulsec , "volume_uL": apm.pars.Syringe_retraction_ul},start_condition=ActionStartCondition.no_wait)
     apm.add(ORCH_server, "wait", {"waittime": 0.25})
 #    if Waterclean_volume_ul != 0:
-    apm.add(CO2S_server, "acquire_co2", {"duration": withdr_acqtime, "acquisition_rate": apm.pars.co2measure_acqrate})
+#    apm.add(CO2S_server, "acquire_co2", {"duration": withdr_acqtime, "acquisition_rate": apm.pars.co2measure_acqrate})
     apm.add(WATERCLEANPUMP_server, "withdraw", {"rate_uL_sec": apm.pars.Syringe_rate_ulsec, "volume_uL": apm.pars.Syringe_retraction_ul},start_condition=ActionStartCondition.no_wait)    
 
     #mfc off, v2, v1ab v7 close    
