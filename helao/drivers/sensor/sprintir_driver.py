a9a165ad2cdb91247443cadee7dbe4fb5e0b8fe0
onepunchdan <onepunchdan@users.noreply.github.com>
2023-01-12 20:35:21 -0800

    fix: use serial readlines()

--- helao/drivers/sensor/sprintir_driver.py
+++ helao/drivers/sensor/sprintir_driver.py
@@ -222,8 +222,7 @@ class SprintIR:
 
     def read_stream(self):
         self.base.print_message("Reading sensor stream.")
-        # buf = self.com.read_until(b"\r\n")
-        buf = self.com.read()
+        buf = self.com.readlines()
         lines = buf.decode("utf8").split("\n")
         self.base.print_message(f"Stream returned {lines}")
         for line in lines:
