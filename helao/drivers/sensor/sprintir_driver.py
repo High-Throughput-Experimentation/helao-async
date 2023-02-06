9aa2ad84d449cc2f7270d934bf5e7e19be6f3dc8
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-02-06 14:56:21 -0800

    wip: print live buffer query

--- helao/drivers/sensor/sprintir_driver.py
+++ helao/drivers/sensor/sprintir_driver.py
@@ -398,6 +398,7 @@ class CO2MonExec(Executor):
     async def _poll(self):
         """Read CO2 ppm from live buffer."""
         live_dict, epoch_s = self.active.base.get_lbuf("co2_ppm")
+        self.active.base.print_message(f"got from live buffer: {live_dict}")
         live_dict["epoch_s"] = epoch_s
         iter_time = time.time()
         elapsed_time = iter_time - self.start_time
