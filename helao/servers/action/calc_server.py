f5fc215f70e38542180194aa47667dc36edd45ad
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-02-07 08:37:34 -0800

    fix: await Calc.check_co2purge()

--- helao/servers/action/calc_server.py
+++ helao/servers/action/calc_server.py
@@ -94,7 +94,7 @@ def makeApp(confPrefix, servKey, helao_root):
         repeat_experiment_kwargs: dict = {},
     ):
         active = await app.base.setup_and_contain_action(action_abbr="checkCO2")
-        result = app.driver.check_co2_purge_level(active)
+        result = await app.driver.check_co2_purge_level(active)
         app.base.print_message(f"result dict was: {result}")
         await active.enqueue_data_dflt(datadict=result)
         finished_action = await active.finish()
