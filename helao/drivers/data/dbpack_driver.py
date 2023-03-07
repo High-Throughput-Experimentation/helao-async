afd7261020b0887a8ded46541533cd7a4f64189a
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-03-07 10:44:09 -0800

    fix: get action list from progress dict

--- helao/drivers/data/dbpack_driver.py
+++ helao/drivers/data/dbpack_driver.py
@@ -415,7 +415,7 @@ class ExpYml(HelaoYml):
         fill_process["process_params"] = fill_process.pop("action_params")
 
         # deduplicate samples_in by earliest action, samples_out by latest
-        expacts = [x for k, x in self.dict.items() if k.startswith("action--")]
+        expacts = [x for k, x in self.progress.items() if k.startswith("action--")]
         print(expacts)
         if fill_process["samples_in"]:
             smpindates = []
