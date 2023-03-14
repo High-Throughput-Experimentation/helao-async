a4f4b5e884b7b33e54f8d1d4b4fbbaf0e62126f3
yul69-cell <64283970+yul69-cell@users.noreply.github.com>
2023-03-14 14:50:57 -0700

    add ANEC_OCV to sequence

--- helao/sequences/ANEC_seq.py
+++ helao/sequences/ANEC_seq.py
@@ -1,6 +1,6 @@
 """Sequence library for ANEC"""
 
-__all__ = ["ANEC_sample_ready", "ANEC_series_CA", "ANEC_series_CAliquidOnly", "ANEC_photo_CA", "ANEC_photo_CV", "ANEC_cleanup_disengage","ANEC_repeat_CA", "ANEC_repeat_CV", "ANEC_CA_pretreat", "ANEC_gasonly_CA", "GC_Archiveliquid_analysis", "HPLC_Archiveliquid_analysis"]
+__all__ = ["ANEC_sample_ready", "ANEC_series_CA", "ANEC_series_CAliquidOnly", "ANEC_OCV","ANEC_photo_CA", "ANEC_photo_CV", "ANEC_cleanup_disengage","ANEC_repeat_CA", "ANEC_repeat_CV", "ANEC_CA_pretreat", "ANEC_gasonly_CA", "GC_Archiveliquid_analysis", "HPLC_Archiveliquid_analysis"]
 
 from typing import List
 from typing import Optional
@@ -300,6 +300,89 @@ def ANEC_series_CAliquidOnly(
 
     epm.add_experiment("ANEC_sub_alloff", {})
     
+    return epm.experiment_plan_list
+
+def ANEC_OCV(
+    sequence_version: int = 1,
+    plate_id: int = 4534,
+    solid_sample_no: int = 1,
+    reservoir_liquid_sample_no: int = 1,
+    volume_ul_cell_liquid: float = 1000,
+    Tval__s: Optional[float] = 900.0,
+    IErange: Optional[str] = "auto",
+    toolarchive: str = "LS 3",
+    volume_ul_archive: int = 500,
+    liquidDrain_time: float = 80.0,
+    wash1: bool = True,
+    wash2: bool = True,
+    wash3: bool = True,
+    wash4: bool = False,
+):
+    """running CA at different potentials and aliquot sampling at the cell1_we position.
+
+    Flush and fill cell, run CA, and drain.
+
+    (1) Fill cell with liquid for 90 seconds
+    (2) Equilibrate for 15 seconds
+    (3) run CA
+    (4) mix product
+    (5) Drain cell and purge with CO2 for 60 seconds
+
+    Args:
+        exp (Experiment): Active experiment object supplied by Orchestrator
+        toolGC (str): PAL tool string enumeration (see pal_driver.PALTools)
+        volume_ul_GC: GC injection volume
+
+
+
+    """
+
+    epm = ExperimentPlanMaker()
+
+    # housekeeping
+    epm.add_experiment("ANEC_sub_unload_cell", {})
+
+    #epm.add_experiment("ANEC_sub_normal_state", {})
+
+    epm.add_experiment(
+        "ANEC_sub_load_solid",
+        {"solid_plate_id": plate_id, "solid_sample_no": solid_sample_no},
+    )
+
+
+
+    epm.add_experiment(
+        "ANEC_sub_flush_fill_cell",
+        {
+            "liquid_flush_time": 80,
+            "co2_purge_time": 15,
+            "equilibration_time": 1.0,
+            "reservoir_liquid_sample_no": reservoir_liquid_sample_no,
+            "volume_ul_cell_liquid": volume_ul_cell_liquid,
+        },
+    )
+
+    epm.add_experiment(
+        "ANEC_sub_OCV",
+        {
+            "Tval__s": Tval__s,
+            "IErange": IErange,
+        },
+    )
+
+    epm.add_experiment(
+        "ANEC_sub_liquidarchive",
+        {
+            "toolarchive": toolarchive,
+            "volume_ul_archive": volume_ul_archive,
+            "wash1": wash1,
+            "wash2": wash2,
+            "wash3": wash3,
+            "wash4": wash4,
+        },
+    )
+
+    epm.add_experiment("ANEC_sub_drain_cell", {"drain_time": liquidDrain_time})    
     return epm.experiment_plan_list
 
 def ANEC_photo_CA(
