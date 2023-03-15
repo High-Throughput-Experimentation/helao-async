b276dd34b93d2007f2532bf74b1fbe08b778737a
yul69-cell <64283970+yul69-cell@users.noreply.github.com>
2023-03-15 09:42:06 -0700

    Anec add ocv (#49)

--- helao/experiments/ANEC_exp.py
+++ helao/experiments/ANEC_exp.py
@@ -19,6 +19,7 @@ __all__ = [
     "ANEC_sub_cleanup",
     "ANEC_sub_CP",
     "ANEC_sub_CA",
+    "ANEC_sub_OCV",
     "ANEC_sub_liquidarchive",
     "ANEC_sub_aliquot",
     "ANEC_sub_alloff",
@@ -246,7 +247,7 @@ def ANEC_sub_flush_fill_cell(
     liquid_flush_time: Optional[float] = 80,
     co2_purge_time: Optional[float] = 15,
     equilibration_time: Optional[float] = 1.0,
-    reservoir_liquid_sample_no: Optional[int] = 1,
+    reservoir_liquid_sample_no: Optional[int] = 1511,
     volume_ul_cell_liquid: Optional[int] = 1000,
 ):
     """Add liquid volume to cell position.
@@ -323,8 +324,8 @@ def ANEC_sub_unload_liquid(
 
 def ANEC_sub_drain_cell(
     experiment: Experiment,
-    experiment_version: int = 2,
-    drain_time: Optional[float] = 50.0,
+    experiment_version: int = 3,
+    drain_time: Optional[float] = 70.0,
 ):
     """Drain liquid from cell and unload liquid sample."""
 
@@ -339,7 +340,7 @@ def ANEC_sub_drain_cell(
 def ANEC_sub_cleanup(
     experiment: Experiment,
     experiment_version: int = 1,
-    reservoir_liquid_sample_no: Optional[int] = 1,
+    reservoir_liquid_sample_no: Optional[int] = 1511,
 ):
     """Flush and purge ANEC cell.
 
@@ -439,7 +440,7 @@ def ANEC_sub_load_solid_and_clean_cell(
     experiment_version: int = 1,
     solid_plate_id: Optional[int] = 1,
     solid_sample_no: Optional[int] = 1,
-    reservoir_liquid_sample_no: Optional[int] = 1,
+    reservoir_liquid_sample_no: Optional[int] = 1511,
     recirculation_time: Optional[float] = 60,
     toolGC: Optional[str] = "HS 2",
     volume_ul_GC: Optional[int] = 300,
@@ -690,6 +691,49 @@ def ANEC_sub_CA(
     # apm.add(ORCH_server, "wait", {"waittime": 10})
 
     return apm.action_list
+
+
+def ANEC_sub_OCV(
+    experiment: Experiment,
+    experiment_version: int = 1,
+    Tval__s: Optional[float] = 900.0,
+    IErange: Optional[str] = "auto",
+):
+    apm = ActionPlanMaker()  # exposes function parameters via apm.pars
+
+    # get sample for gamry
+    apm.add(
+        PAL_server,
+        "archive_custom_query_sample",
+        {
+            "custom": "cell1_we",
+        },
+        to_globalexp_params=[
+            "_fast_samples_in"
+        ],  # save new liquid_sample_no of eche cell to globals
+        start_condition=ActionStartCondition.wait_for_all,  # orch is waiting for all action_dq to finish
+    )
+
+    # OCV
+    apm.add(
+        PSTAT_server,
+        "run_OCV",
+        {
+            "Tval__s": apm.pars.Tval__s,
+            "SampleRate": 0.05,
+            "IErange": apm.pars.IErange,
+        },
+        from_globalexp_params={"_fast_samples_in": "fast_samples_in"},
+        technique_name="CP",
+        process_finish=True,
+        process_contrib=[
+            ProcessContrib.action_params,
+            ProcessContrib.files,
+            ProcessContrib.samples_in,
+            ProcessContrib.samples_out,
+        ],
+    )
+    return apm.action_list  # returns complete action list to orch
 
 
 def ANEC_sub_photo_CA(
