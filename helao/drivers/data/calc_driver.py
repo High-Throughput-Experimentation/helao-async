7f17fd72c3a3c69ff5e490479e05195c7c5e3a8e
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-03-21 17:11:44 -0700

    feat: interlen arrays include time axis

--- helao/drivers/data/calc_driver.py
+++ helao/drivers/data/calc_driver.py
@@ -16,18 +16,6 @@ def handlenan_savgol_filter(
     d_arr, window_length, polyorder, delta=1.0, deriv=0, replacenan_value=0.1
 ):
     """Custom savgol_filter from JCAPDataProcess uvis_basics.py, updated for array ops."""
-    nans = np.isnan(d_arr)
-    xarr = np.arange(len(d_arr))
-    if len(nans) > 1 and len(nans) < len(d_arr):
-        d_arr[nans] = interp(xarr[nans], xarr[~nans], d_arr[~nans])
-        naninds = np.where(np.isnan(d_arr))[0]
-        if len(naninds) > 1:
-            d_arr[naninds] = np.array(
-                [
-                    np.nanmean(d_arr[max(0, nind - 3) : min(nind + 3, len(d_arr))])
-                    for nind in naninds
-                ]
-            )
     try:
         return savgol_filter(d_arr, window_length, polyorder, delta=delta, deriv=deriv)
     except Exception:
@@ -41,21 +29,35 @@ def refadjust(v, min_mthd_allowed, max_mthd_allowed, min_limit, max_limit):
     """Normalization func from JCAPDataProcess uvis_basics.py, updated for array ops."""
     w = copy(v)
     min_rescaled = np.bitwise_and(
-        (w.min(axis=1) >= min_mthd_allowed),
-        (w.min(axis=1) < min_limit),
+        (w.min(axis=-1) >= min_mthd_allowed),
+        (w.min(axis=-1) < min_limit),
     )
-    w[min_rescaled, :] = (
-        w[min_rescaled, :] - w[min_rescaled, :].min(axis=1).reshape(-1, 1) + min_limit
+    w[min_rescaled] = (
+        w[min_rescaled] - w[min_rescaled].min(axis=-1).reshape(-1, 1) + min_limit
     )
     max_rescaled = np.bitwise_and(
-        (w.max(axis=1) <= max_mthd_allowed),
-        (w.max(axis=1) >= max_limit),
+        (w.max(axis=-1) <= max_mthd_allowed),
+        (w.max(axis=-1) >= max_limit),
     )
-    w[max_rescaled, :] = w[max_rescaled, :] / (
-        w[max_rescaled, :].max(axis=1).reshape(-1, 1) + 0.02
+    w[max_rescaled] = w[max_rescaled] / (
+        w[max_rescaled].max(axis=-1).reshape(-1, 1) + 0.02
     )
 
     return min_rescaled, max_rescaled, w
+
+
+def squeeze_foms(d):
+    sd = {}
+    for k, v in d.items():
+        if isinstance(v, np.ndarray):
+            if len(v.shape) > 1:
+                sv = v.mean(axis=1).tolist()
+            else:
+                sv = v.tolist()
+        else:
+            sv = v
+        sd[k] = sv
+    return sd
 
 
 class Calc:
@@ -159,6 +161,7 @@ class Calc:
                 for dk in list(rud[rk].keys()):
                     rud["technique_name"] = rud[rk][dk]["actd"]["technique_name"]
                     data = rud[rk][dk]["data"]
+                    epochs = data["epoch_s"]
                     vals = [
                         data[chk]
                         for chk in sorted(
@@ -172,6 +175,7 @@ class Calc:
                             "raw": arr,
                             "median": np.median(arr, axis=0),
                             "mean": arr.mean(axis=0),
+                            "epoch": epochs,
                         }
                     )
 
@@ -230,6 +234,7 @@ class Calc:
                 smplist = []
                 wllist = []
                 bsnlist = []
+                epochlist = []
 
                 for d in rud["data"].values():
                     actd = d["actd"]
@@ -259,7 +264,7 @@ class Calc:
                         smplist.append(solid_smp)
                         wllist.append(d["meta"]["optional"]["wl"])
                         actuuids.append(actd["action_uuid"])
-                        bsnlist.append((d["mean"] - mindark) / (maxlight - mindark))
+                        bsnlist.append((d["raw"] - mindark) / (maxlight - mindark))
 
                 wlarr = np.array(wllist).mean(axis=0)
                 specd[spec] = {
@@ -267,6 +272,7 @@ class Calc:
                     "action_uuids": actuuids,
                     "bsnlist": bsnlist,
                     "wlarr": wlarr,
+                    "epoch": epochlist,
                     "technique": rud["technique_name"],
                 }
 
@@ -302,7 +308,12 @@ class Calc:
             wl = np.array(sd["wlarr"])
             wlmask = np.where((wl > params["lower_wl"]) & (wl < params["upper_wl"]))[0]
             pred[k]["full"]["wl"] = wl[wlmask]
-            pred[k]["full"]["sig"] = np.array(sd["bsnlist"])[:, wlmask]
+
+            shortest = min([x.shape[0] for x in sd["bsnlist"]])
+            trunc_bsnlist = [x[:shortest] for x in sd["bsnlist"]]
+            trunc_epoch = [x[:shortest] for x in sd["epoch"]]
+            pred[k]["full"]["sig"] = np.array(trunc_bsnlist)[:, :, wlmask]
+            pred[k]["full"]["epoch"] = np.array(trunc_epoch)
             binds = [
                 [
                     y
@@ -315,6 +326,7 @@ class Calc:
             rsi = [np.median(inds).astype(int) for inds in pred[k]["binds"]]
             pred[k]["rsi"] = rsi  # raw indices from full vector
             pred[k]["bin"]["wl"] = pred["T"]["full"]["wl"][rsi]  # center index of bin
+            pred[k]["bin"]["epoch"] = pred[k]["full"]["epoch"]
             hv = [
                 1239.8 / x for x in pred[k]["bin"]["wl"]
             ]  # convert binned wl[nm] to energy[eV]
@@ -339,9 +351,9 @@ class Calc:
             smplist = specd["T"]["smplist"]
             smpT = np.array(smplist)
             asT = np.argsort(smpT)
-            arrTR = pred["T"]["full"]["sig"] / (1 - pred["R"]["full"]["sig"][asT, :])
+            arrTR = pred["T"]["full"]["sig"] / (1 - pred["R"]["full"]["sig"][asT, :, :])
             pred["TR"]["full"]["sig"] = arrTR
-            omTR = 1 - pred["T"]["full"]["sig"] - pred["R"]["full"]["sig"][asT, :]
+            omTR = 1 - pred["T"]["full"]["sig"] - pred["R"]["full"]["sig"][asT, :, :]
             omT = 1 - pred["T"]["full"]["sig"]
             pred["TR"]["full"]["omTR"] = omTR
             pred["TR"]["full"]["omT"] = omT
@@ -366,8 +378,12 @@ class Calc:
             # bin full arrays that haven't been binned
             for k, arr in pred[sk]["full"].items():
                 if k not in pred[sk]["bin"].keys():
-                    pred[sk]["bin"][k] = [arr[:, inds].mean(axis=1) for inds in binds]
-                    pred[sk]["bin"][k] = np.array(pred[sk]["bin"][k]).transpose()
+                    pred[sk]["bin"][k] = [
+                        arr[:, :, inds].mean(axis=-1) for inds in binds
+                    ]
+                    pred[sk]["bin"][k] = np.array(pred[sk]["bin"][k]).transpose(
+                        (1, 2, 0)
+                    )
             # smooth binned arrays that haven't been smoothed
             for k, arr in pred[sk]["bin"].items():
                 if (k != "abs" or sk == "T") and k not in pred[sk]["smooth"].keys():
@@ -474,30 +490,30 @@ class Calc:
                 lo, hi = evp[i], evp[i + 1]
                 evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
                 datadict[f"1-T-R_av_{lo}_{hi}"] = interd["smooth"]["omTR"][
-                    :, evrange
-                ].mean(axis=1)
+                    :, :, evrange
+                ].mean(axis=-1)
                 datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][
-                    :, evrange
-                ].mean(axis=1)
+                    :, :, evrange
+                ].mean(axis=-1)
             # full range
             lo, hi = evp[0], evp[-1]
             evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
-            datadict[f"1-T-R_av_{lo}_{hi}"] = interd["smooth"]["omTR"][:, evrange].mean(
-                axis=1
-            )
-            datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][:, evrange].mean(
-                axis=1
+            datadict[f"1-T-R_av_{lo}_{hi}"] = interd["smooth"]["omTR"][
+                :, :, evrange
+            ].mean(axis=-1)
+            datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][:, :, evrange].mean(
+                axis=-1
             )
             datadict["TplusR_0to1"] = np.bitwise_and(
                 (interd["smooth"]["omTR"] > 0.0), (interd["smooth"]["omTR"] < 1.0)
-            ).all(axis=1)
+            ).all(axis=-1)
 
             datadict["T_0to1"] = np.bitwise_and(
                 (pred["T"]["smooth"]["sig"] > 0.0), (pred["T"]["smooth"]["sig"] < 1.0)
-            ).all(axis=1)
+            ).all(axis=-1)
             datadict["R_0to1"] = np.bitwise_and(
                 (pred["R"]["smooth"]["sig"] > 0.0), (pred["R"]["smooth"]["sig"] < 1.0)
-            ).all(axis=1)
+            ).all(axis=-1)
 
         elif specd.get("T", {}) and not specd.get("R", {}):  # T_UVVIS only
             interd = pred["T"]
@@ -506,50 +522,51 @@ class Calc:
                 lo, hi = evp[i], evp[i + 1]
                 evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
                 datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][
-                    :, evrange
-                ].mean(axis=1)
+                    :, :, evrange
+                ].mean(axis=-1)
             # full range
             lo, hi = evp[0], evp[-1]
             evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
-            datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][:, evrange].mean(
-                axis=1
+            datadict[f"1-T_av_{lo}_{hi}"] = interd["smooth"]["omT"][:, :, evrange].mean(
+                axis=-1
             )
             datadict["T_0to1"] = np.bitwise_and(
                 (pred["T"]["smooth"]["sig"] > 0.0), (pred["T"]["smooth"]["sig"] < 1.0)
-            ).all(axis=1)
+            ).all(axis=-1)
 
         elif specd.get("R", {}) and not specd.get("T", {}):  # DR_UVVIS only
             interd = pred["R"]
             datadict["DR_0to1"] = np.bitwise_and(
                 (pred["R"]["smooth"]["sig"] > 0.0), (pred["R"]["smooth"]["sig"] < 1.0)
-            ).all(axis=1)
+            ).all(axis=-1)
 
-        datadict["max_abs"] = np.nanmax(interd["smooth_refadj"]["abs"], axis=1)
+        datadict["max_abs"] = np.nanmax(interd["smooth_refadj"]["abs"], axis=-1)
         checknanrange = np.bitwise_and(
             (interd["bin"]["wl"] >= 410), (interd["bin"]["wl"] <= 850)
         )
         datadict["abs_hasnan"] = np.any(
-            np.isnan(interd["smooth_refadj"]["abs"][:, checknanrange]), axis=1
+            np.isnan(interd["smooth_refadj"]["abs"][:, :, checknanrange]), axis=-1
         )
+
         evp = params["ev_parts"]
         for i in range(len(evp) - 1):
             lo, hi = evp[i], evp[i + 1]
             evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
             datadict[f"abs_{lo}_{hi}"] = -1 * np.trapz(
-                interd["smooth_refadj"]["abs"][:, evrange], x=interd["hv"][evrange]
+                interd["smooth_refadj"]["abs"][:, :, evrange], x=interd["hv"][evrange]
             )
         # full range
         lo, hi = evp[0], evp[-1]
         evrange = np.bitwise_and((interd["hv"] > lo), (interd["hv"] < hi))
         datadict[f"abs_{lo}_{hi}"] = -1 * np.trapz(
-            interd["smooth_refadj"]["abs"][:, evrange], x=interd["hv"][evrange]
+            interd["smooth_refadj"]["abs"][:, :, evrange], x=interd["hv"][evrange]
         )
 
         datadict["max_abs2ndderiv"] = np.nanmax(
-            interd["smooth_refadj_scl"]["abs_2ndderiv"], axis=1
+            interd["smooth_refadj_scl"]["abs_2ndderiv"], axis=-1
         )
         datadict["min_abs1stderiv"] = np.nanmin(
-            interd["smooth_refadj_scl"]["abs_1stderiv"], axis=1
+            interd["smooth_refadj_scl"]["abs_1stderiv"], axis=-1
         )
 
         for z in ("DA", "IA", "DF", "IF"):
@@ -568,7 +585,7 @@ class Calc:
                 )
                 / sdx
             )
-            datadict[f"{z}_minslope"] = np.nanmin(slope, axis=1)
+            datadict[f"{z}_minslope"] = np.nanmin(slope, axis=-1)
 
         datadict["sample_label"] = smplist
         datadict["min_rescaled"] = interd["min_rescaled"]
@@ -599,6 +616,7 @@ class Calc:
             "smooth_refadj_scl": "interlen_smoothrefadjscl",
         }
         keymap = {"omTR": "one_minus_T_minus_R", "omT": "one_minus_T", "abs": "abs"}
+
         for sk, sd in pred.items():
             for bk, bd in sd.items():
                 if not isinstance(bd, dict):
@@ -614,6 +632,7 @@ class Calc:
                     ad = {
                         "sample_label": specd[uk]["smplist"],
                         "action_uuids": specd[uk]["action_uuids"],
+                        "epoch": sd["bin"]["epoch"].tolist(),
                         "wavelength": sd["bin"]["wl"].tolist()
                         if bk.startswith("smooth")
                         else bd["wl"].tolist(),
@@ -621,10 +640,7 @@ class Calc:
                     }
                     arraydict[f"{arrayname}"] = ad
 
-        datadict = {
-            k: v.tolist() if isinstance(v, np.ndarray) else v
-            for k, v in datadict.items()
-        }
+        datadict = squeeze_foms(datadict)
 
         return datadict, arraydict
 
