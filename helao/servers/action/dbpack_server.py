09ff270014619094393e0717f58d27be1ebecbeb
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-03-27 15:18:59 -0700

    feat: switch DB server to HelaoSyncer class

--- helao/servers/action/dbpack_server.py
+++ helao/servers/action/dbpack_server.py
@@ -8,7 +8,7 @@ Finished actions which do not contribute process information are pushed to
 __all__ = ["makeApp"]
 
 from helao.servers.base import HelaoBase
-from helao.drivers.data.dbpack_driver import DBPack
+from helao.drivers.data.sync_driver import HelaoSyncer
 from helao.helpers.config_loader import config_loader
 
 
@@ -22,22 +22,22 @@ def makeApp(confPrefix, server_key, helao_root):
         server_title=server_key,
         description="Data packaging server",
         version=0.1,
-        driver_class=DBPack,
+        driver_class=HelaoSyncer,
     )
 
-    @app.post(f"/finish_yml", tags=["private"])
-    async def finish_yml(yml_path: str, timeout: int = 300):
-        await app.driver.add_yml_task(yml_path, timeout)
+    @app.post("/finish_yml", tags=["private"])
+    async def finish_yml(yml_path: str):
+        await app.driver.enqueue_yml(yml_path)
         return yml_path
 
-    @app.post(f"/finish_pending", tags=["private"])
-    async def finish_pending():
-        pending_dict = await app.driver.finish_pending()
-        return pending_dict
+    # @app.post("/finish_pending", tags=["private"])
+    # async def finish_pending():
+    #     pending_dict = await app.driver.finish_pending()
+    #     return pending_dict
 
-    @app.post(f"/list_pending", tags=["private"])
-    def list_pending():
-        pending_dict = app.driver.list_pending()
-        return pending_dict
+    # @app.post("/list_pending", tags=["private"])
+    # def list_pending():
+    #     pending_dict = app.driver.list_pending()
+    #     return pending_dict
         
     return app
