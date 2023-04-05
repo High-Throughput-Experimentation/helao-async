f94f85b434775717796dc989e8038e26a7bd9a00
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-04-05 11:24:45 -0700

    fix: insert empty tray in anec config

--- helao/configs/anec.py
+++ helao/configs/anec.py
@@ -118,6 +118,7 @@ config["servers"] = {
                 "archive": "HELAO_GCHeadspace_Liquid_Archive_220215.cam",
             },
             "positions": {
+                "tray1": {"slot1": None, "slot2": "VT54", "slot3": "VT54"},
                 "tray2": {"slot1": "VT54", "slot2": "VT54", "slot3": "VT54"},
                 "custom": {
                     "cell1_we": "cell",
