f6f318b62930d272cd97ca8b67a04f2c6db93313
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-04-28 15:22:00 -0700

    feat: TEC server and polling executor

--- helao/drivers/temperature_control/mecom_driver.py
+++ helao/drivers/temperature_control/mecom_driver.py
@@ -4,7 +4,6 @@ import time
 import asyncio
 import logging
 from mecom import MeCom, ResponseException, WrongChecksum
-from serial import SerialException
 
 from helaocore.error import ErrorCodes
 from helaocore.models.hlostatus import HloStatus
@@ -12,23 +11,23 @@ from helao.servers.base import Base, Executor
 
 # default queries from command table below
 DEFAULT_QUERIES = [
-    "loop status",
-    "object temperature",
-    "target object temperature",
-    "output current",
-    "output voltage",
+    "enabled_status",
+    "object_temperature",
+    "target_object_temperature",
 ]
 
 # syntax
 # { display_name: [parameter_id, unit], }
 COMMAND_TABLE = {
-    "loop status": [1200, ""],
-    "object temperature": [1000, "degC"],
-    "target object temperature": [1010, "degC"],
-    "output current": [1020, "A"],
-    "output voltage": [1021, "V"],
-    "sink temperature": [1001, "degC"],
-    "ramp temperature": [1011, "degC"],
+    "device_status": [104, ""],
+    "enabled_status": [2010, ""],
+    "temperature_is_stable": [1200, ""],
+    "object_temperature": [1000, "degC"],
+    "target_object_temperature": [1010, "degC"],
+    "output_current": [1020, "A"],
+    "output_voltage": [1021, "V"],
+    "sink_temperature": [1001, "degC"],
+    "ramp_temperature": [1011, "degC"],
 }
 
 
@@ -127,24 +126,13 @@ class MeerstetterTEC(object):
     def disable(self):
         return self._set_enable(False)
 
-    async def poll_sensor_loop(self, frequency: int = 2):
+    async def poll_sensor_loop(self, frequency: int = 1):
         waittime = 1.0 / frequency
         self.base.print_message("Starting polling loop")
         while True:
-            try:
-                o2_level = self.inst.read_register(1, functioncode=4) * 10
-            except minimalmodbus.NoResponseError as err:
-                self.base.print_message(
-                    f"NoResponseError: Driver polling rate is too fast. {err}"
-                )
-                continue
-            except serial.SerialException as err:
-                self.base.print_message(
-                    f"Device {self.config_dict['device']} is in use. {err}"
-                )
-                continue
-            if o2_level:
-                msg_dict = {"o2_ppm": int(o2_level)}
+            tec_vals = self.get_data()
+            if tec_vals:
+                msg_dict = {"tec_vals": tec_vals}
                 await self.base.put_lbuf(msg_dict)
             await asyncio.sleep(waittime)
 
@@ -152,57 +140,24 @@ class MeerstetterTEC(object):
         try:
             self.polling_task.cancel()
         except asyncio.CancelledError:
-            self.base.print_message("closed sensor polling loop task")
-        try:
-            self.recording_task.cancel()
-        except asyncio.CancelledError:
-            self.base.print_message("closed sensor recording loop task")
-        self.inst.serial.close()
+            self.base.print_message("closed TEC polling loop task")
+        self.disable()
 
 
-class O2MonExec(Executor):
+class TECMonExec(Executor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.active.base.print_message("O2MonExec initialized.")
+        self.active.base.print_message("TECMonExec initialized.")
         self.start_time = time.time()
         self.duration = self.active.action.action_params.get("duration", -1)
 
     async def _poll(self):
-        """Read O2 ppm from live buffer."""
+        """Read TEC values from live buffer."""
         live_dict = {}
-        o2_ppm, epoch_s = self.active.base.get_lbuf("o2_ppm")
-        live_dict["co2_ppm"] = o2_ppm
+        tec_vals, epoch_s = self.active.base.get_lbuf("tec_vals")
         live_dict["epoch_s"] = epoch_s
-        iter_time = time.time()
-        elapsed_time = iter_time - self.start_time
-        if (self.duration < 0) or (elapsed_time < self.duration):
-            status = HloStatus.active
-        else:
-            status = HloStatus.finished
-        await asyncio.sleep(0.001)
-
-        return {
-            "error": ErrorCodes.none,
-            "status": status,
-            "data": live_dict,
-        }
-
-
-class O2MonExec(Executor):
-    """O2 recording action written as an executor."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.active.base.print_message("O2MonExec initialized.")
-        self.start_time = time.time()
-        self.duration = self.active.action.action_params.get("duration", -1)
-
-    async def _poll(self):
-        """Read O2 ppm from live buffer."""
-        live_dict = {}
-        o2_ppm, epoch_s = self.active.base.get_lbuf("o2_ppm")
-        live_dict["co2_ppm"] = o2_ppm
-        live_dict["epoch_s"] = epoch_s
+        for k, v in tec_vals.items():
+            live_dict[k] = v
         iter_time = time.time()
         elapsed_time = iter_time - self.start_time
         if (self.duration < 0) or (elapsed_time < self.duration):
