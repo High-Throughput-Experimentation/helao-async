da4dbba09b8d462ff6fa645f95a5db372dfc67d0
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-05-03 16:07:12 -0700

    fix: revert multisubscriber, requires py>3.10

--- helao/helpers/multisubscriber_queue.py
+++ helao/helpers/multisubscriber_queue.py
@@ -2,106 +2,103 @@ __all__ = ["MultisubscriberQueue"]
 
 
 from asyncio import Queue
-from contextlib import contextmanager
-from typing import Any, AsyncGenerator, Generator, Generic, TypeVar, cast
+from typing import Any
 
 
 # multisubscriber queue by Kyle Smith
 # https://github.com/smithk86/asyncio-multisubscriber-queue
-T = TypeVar("T")
+class MultisubscriberQueue:
+    def __init__(self, **kwargs):
+        """
+        The constructor for MultisubscriberQueue class
+        """
+        super().__init__()
+        self.subscribers = []
 
-
-class MultisubscriberQueue(Generic[T]):
-    """Allow a single producer to provide the same payload to multiple consumers simultaneously.
-    An `asyncio.Queue` can be obtained directly by calling MultisubscriberQueue.queue() or
-    `MultisubscriberQueue.subscribe()` can be used to yield data as it is available.
-    """
-
-    subscribers: set[Queue[T]]
-    _close_sentinel: T = cast(T, object())
-
-    __slots__ = ("subscribers",)
-
-    def __init__(self) -> None:
-        """The constructor for MultisubscriberQueue class."""
-        self.subscribers = {*()}
-
-    def __len__(self) -> int:
+    def __len__(self):
         return len(self.subscribers)
 
-    def __contains__(self, q: Queue[T]) -> bool:
+    def __contains__(self, q):
         return q in self.subscribers
 
-    async def subscribe(self) -> AsyncGenerator[T, None]:
-        """Subscribe to `MultisubscriberQueue` using an async generator.
+    async def subscribe(self):
+        """
+        Subscribe to data using an async generator
         Instead of working with the Queue directly, the client can
-        subscribe to data and have it yielded as it is available.
+        subscribe to data and have it yielded directly.
         Example:
-        ```
-        from asyncio_multisubscriber_queue import MultisubscriberQueue
-        # create a MultisubscriberQueue for distributing str instances
-        mqueue: MultisubscriberQueue[str] = MultisubscriberQueue()
-        async with mqueue.subscribe() as data:
-                first_item: str = await q.get()
-        ```
-        Returns:
-            AsyncGenerator containing subscriber data.
+            with MultisubscriberQueue.subscribe() as data:
+                print(data)
         """
-        with self.queue() as q:
+        with self.queue_context() as q:
             while True:
-                _data: Any = await q.get()
-                if _data is self._close_sentinel:
+                val = await q.get()
+                if val is StopAsyncIteration:
                     break
-                yield _data
+                else:
+                    yield val
 
-    def add(self) -> Queue[T]:
-        """Get a new Queue which is part of the subscriber set.
-        Returns:
-            A subscriber Queue.
+    def queue(self):
         """
-        queue: Queue[T] = Queue()
-        self.subscribers.add(queue)
-        return queue
-
-    def remove(self, queue: Queue[T]) -> None:
-        """Remove given queue from the subscriber set.
-        Args:
-            queue: Queue object to be removed from the subscriber set.
+        Get a new async Queue
         """
-        if queue in self.subscribers:
-            self.subscribers.remove(queue)
+        q = Queue()
+        self.subscribers.append(q)
+        return q
 
-    @contextmanager
-    def queue(self) -> Generator[Queue[T], None, None]:
-        """Context helper which manages the lifecycle of the subscriber Queue.
+    def queue_context(self):
+        """
+        Get a new queue context wrapper
+        The queue context wrapper allows the queue to be automatically removed
+        from the subscriber pool when the context is exited.
         Example:
-        ```
-        from asyncio import Queue
-        from asyncio_multisubscriber_queue import MultisubscriberQueue
-        # create a MultisubscriberQueue for distributing int instances
-        mqueue: MultisubscriberQueue[int] = MultisubscriberQueue()
-        with mqueue.queue() as q:
-            first_item: int = await q.get()
-        ```
-        Returns:
-            Generator containing a subscriber Queue.
+            with MultisubscriberQueue.queue_context() as q:
+                await q.get()
         """
-        _queue = self.add()
-        try:
-            yield _queue
-        finally:
-            self.remove(_queue)
+        return _QueueContext(self)
 
-    async def put(self, data: T) -> None:
-        """Put data on all the Queues in the subscriber set.
-        Args:
-            data: Data for the Queues.
+    def remove(self, q):
         """
-        for _queue in self.subscribers:
-            await _queue.put(data)
+        Remove queue from the pool of subscribers
+        """
+        if q in self.subscribers:
+            self.subscribers.remove(q)
+        else:
+            raise KeyError("subscriber queue does not exist")
 
-    async def close(self) -> None:
-        """Put the close sentinel on the Queues.
-        This is used to signal the end of the MultisubscriberQueue session.
+    async def put(self, data: Any):
         """
-        await self.put(self._close_sentinel)
+        Put new data on all subscriber queues
+        Parameters:
+            data: queue data
+        """
+        for q in self.subscribers:
+            await q.put(data)
+
+    def put_nowait(self, data: Any):
+        """
+        Put new data on all subscriber queues
+        Parameters:
+            data: queue data
+        """
+        for q in self.subscribers:
+            q.put_nowait(data)
+
+    async def close(self):
+        """
+        Force clients using MultisubscriberQueue.subscribe() to end iteration
+        """
+        await self.put(StopAsyncIteration)
+
+
+class _QueueContext:
+    def __init__(self, parent):
+        self.parent = parent
+        self.queue = None
+
+    def __enter__(self):
+        self.queue = self.parent.queue()
+        return self.queue
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.parent.remove(self.queue)
