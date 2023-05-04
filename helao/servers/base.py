db65a964d721eb79736caf854909b05f48a3cfb3
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-05-04 16:35:17 -0700

    fix: lock event loop when setting up output file

--- helao/servers/base.py
+++ helao/servers/base.py
@@ -350,6 +350,7 @@ class Base:
         self.ntp_response = None
         self.ntp_offset = None  # add to system time for correction
         self.ntp_last_sync = None
+        self.aiolock = asyncio.Lock()
 
         self.ntp_last_sync_file = None
         if self.helaodirs.root is not None:
@@ -1398,15 +1399,16 @@ class Active:
 
         self.base.print_message(f"writing data to: {output_file}")
         # create output file and set connection
-        self.file_conn_dict[file_conn_key].file = await aiofiles.open(
-            output_file, mode="a+"
-        )
+        async with self.base.aiolock:
+            self.file_conn_dict[file_conn_key].file = await aiofiles.open(
+                output_file, mode="a+"
+            )
 
-        if header:
-            self.base.print_message("adding header to new file")
-            if not header.endswith("\n"):
-                header += "\n"
-            await self.file_conn_dict[file_conn_key].file.write(header)
+            if header:
+                self.base.print_message("adding header to new file")
+                if not header.endswith("\n"):
+                    header += "\n"
+                await self.file_conn_dict[file_conn_key].file.write(header)
 
     async def log_data_task(self):
         """Self-subscribe to data queue, write to present file path."""
