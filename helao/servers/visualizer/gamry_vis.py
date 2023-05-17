3290526ed153a1cee291b3d6037e65ee5af70cc3
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-05-17 13:44:00 -0700

    wip: use Wss for gamry vis

--- helao/servers/visualizer/gamry_vis.py
+++ helao/servers/visualizer/gamry_vis.py
@@ -1,6 +1,7 @@
-import websockets
+import time
 import asyncio
 import json
+from datetime import datetime
 from functools import partial
 from uuid import UUID
 from copy import deepcopy
@@ -19,10 +20,20 @@ from bokeh.models import ColumnDataSource
 from helaocore.models.hlostatus import HloStatus
 from helaocore.models.data import DataPackageModel
 from helao.servers.vis import Vis
+from helao.helpers.ws_subscriber import WsSubscriber as Wss
 
-valid_data_status = (
+VALID_DATA_STATUS = (
     None,
     HloStatus.active,
+)
+
+VALID_ACTION_NAME = (
+    "run_LSV",
+    "run_CA",
+    "run_CP",
+    "run_CV",
+    "run_EIS",
+    "run_OCV",
 )
 
 
@@ -34,6 +45,8 @@ class C_potvis:
         self.config_dict = self.vis.server_cfg["params"]
         self.max_points = 500
         self.max_prev = 4
+        self.update_rate = 1e-3
+        self.last_update_time = time.time()
 
         self.potentiostat_key = serv_key
         potserv_config = self.vis.world_cfg["servers"].get(self.potentiostat_key, None)
@@ -41,21 +54,23 @@ class C_potvis:
             return
         potserv_host = potserv_config.get("host", None)
         potserv_port = potserv_config.get("port", None)
+        self.wss = Wss(potserv_host, potserv_port, "ws_data")
 
         self.data_url = (
             f"ws://{potserv_config['host']}:{potserv_config['port']}/ws_data"
         )
-        # self.stat_url = f"ws://{potserv_config["host"]}:{potserv_config["port"]}/ws_status"
 
         self.IOloop_data_run = False
         self.IOloop_stat_run = False
 
         self.data_dict_keys = ["t_s", "Ewe_V", "Ach_V", "I_A"]
-        self.data_dict = {key: [] for key in self.data_dict_keys}
-
-        self.datasource = ColumnDataSource(data=self.data_dict)
-        self.prev_datasources = {}
+        self.datasource = ColumnDataSource(
+            data={key: [] for key in self.data_dict_keys}
+        )
         self.cur_action_uuid = ""
+
+        # prev_datasources aren't streamed, replot when axis or action_uuid changes
+        self.prev_datasources = {}
         self.prev_action_uuid = ""
         self.prev_action_uuids = []
 
@@ -93,19 +108,17 @@ class C_potvis:
         )
 
         self.plot = figure(title="Title", height=300, width=500)
-
         self.plot_prev = figure(title="Title", height=300, width=500)
+
         # combine all sublayouts into a single one
+        docs_url = f"http://{potserv_host}:{potserv_port}/docs#/"
+        server_link = (
+            f'<a href="{docs_url}" target="_blank">\'{self.potentiostat_key}\'</a>'
+        )
+        headerbar = f"<b>Live vis module for server {server_link}</b>"
         self.layout = layout(
             [
-                [
-                    Spacer(width=20),
-                    Div(
-                        text=f'<b>Potentiostat Visualizer module for server <a href="http://{potserv_host}:{potserv_port}/docs#/" target="_blank">\'{self.potentiostat_key}\'</a></b>',
-                        width=1004,
-                        height=15,
-                    ),
-                ],
+                [Spacer(width=20), Div(text=headerbar, width=1004, height=15)],
                 [self.input_max_points, Spacer(width=20), self.input_max_prev],
                 [
                     Paragraph(text="""x-axis:""", width=500, height=15),
@@ -124,12 +137,11 @@ class C_potvis:
         self.xselect = self.xaxis_selector_group.active
         self.yselect = self.yaxis_selector_group.active
 
-        # self.reset_plot(self.cur_action_uuid, forceupdate=True)
-
         self.vis.doc.add_root(self.layout)
         self.vis.doc.add_root(Spacer(height=10))
         self.IOtask = asyncio.create_task(self.IOloop_data())
         self.vis.doc.on_session_destroyed(self.cleanup_session)
+        self.reset_plot(self.cur_action_uuid, forceupdate=True)
 
     def cleanup_session(self, session_context):
         self.vis.print_message(
@@ -194,71 +206,33 @@ class C_potvis:
     def update_input_value(self, sender, value):
         sender.value = value
 
-    def add_points(self, datapackage: DataPackageModel):
-        def _add_helper(datadict, pointlist):
-            for pt in pointlist:
-                datadict.append(pt)
+    def add_points(self, datapackage_list: list):
+        data_dict = {k: [] for k in self.data_dict_keys}
+        for data_msg in datapackage_list:
+            data_package = DataPackageModel(**data_msg)
 
-        self.reset_plot(str(datapackage.action_uuid))
-        if len(self.data_dict[self.data_dict_keys[0]]) > self.max_points:
-            delpts = len(self.data_dict[self.data_dict_keys[0]]) - self.max_points
-            for key in self.data_dict_keys:
-                del self.data_dict[key][:delpts]
-        for _, data_dict in datapackage.datamodel.data.items():
-            datalen = len(list(data_dict.values())[0])
-            for key in self.data_dict_keys:
-                _add_helper(
-                    datadict=self.data_dict[key],
-                    pointlist=data_dict.get(key, [0 for i in range(datalen)]),
-                )
+            # only resets if axis selector or action_uuid changes
+            self.reset_plot(data_package.action_uuid)
 
-        self.datasource.data = self.data_dict
+            if (
+                data_package.datamodel.status in VALID_DATA_STATUS
+                and data_package.action_name in VALID_ACTION_NAME
+            ):
+                for data_label, data_val in data_package.datamodel.data.items():
+                    if data_label in self.data_dict_keys:
+                        data_dict[data_label].append(data_val)
+            self.datasource.stream(data_dict, rollover=self.max_points)
 
     async def IOloop_data(self):  # non-blocking coroutine, updates data source
         self.vis.print_message(
             f" ... potentiostat visualizer subscribing to: {self.data_url}"
         )
-        retry_limit = 5
-        for _ in range(retry_limit):
-            try:
-                async with websockets.connect(self.data_url) as ws:
-                    self.IOloop_data_run = True
-                    while self.IOloop_data_run:
-                        try:
-                            datapackage = DataPackageModel(
-                                **json.loads(await ws.recv())
-                            )
-                            datastatus = datapackage.datamodel.status
-                            if (
-                                datapackage.action_name
-                                in (
-                                    "run_LSV",
-                                    "run_CA",
-                                    "run_CP",
-                                    "run_CV",
-                                    "run_EIS",
-                                    "run_OCV",
-                                )
-                                and datastatus in valid_data_status
-                            ):
-                                self.vis.doc.add_next_tick_callback(
-                                    partial(self.add_points, datapackage)
-                                )
-                        except Exception:
-                            self.IOloop_data_run = False
-                    await ws.close()
-                    self.IOloop_data_run = False
-            except Exception:
-                self.vis.print_message(
-                    f"failed to subscribe to "
-                    f"{self.data_url}"
-                    "trying again in 1sec",
-                    info=True,
-                )
-                await asyncio.sleep(1)
-            if not self.IOloop_data_run:
-                self.vis.print_message("IOloop closed", info=True)
-                break
+        while True:
+            if time.time() - self.last_update_time >= self.update_rate:
+                messages = await self.wss.read_messages()
+                self.vis.doc.add_next_tick_callback(partial(self.add_points, messages))
+                self.last_update_time = time.time()
+            await asyncio.sleep(0.001)
 
     def _add_plots(self):
         # clear legend
@@ -286,37 +260,33 @@ class C_potvis:
             legend_label=ystr,
         )
         for i, puuid in enumerate(self.prev_action_uuids):
-            if puuid != "":
-                self.vis.print_message(f"Adding {puuid} to previous plots")
-                self.plot_prev.line(
-                    x=xstr,
-                    y=ystr,
-                    line_color=colors[i % len(colors)],
-                    source=self.prev_datasources[puuid],
-                    name=puuid,
-                    legend_label=puuid.split("-")[0],
-                )
-
-    def reset_plot(self, new_action_uuid: UUID, forceupdate: bool = False):
-        if (new_action_uuid != self.cur_action_uuid) or forceupdate:
-            self.vis.print_message(" ... reseting Gamry graph")
-            self.prev_action_uuid = self.cur_action_uuid
-            self.cur_action_uuid = new_action_uuid
-            self.prev_action_uuids.append(self.prev_action_uuid)
-            self.vis.print_message(f"previous uuids: {self.prev_action_uuids}")
-
-            # copy old data to "prev" plot
-            self.prev_datasources[self.prev_action_uuid] = ColumnDataSource(
-                data=deepcopy(self.datasource.data)
+            self.vis.print_message(f"Adding {puuid} to previous plots")
+            self.plot_prev.line(
+                x=xstr,
+                y=ystr,
+                line_color=colors[i % len(colors)],
+                source=self.prev_datasources[puuid],
+                name=puuid,
+                legend_label=puuid.split("-")[0],
             )
+
+    def reset_plot(self, new_action_uuid=None, forceupdate: bool = False):
+        if (new_action_uuid is not None) or forceupdate:
+            self.vis.print_message(" ... reseting Gamry graph")
+            if self.cur_action_uuid not in ["", new_action_uuid]:
+                self.prev_action_uuid = self.cur_action_uuid
+                self.cur_action_uuid = new_action_uuid
+                self.prev_action_uuids.append(self.prev_action_uuid)
+                self.vis.print_message(f"previous uuids: {self.prev_action_uuids}")
+                # copy old data to "prev" plot
+                self.prev_datasources[self.prev_action_uuid] = ColumnDataSource(
+                    data=deepcopy(self.datasource.data)
+                )
             while len(self.prev_action_uuids) > self.max_prev:
                 rp = self.prev_action_uuids.pop(0)
                 self.prev_datasources.pop(rp)
-            self.data_dict = {key: [] for key in self.data_dict_keys}
-            self.datasource.data = self.data_dict
-
+            self.datasource.data = {key: [] for key in self.data_dict_keys}
             self._add_plots()
-
         elif (self.xselect != self.xaxis_selector_group.active) or (
             self.yselect != self.yaxis_selector_group.active
         ):
