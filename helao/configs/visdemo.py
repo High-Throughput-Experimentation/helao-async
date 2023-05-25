ab3ee47fb28ee973ac87b6a06cd327d5223c08aa
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-05-25 13:13:41 -0700

    wip: setup visdemo config for processes

--- helao/configs/visdemo.py
+++ helao/configs/visdemo.py
@@ -4,10 +4,10 @@ config["dummy"] = True
 config["simulation"] = True
 
 # action library provides generator functions which produce actions
-config["experiment_libraries"] = ["simulate_exp"]
-config["sequence_libraries"] = []
+config["experiment_libraries"] = ["simulatews_exp"]
+config["sequence_libraries"] = ["TEST_seq"]
 config["run_type"] = "simulation"
-config["root"] = "/mnt/STORAGE/INST_hlo"  # software log and run files saved here
+# config["root"] = "/mnt/STORAGE/INST_hlo"  # software log and run files saved here
 config["root"] = "c:/INST_hlo"  # software log and run files saved here
 
 
