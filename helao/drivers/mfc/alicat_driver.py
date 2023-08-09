765d287371d27e23fd9ffbc744bde6746806b0be
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-08-09 15:06:01 -0700

    fix: close valve on shutdown

--- helao/drivers/mfc/alicat_driver.py
+++ helao/drivers/mfc/alicat_driver.py
@@ -249,6 +249,7 @@ class AliCatMFC:
         if device_name is None:
             resp = []
             for dev_name, _ in self.fcs.items():
+                await self.set_flowrate(dev_name, 0)
                 chold_resp = self._send(dev_name, "hc")
                 resp.append({dev_name: chold_resp})
         else:
@@ -313,6 +314,13 @@ class AliCatMFC:
         """Await tasks prior to driver shutdown."""
         await self.stop_polling()
         await asyncio.sleep(0.5)
+        self.base.print_message("stopping MFC flows")
+        await self.hold_valve_closed()
+
+    async def estop(self, *args, **kwargs):
+        self.base.print_message("stopping MFC flows")
+        await self.hold_valve_closed()
+        return True
 
     def shutdown(self):
         # this gets called when the server is shut down or reloaded to ensure a clean
