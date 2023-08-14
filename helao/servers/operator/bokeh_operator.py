372217c9e8d9523a6577bd1726a6471d510085dc
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-08-14 10:38:14 -0700

    fix: adjust button widths

--- helao/servers/operator/bokeh_operator.py
+++ helao/servers/operator/bokeh_operator.py
@@ -244,31 +244,31 @@ class Operator:
 
         if self.orch.step_thru_actions:
             self.orch_stepact_button = Button(
-                label="STEP-THRU actions", button_type="danger", width=70
+                label="STEP-THRU actions", button_type="danger", width=150
             )
         else:
             self.orch_stepact_button = Button(
-                label="RUN-THRU actions", button_type="success", width=70
+                label="RUN-THRU actions", button_type="success", width=150
             )
         self.orch_stepact_button.on_event(ButtonClick, self.callback_toggle_stepact)
 
         if self.orch.step_thru_experiments:
             self.orch_stepexp_button = Button(
-                label="STEP-THRU experiments", button_type="danger", width=70
+                label="STEP-THRU experiments", button_type="danger", width=150
             )
         else:
             self.orch_stepexp_button = Button(
-                label="RUN-THRU experiments", button_type="success", width=70
+                label="RUN-THRU experiments", button_type="success", width=150
             )
         self.orch_stepexp_button.on_event(ButtonClick, self.callback_toggle_stepexp)
 
         if self.orch.step_thru_experiments:
             self.orch_stepseq_button = Button(
-                label="STEP-THRU sequences", button_type="danger", width=70
+                label="STEP-THRU sequences", button_type="danger", width=150
             )
         else:
             self.orch_stepseq_button = Button(
-                label="RUN-THRU sequences", button_type="success", width=70
+                label="RUN-THRU sequences", button_type="success", width=150
             )
         self.orch_stepseq_button.on_event(ButtonClick, self.callback_toggle_stepseq)
 
