cd3fa5c055e929e81cd68a0a6e7a42e7da5f604f
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-08-22 12:31:07 -0700

    fix: increase delays between launch and start

--- helao/demos/multi_orch_demo.bat
+++ helao/demos/multi_orch_demo.bat
@@ -1,9 +1,9 @@
 @echo off
 call conda activate helao
 start %windir%\System32\cmd.exe /k "cd %dp0&cd..&cd..&helao.bat demo0"
-%windir%\System32\timeout.exe 15 /nobreak
+%windir%\System32\timeout.exe 30 /nobreak
 python multi_orch_demo_helper.py demo0
 %windir%\System32\timeout.exe 15 /nobreak
 start %windir%\System32\cmd.exe /k "cd %dp0&cd..&cd..&helao.bat demo1"
-%windir%\System32\timeout /t 15 /nobreak
+%windir%\System32\timeout /t 30 /nobreak
 python multi_orch_demo_helper.py demo1
