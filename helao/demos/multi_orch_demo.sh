e486af756ac6a522942fe8d1afc7c81a96191062
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-09-12 15:48:54 -0700

    fix: demo launcher args

--- helao/demos/multi_orch_demo.sh
+++ helao/demos/multi_orch_demo.sh
@@ -3,14 +3,13 @@ TERMGUI=/usr/bin/alacritty
 CONDA_DIR=$(echo $CONDA_EXE | xargs -0 dirname | xargs -0 dirname)
 source $CONDA_DIR/etc/profile.d/conda.sh
 conda activate helao
-
-$TERMGUI -e "$HELAOLAUNCH demo0 actionvis"
+$TERMGUI -e $HELAOLAUNCH demo0 actionvis &
 sleep 35
 python multi_orch_demo_helper.py demo0 50
 sleep 30 
-start %TERMGUI% /k "$HELAOLAUNCH demo0 gpvis"
+$TERMGUI -e $HELAOLAUNCH demo0 gpvis &
 sleep 30 
-start %TERMGUI% /k "$HELAOLAUNCH demo1"
+$TERMGUI -e $HELAOLAUNCH demo1 &
 sleep 30 
 python multi_orch_demo_helper.py demo1 5
 sleep 300 
