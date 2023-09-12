4662cf6fcec99dffb382ec5a65841503bb5e51fc
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-09-12 16:16:27 -0700

    fix: surround cmd.exe args

--- helao/demos/multi_orch_demo.bat
+++ helao/demos/multi_orch_demo.bat
@@ -2,13 +2,13 @@
 set TERMGUI=%windir%\System32\cmd.exe
 set SLEEPCMD=%windir%\System32\timeout.exe
 call conda activate helao
-start %TERMGUI% /k %HELAOLAUNCHER% demo0 actionvis
+start %TERMGUI% /k ("%HELAOLAUNCH%" "demo0" "actionvis")
 %SLEEPCMD% /t 35 /nobreak
 python multi_orch_demo_helper.py demo0 50
 %SLEEPCMD% /t 30 /nobreak
-start %TERMGUI% /k %HELAOLAUNCHER% demo0 gpvis
+start %TERMGUI% /k ("%HELAOLAUNCH%" "demo0" "gpvis")
 %SLEEPCMD% /t 30 /nobreak
-start %TERMGUI% /k %HELAOLAUNCHER% demo1
+start %TERMGUI% /k ("%HELAOLAUNCH%" "demo1")
 %SLEEPCMD% /t 30 /nobreak
 python multi_orch_demo_helper.py demo1 5
 %SLEEPCMD% /t 300 /nobreak
