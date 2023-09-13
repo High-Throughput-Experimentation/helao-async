444123121f5af2082591e2681441c73001374b1f
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-09-12 18:11:54 -0700

    feat: support yml configs

--- helao/helpers/config_loader.py
+++ helao/helpers/config_loader.py
@@ -1,11 +1,13 @@
 __all__ = ["config_loader"]
 
 import os
+from pathlib import Path
 from importlib.util import spec_from_file_location
 from importlib.util import module_from_spec
 from importlib.machinery import SourceFileLoader
 
 from helao.helpers.print_message import print_message
+from helao.helpers.yml_tools import yml_load
 
 
 def config_loader(confArg, helao_root):
@@ -17,24 +19,36 @@ def config_loader(confArg, helao_root):
         conf_spec.loader.exec_module(conf_mod)
         config = conf_mod.config
         full_path = os.path.abspath(confArg)
-    elif confArg.endswith(".py") and not os.path.exists(confArg):
+    elif confArg.endswith(".yml") and os.path.exists(confArg):
+        print_message({}, "launcher", f"Loading config from {confArg}", info=True)
+        config = yml_load(Path(confArg))
+        full_path = os.path.abspath(confArg)
+    elif (confArg.endswith(".py") or confArg.endswith(".yml")) and not os.path.exists(confArg):
         print_message({}, "launcher", f"Config not found at {os.path.abspath(confArg)}", error=True)
-        raise FileNotFoundError("Launcher argument ends with .py, expected path not found.")
+        raise FileNotFoundError("Config argument ends with .py or .yml but expected path not found.")
     else:
+        yml_path = os.path.join(helao_root, "helao", "configs", f"{confPrefix}.yml")
+        py_path = yml_path.replace(".yml", ".py")
+        if os.path.exists(yml_path):
+            full_path = yml_path
+            config = yml_load(Path(yml_path))
+        elif os.path.exists(py_path):
+            full_path = py_path
+            config = (
+                SourceFileLoader(
+                    "config",
+                    full_path,
+                )
+                .load_module()
+                .config
+            )
+        else:
+            raise FileNotFoundError("Config argument was a prefix but .py or .yml could not be found.")
         print_message(
             {},
             "launcher",
-            f"Loading config from helao/configs/{confPrefix}.py",
+            f"Loading config from {full_path}",
             info=True,
-        )
-        full_path = os.path.join(helao_root, "helao", "configs", f"{confPrefix}.py")
-        config = (
-            SourceFileLoader(
-                "config",
-                full_path,
-            )
-            .load_module()
-            .config
         )
     config["loaded_config_path"] = full_path
     return config
