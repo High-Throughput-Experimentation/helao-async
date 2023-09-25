95e7389fa178d3ca65cfe41ce73e4abd1ec39a4d
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-09-25 14:47:30 -0700

    fix: get server params

--- helao/servers/action/mfc_server.py
+++ helao/servers/action/mfc_server.py
@@ -17,10 +17,10 @@ from helao.helpers.make_str_enum import make_str_enum
 
 async def mfc_dyn_endpoints(app=None):
     server_key = app.base.server.server_name
-    co2_sensor_key = app.base.server_cfg.get("co2_server_name", None)
-    app.base.print_message(app.base.server_cfg)
+    co2_sensor_key = app.base.server_cfg["params"].get("co2_server_name", None)
+    app.base.print_message(app.base.server_cfg["params"])
     app.base.print_message(f"co2_server_name set to: {co2_sensor_key}")
-    dev_names = list(app.base.server_cfg.get("devices", {}).keys())
+    dev_names = list(app.base.server_cfg["params"].get("devices", {}).keys())
     dev_mfcs = make_str_enum("dev_mfcs", {k: k for k in dev_names})
     
     if co2_sensor_key in app.helao_cfg["servers"]:
