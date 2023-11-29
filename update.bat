e6e3182417b0de4b5625bb16009e8c9623ff8519
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-11-29 13:36:24 -0800

    feat: remove unstable branch before pulling update

--- update.bat
+++ update.bat
@@ -1,10 +1,14 @@
 @echo off
 call conda activate helao
 cd ..\helao-core
+git switch main
+git branch -D unstable
 git fetch --prune
 git reset --hard
 git pull
 cd ..\helao-async
+git switch main
+git switch -D unstable
 git fetch --prune
 git reset --hard
 git pull
