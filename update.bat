43ef60692ae9ef3d8faa5da5275a620960e1bc2a
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:31:22 -0800

    wip: extra newlines

--- update.bat
+++ update.bat
@@ -1,16 +1,23 @@
 @echo off
 call conda activate helao
 for /f "delims=" %%i in ('git branch --show-current') do set currentbranch=%%i
+
 git reset --hard
+
 git switch main
 git branch -D unstable
 git fetch --prune
 git checkout %currentbranch%
 git pull
+
 cd ..\helao-core
+
 git reset --hard
+
 git switch main
 git branch -D unstable
 git fetch --prune
 git checkout %currentbranch%
 git pull
+
+cd ..\helao-async
