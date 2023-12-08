a692c5983a53abba4b717b0e93b94bdb0d74aa12
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:14:43 -0800

    wip: debug git behavior

--- update.bat
+++ update.bat
@@ -2,7 +2,6 @@
 call conda activate helao
 cd ..\helao-core
 for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
-echo %corebranch%
 git reset --hard
 git fetch --prune
 git switch main
@@ -12,11 +11,12 @@ git switch %corebranch%
 git pull
 cd ..\helao-async
 for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
-echo %asyncbranch%
 git reset --hard
 git fetch --prune
 git switch main
 git branch -D unstable
+echo %asyncbranch%
+echo %asyncbranch%
 git pull
 git switch %asyncbranch%
 git pull
