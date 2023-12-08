7cd9411c936e2908498c4e95a32d18b244f23c55
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:39:37 -0800

    wip: revert update script

--- update.bat
+++ update.bat
@@ -1,23 +1,10 @@
 @echo off
 call conda activate helao
-for /f "delims=" %%i in ('git branch --show-current') do set currentbranch=%%i
-
-git reset --hard origin/main
-git checkout main
-git branch -D unstable
-git fetch --prune --all
-git pull
-git checkout %currentbranch%
-git pull
-
 cd ..\helao-core
-
-git reset --hard origin/main
-git checkout main
-git branch -D unstable
 git fetch --prune --all
+git reset --hard
 git pull
-git checkout %currentbranch%
-git pull
-
 cd ..\helao-async
+git fetch --prune --all
+git reset --hard
+git pull
