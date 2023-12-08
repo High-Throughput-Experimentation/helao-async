a71c27ccaa270b26f1889b7348efb57e23b9ff74
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:02:46 -0800

    fix: typo branch/switch

--- update.bat
+++ update.bat
@@ -1,14 +1,5 @@
 @echo off
 call conda activate helao
-cd ..\helao-async
-for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
-echo %asyncbranch%
-git switch main
-git switch -D unstable
-git fetch --prune
-git reset --hard
-git switch %asyncbranch%
-git pull
 cd ..\helao-core
 for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
 echo %corebranch%
@@ -17,4 +8,13 @@ git branch -D unstable
 git fetch --prune
 git reset --hard
 git switch %corebranch%
+git pull
+cd ..\helao-async
+for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
+echo %asyncbranch%
+git switch main
+git branch -D unstable
+git fetch --prune
+git reset --hard
+git switch %asyncbranch%
 git pull
