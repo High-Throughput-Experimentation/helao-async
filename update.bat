4e6e263c0d3be72b4a0006de9a95ed888b5c9ea0
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:34:36 -0800

    wip: only use checkout

--- update.bat
+++ update.bat
@@ -2,21 +2,19 @@
 call conda activate helao
 for /f "delims=" %%i in ('git branch --show-current') do set currentbranch=%%i
 
-git reset --hard
-
-git switch main
+git reset --hard origin/main
+git checkout main
 git branch -D unstable
-git fetch --prune
+git fetch --prune --all
 git checkout %currentbranch%
 git pull
 
 cd ..\helao-core
 
-git reset --hard
-
-git switch main
+git reset --hard origin/main
+git checkout main
 git branch -D unstable
-git fetch --prune
+git fetch --prune --all
 git checkout %currentbranch%
 git pull
 
