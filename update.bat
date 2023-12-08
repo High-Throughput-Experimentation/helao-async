acc7c2d1974b9dcd6b29ba31868909fb3930693f
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:26:37 -0800

    wip: update async first, pull after switch

--- update.bat
+++ update.bat
@@ -1,22 +1,16 @@
 @echo off
 call conda activate helao
+for /f "delims=" %%i in ('git branch --show-current') do set currentbranch=%%i
+git reset --hard
+git switch main
+git branch -D unstable
+git fetch --prune
+git switch %currentbranch%
+git pull
 cd ..\helao-core
-for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
 git reset --hard
-git fetch --prune
-..\helao-async\switch.bat main
+git switch main
 git branch -D unstable
-git pull
-..\helao-async\switch.bat %corebranch%
-git pull
-cd ..\helao-async
-for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
-git reset --hard
 git fetch --prune
-switch.bat main
-git branch -D unstable
-echo %asyncbranch%
-echo %asyncbranch%
-git pull
-switch.bat %asyncbranch%
+git switch %currentbranch%
 git pull
