e881962b241f92850177e4d99348427c39a0ec89
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:08:09 -0800

    fix: reorder git ops

--- update.bat
+++ update.bat
@@ -3,18 +3,20 @@ call conda activate helao
 cd ..\helao-core
 for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
 echo %corebranch%
+git reset --hard
+git fetch --prune
 git switch main
 git branch -D unstable
-git fetch --prune
-git reset --hard
+git pull
 git switch %corebranch%
 git pull
 cd ..\helao-async
 for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
 echo %asyncbranch%
+git reset --hard
+git fetch --prune
 git switch main
 git branch -D unstable
-git fetch --prune
-git reset --hard
+git pull
 git switch %asyncbranch%
 git pull
