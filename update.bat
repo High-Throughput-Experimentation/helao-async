0c01042a8519dc2fec260ad6dd4cbc13c79cf534
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 11:22:59 -0800

    fix: switch to active branch and pull

--- update.bat
+++ update.bat
@@ -1,14 +1,20 @@
 @echo off
 call conda activate helao
 cd ..\helao-core
+for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
 git switch main
 git branch -D unstable
 git fetch --prune
 git reset --hard
 git pull
+git switch %corebranch%
+git pull
 cd ..\helao-async
+for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
 git switch main
 git switch -D unstable
 git fetch --prune
 git reset --hard
+git pull
+git switch %asyncbranch%
 git pull
