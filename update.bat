d2a9797708e95ae5ccd982dc859425009d204508
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-08 13:20:48 -0800

    fix: use switch.bat in update.bat

--- update.bat
+++ update.bat
@@ -1,24 +1,22 @@
 @echo off
 call conda activate helao
 cd ..\helao-core
-echo cd
 for /f "delims=" %%i in ('git branch --show-current') do set corebranch=%%i
 git reset --hard
 git fetch --prune
-git switch main
+switch.bat main
 git branch -D unstable
 git pull
-git switch %corebranch%
+switch.bar %corebranch%
 git pull
 cd ..\helao-async
-echo cd
 for /f "delims=" %%i in ('git branch --show-current') do set asyncbranch=%%i
 git reset --hard
 git fetch --prune
-git switch main
+switch.bat main
 git branch -D unstable
 echo %asyncbranch%
 echo %asyncbranch%
 git pull
-git switch %asyncbranch%
+switch.bat %asyncbranch%
 git pull
