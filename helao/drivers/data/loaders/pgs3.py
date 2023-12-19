56b9ce56e417f7034eea6669f5ed07d81ffcdad9
Dan Guevarra <onepunchdan@users.noreply.github.com>
2023-12-19 14:46:55 -0800

    fix: use private method for sqlalchemy row -> dict

--- helao/drivers/data/loaders/pgs3.py
+++ helao/drivers/data/loaders/pgs3.py
@@ -144,7 +144,7 @@ class HelaoLoader:
                     obj_uuid,
                 )
             ] = (
-                dict(resp[0]) if resp else {}
+                resp[0].as_dict() if resp else {}
             )
         return self.sql_cache[
             (
