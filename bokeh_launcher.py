aca86d53623806a6e2b85fd3f40355b9a89ef86e
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-01-02 11:21:30 -0800

    fix: remove general try/except from launchers

--- bokeh_launcher.py
+++ bokeh_launcher.py
@@ -13,59 +13,49 @@ from helao.helpers.config_loader import config_loader
 
 if __name__ == "__main__":
     log_root = "."
-    try:
-        colorama.init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
-        helao_root = os.path.dirname(os.path.realpath(__file__))
-        server_key = sys.argv[2]
-        confArg = sys.argv[1]
-        config = config_loader(confArg, helao_root)
+    colorama.init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
+    helao_root = os.path.dirname(os.path.realpath(__file__))
+    server_key = sys.argv[2]
+    confArg = sys.argv[1]
+    config = config_loader(confArg, helao_root)
 
-        C = config["servers"]
-        S = C[server_key]
-        servHost = S["host"]
-        servPort = S["port"]
-        servPy = S["bokeh"]
-        launch_browser = S.get("params", {}).get("launch_browser", False)
+    C = config["servers"]
+    S = C[server_key]
+    servHost = S["host"]
+    servPort = S["port"]
+    servPy = S["bokeh"]
+    launch_browser = S.get("params", {}).get("launch_browser", False)
 
-        makeApp = import_module(
-            f"helao.servers.{S['group']}.{S['bokeh']}"
-        ).makeBokehApp
-        root = config.get("root", None)
-        if root is not None:
-            log_root = os.path.join(root, "LOGS")
-        else:
-            log_root = None
-        print_message(
-            {},
-            "bokeh_launcher",
-            f" ---- starting  {server_key} ----",
-            log_dir=log_root,
-            info=True,
-        )
+    makeApp = import_module(
+        f"helao.servers.{S['group']}.{S['bokeh']}"
+    ).makeBokehApp
+    root = config.get("root", None)
+    if root is not None:
+        log_root = os.path.join(root, "LOGS")
+    else:
+        log_root = None
+    print_message(
+        {},
+        "bokeh_launcher",
+        f" ---- starting  {server_key} ----",
+        log_dir=log_root,
+        info=True,
+    )
 
-        bokehapp = Server(
-            {f"/{servPy}": partial(makeApp, confPrefix=confArg, server_key=server_key, helao_root=helao_root)},
-            port=servPort,
-            address=servHost,
-            allow_websocket_origin=[f"{servHost}:{servPort}"],
-        )
-        print_message(
-            {},
-            "bokeh_launcher",
-            f"started {server_key} {bokehapp}",
-            log_dir=log_root,
-            info=True,
-        )
-        bokehapp.start()
-        if launch_browser:
-            bokehapp.io_loop.add_callback(bokehapp.show, f"/{servPy}")
-        bokehapp.io_loop.start()
-    except Exception as e:
-        tb = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-        print_message(
-            {},
-            "bokeh_launcher",
-            f"bokeh_launcher unhandled exception: {repr(e), tb,}",
-            log_dir=log_root,
-            error=True
-        )
+    bokehapp = Server(
+        {f"/{servPy}": partial(makeApp, confPrefix=confArg, server_key=server_key, helao_root=helao_root)},
+        port=servPort,
+        address=servHost,
+        allow_websocket_origin=[f"{servHost}:{servPort}"],
+    )
+    print_message(
+        {},
+        "bokeh_launcher",
+        f"started {server_key} {bokehapp}",
+        log_dir=log_root,
+        info=True,
+    )
+    bokehapp.start()
+    if launch_browser:
+        bokehapp.io_loop.add_callback(bokehapp.show, f"/{servPy}")
+    bokehapp.io_loop.start()
--- fast_launcher.py
+++ fast_launcher.py
@@ -14,50 +14,40 @@ from helao.helpers.config_loader import config_loader
 
 if __name__ == "__main__":
     log_root = "."
-    try:
-        colorama.init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
-        helao_root = os.path.dirname(os.path.realpath(__file__))
-        server_key = sys.argv[2]
-        confArg = sys.argv[1]
-        config = config_loader(confArg, helao_root)
-        C = config["servers"]
-        S = C[server_key]
+    colorama.init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
+    helao_root = os.path.dirname(os.path.realpath(__file__))
+    server_key = sys.argv[2]
+    confArg = sys.argv[1]
+    config = config_loader(confArg, helao_root)
+    C = config["servers"]
+    S = C[server_key]
 
-        makeApp = import_module(f"helao.servers.{S['group']}.{S['fast']}").makeApp
-        app = makeApp(confArg, server_key, helao_root)
-        root = config.get("root", None)
-        if root is not None:
-            log_root = os.path.join(root, "LOGS")
-        else:
-            log_root = None
-        # LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
+    makeApp = import_module(f"helao.servers.{S['group']}.{S['fast']}").makeApp
+    app = makeApp(confArg, server_key, helao_root)
+    root = config.get("root", None)
+    if root is not None:
+        log_root = os.path.join(root, "LOGS")
+    else:
+        log_root = None
+    # LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
 
-        LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%H:%M:%S"
-        LOGGING_CONFIG["formatters"]["default"][
-            "fmt"
-        ] = f"\n[%(asctime)s_{server_key}]: %(levelprefix)s %(message)s\r"
-        LOGGING_CONFIG["formatters"]["default"]["use_colors"] = False
+    LOGGING_CONFIG["formatters"]["default"]["datefmt"] = "%H:%M:%S"
+    LOGGING_CONFIG["formatters"]["default"][
+        "fmt"
+    ] = f"\n[%(asctime)s_{server_key}]: %(levelprefix)s %(message)s\r"
+    LOGGING_CONFIG["formatters"]["default"]["use_colors"] = False
 
-        LOGGING_CONFIG["formatters"]["access"]["datefmt"] = "%H:%M:%S"
-        LOGGING_CONFIG["formatters"]["access"][
-            "fmt"
-        ] = f"\n[%(asctime)s_{server_key}]: %(levelprefix)s %(message)s\r"
-        LOGGING_CONFIG["formatters"]["access"]["use_colors"] = False
+    LOGGING_CONFIG["formatters"]["access"]["datefmt"] = "%H:%M:%S"
+    LOGGING_CONFIG["formatters"]["access"][
+        "fmt"
+    ] = f"\n[%(asctime)s_{server_key}]: %(levelprefix)s %(message)s\r"
+    LOGGING_CONFIG["formatters"]["access"]["use_colors"] = False
 
-        print_message(
-            {},
-            "fast_launcher",
-            f" ---- starting  {server_key} ----",
-            log_dir=log_root,
-            info=True,
-        )
-        fastapp = uvicorn.run(app, host=S["host"], port=S["port"])
-    except Exception as e:
-        tb = "".join(traceback.format_exception(type(e), e, e.__traceback__))
-        print_message(
-            {},
-            "fast_launcher",
-            f"fast_launcher unhandled exception: {repr(e), tb,}",
-            log_dir=log_root,
-            error=True
-        )
+    print_message(
+        {},
+        "fast_launcher",
+        f" ---- starting  {server_key} ----",
+        log_dir=log_root,
+        info=True,
+    )
+    fastapp = uvicorn.run(app, host=S["host"], port=S["port"])
