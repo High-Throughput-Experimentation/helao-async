25bd4ac324afe52de8b5dec0c75d6f681f0aa19d
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-01-02 09:28:29 -0800

    wip: gcld client test script

--- helao/servers/operator/gcld_operator_test.py
+++ helao/servers/operator/gcld_operator_test.py
@@ -2,31 +2,51 @@ import sys
 import os
 import inspect
 import time
+from tqdm import tqdm
 from dotenv import load_dotenv
 from pathlib import Path
+
+from helao.servers.operator.helao_operator import HelaoOperator
+
+# from helao.helpers.gcld_client import DataRequestsClient
+from data_request_client.client import DataRequestsClient
+from data_request_client.models import Status
+from helao.helpers.premodels import Sequence
+from helao.helpers.dispatcher import private_dispatcher
+from helao.helpers.config_loader import config_loader
+from helao.helpers.gen_uuid import gen_uuid
+from helao.sequences.TEST_seq import TEST_consecutive_noblocking
+from helaocore.models.orchstatus import OrchStatus
 
 inst_config = sys.argv[1]
 PLATE_ID = int(sys.argv[2])
 env_config = sys.argv[3]
 load_dotenv(dotenv_path=Path(env_config))
 
-from helao.servers.operator.operator import HelaoOperator
-from helao.helpers.gcld_client import DataRequestsClient, CreateDataRequestModel
-from helao.helpers.premodels import Sequence, Experiment
-from helao.helpers.dispatcher import private_dispatcher
-from helao.helpers.config_loader import config_loader
-from helao.helpers.gen_uuid import gen_uuid
-from helao.sequences.UVIS_T_seq import UVIS_T, UVIS_T_postseq
-
 
 # print({k: v for k, v in os.environ.items() if k in ('API_KEY', 'BASE_URL')})
-client = DataRequestsClient(
+CLIENT = DataRequestsClient(
     base_url=os.environ["BASE_URL"], api_key=os.environ["API_KEY"]
 )
 
+TEST_defaults = {
+    "wait_time": 3.0,
+    "cycles": 5,
+    "dummy_list": [[0.0, 1.0], [2.0, 3.0]],
+}
 
-def uvis_seq_constructor(plate_id, sample_no, data_request_id, params={}):
-    argspec = inspect.getfullargspec(UVIS_T)
+
+def seq_constructor(
+    plate_id,
+    sample_no,
+    data_request_id,
+    params={},
+    seq_func=TEST_consecutive_noblocking,
+    seq_name="TEST_consecutive_noblocking",
+    seq_label="gcld-test",
+    param_defaults={},
+):
+    argspec = inspect.getfullargspec(seq_func)
     seq_args = list(argspec.args)
     seq_defaults = list(argspec.defaults)
     seq_uuid = gen_uuid()
@@ -34,64 +54,19 @@ def uvis_seq_constructor(plate_id, sample_no, data_request_id, params={}):
     seq_params.update(params)
     seq_params["plate_id"] = plate_id
     seq_params["plate_sample_no_list"] = [sample_no]
-    seq_params.update(
-        {
-            "reference_mode": "builtin",
-            "custom_position": "cell1_we",
-            "spec_n_avg": 5,
-            "spec_int_time_ms": 10,
-            "duration_sec": 5,
-            "specref_code": 1,
-            "led_type": "front",
-            "led_date": "4/28/2023",
-            "led_names": ["doric_wled"],
-            "led_wavelengths_nm": [-1],
-            "led_intensities_mw": [-1],
-            "toggle_is_shutter": False,
-            "analysis_seq_uuid": str(seq_uuid),
-        }
-    )
-    experiment_list = UVIS_T(**seq_params)
+    seq_params.update({k: v for k, v in param_defaults.items() if k not in seq_params})
+    experiment_list = seq_func(**seq_params)
     seq = Sequence(
-        sequence_name="UVIS_T",
-        sequence_label="gcld-mvp-demo",
+        sequence_name=seq_name,
+        sequence_label=seq_label,
         sequence_params=seq_params,
         sequence_uuid=seq_uuid,
         data_request_id=data_request_id,
-        experiment_list=experiment_list,
+        experiment_list=[],
         experiment_plan_list=experiment_list,
         experimentmodel_list=experiment_list,
     )
-    seq.sequence_uuid = seq_uuid
-    return seq
-
-def uvis_ana_constructor(plate_id, sequence_uuid, data_request_id, params={}):
-    argspec = inspect.getfullargspec(UVIS_T_postseq)
-    seq_args = list(argspec.args)
-    seq_defaults = list(argspec.defaults)
-    seq_uuid = gen_uuid()
-    seq_params = {k: v for k, v in zip(seq_args, seq_defaults)}
-    seq_params.update(params)
-    seq_params["plate_id"] = plate_id
-    seq_params.update(
-        {
-            "analysis_seq_uuid": sequence_uuid,
-            "plate_id": plate_id,
-            "recent": False
-        }
-    )
-    experiment_list = UVIS_T_postseq(**seq_params)
-    seq = Sequence(
-        sequence_name="UVIS_T_postseq",
-        sequence_label="gcld-mvp-demo-analysis",
-        sequence_params=seq_params,
-        sequence_uuid=seq_uuid,
-        data_request_id=data_request_id,
-        experiment_list=experiment_list,
-        experiment_plan_list=experiment_list,
-        experimentmodel_list=experiment_list,
-    )
-    seq.sequence_uuid = seq_uuid
+    # seq.sequence_uuid = seq_uuid
     return seq
 
 
@@ -99,7 +74,34 @@ def gen_ts():
     return f"[{time.strftime('%H:%M:%S')}]"
 
 
-if __name__ == "__main__":
+def wait_for_orch(
+    op: HelaoOperator, orch_state: OrchStatus = OrchStatus.busy, polling_time=5.0
+):
+    progress = tqdm()
+    current_state = op.orch_state()
+    current_orch = current_state["orch_state"]
+    active_seq = current_state["active_sequence"]
+    last_seq = current_state["last_sequence"]
+    if current_orch != orch_state:
+        print(f"orchestrator status != {orch_state}, waiting {polling_time} per iter:")
+    while current_orch != orch_state:
+        if current_orch in [OrchStatus.error, OrchStatus.estopped]:
+            return current_orch, active_seq, last_seq
+        progress.update()
+        time.sleep(polling_time)
+        current_state = op.orch_state()
+        current_orch = current_state["orch_state"]
+        active_seq = current_state["active_sequence"]
+        last_seq = current_state["last_sequence"]
+    return orch_state, active_seq, last_seq
+
+
+def num_uploads(db_cfg):
+    resp, err = private_dispatcher("DB", db_cfg["host"], db_cfg["port"], "tasks")
+    return len(resp.get("running", [])) + resp.get("num_queued", 0)
+
+
+def main():
     helao_root = os.path.dirname(os.path.realpath(__file__))
     while "helao.py" not in os.listdir(helao_root):
         helao_root = os.path.dirname(helao_root)
@@ -108,70 +110,145 @@ if __name__ == "__main__":
     world_cfg = config_loader(inst_config, helao_root)
     db_cfg = world_cfg["servers"]["DB"]
 
-    def num_running_uploads():
-        resp, err = private_dispatcher("DB", db_cfg["host"], db_cfg["port"], "running")
-        return len(resp)
+    while True:
+        with CLIENT:
+            pending_requests = CLIENT.read_data_requests(status="pending")
 
-    reqmodels = [
-        CreateDataRequestModel(composition = {"Mn": 1.0, "Sb": 0.0}, score = 1.0, sample_label="sample_1000"),
-        CreateDataRequestModel(composition = {"Mn": 0.5, "Sb": 0.5}, score = 1.0, sample_label="sample_3000"),
-    ]
-
-    for reqmod in reqmodels:
-        with client:
-            # get pending data requests
-            output = client.create_data_request(reqmod)
-
-        if output:
-            data_request = output
+        if pending_requests:
+            print(f"Pending data request count: {len(pending_requests)}")
+            data_request = pending_requests[0]
             sample_no = int(data_request.sample_label.split("_")[-1])
 
-            seq = uvis_seq_constructor(PLATE_ID, sample_no, data_request.id)
+            # TEST SEQUENCE
+            seq = seq_constructor(
+                plate_id=PLATE_ID,
+                sample_no=sample_no,
+                data_request_id=data_request.id,
+                params={},
+                seq_func=TEST_consecutive_noblocking,
+                seq_name="TEST_consecutive_noblocking",
+                seq_label="gcld-test",
+                param_defaults=TEST_defaults,
+            )
             operator.add_sequence(seq.get_seq())
             print(f"Dispatching measurement sequence: {seq.sequence_uuid}")
             operator.start()
-            time.sleep(2)
 
-            current_state = operator.orch_state()
+            time.sleep(5)
 
-            if current_state != "stopped":
+            # wait for sequence start (orch_state == "busy")
+            current_state, active_seq, last_seq = wait_for_orch(
+                operator, OrchStatus.busy
+            )
+            print("!!!")
+            print(active_seq["sequence_uuid"])
+            if current_state in [OrchStatus.error, OrchStatus.estopped]:
+                with CLIENT:
+                    output = CLIENT.set_status(
+                        Status.failed, data_request_id=data_request.id
+                    )
+                    input(
+                        "Press Enter to reset failed request to pending and exit operator..."
+                    )
+                    output = CLIENT.set_status(
+                        Status.pending, data_request_id=data_request.id
+                    )
+                    return -1
+            elif str(active_seq["sequence_uuid"]) == str(seq.sequence_uuid):
                 # Acknowledge the data request
-                with client:
-                    output = client.acknowledge_data_request(data_request.id)
+                with CLIENT:
+                    output = CLIENT.acknowledge_data_request(data_request.id)
                 print(f"Data request status: {output.status}")
 
-            while current_state != "stopped":
-                print(
-                    f"{gen_ts()} Orchestrator loop status is {current_state}. Sleeping for 10s."
-                )
-                time.sleep(10)
-                current_state = operator.orch_state()
+            # wait for sequence end (orch_state == "idle")
+            current_state, active_seq, last_seq = wait_for_orch(
+                operator, OrchStatus.idle
+            )
+            if current_state in [OrchStatus.error, OrchStatus.estopped]:
+                with CLIENT:
+                    output = CLIENT.set_status(
+                        Status.failed, data_request_id=data_request.id
+                    )
+                    input(
+                        "Press Enter to reset failed request to pending and exit operator..."
+                    )
+                    output = CLIENT.set_status(
+                        Status.pending, data_request_id=data_request.id
+                    )
+                    return -1
 
             time.sleep(30)
-                
+
             # when orchestrator has stopped, check DB server for upload state
-            num_uploads = num_running_uploads()
-            while num_uploads > 0:
-                print(f"Waiting for {num_uploads} sequence uploads to finish.")
+            num_sync_tasks = num_uploads(db_cfg)
+            while num_sync_tasks > 0:
+                print(f"Waiting for {num_sync_tasks} sequence uploads to finish.")
                 time.sleep(10)
-                num_uploads = num_running_uploads()
-            
-            ana = uvis_ana_constructor(PLATE_ID, str(seq.sequence_uuid), data_request.id)
-            operator.add_sequence(ana.get_seq())
-            print(f"Dispatching analysis sequence: {ana.sequence_uuid}")
+                num_sync_tasks = num_uploads(db_cfg)
+
+            seq2 = seq_constructor(
+                plate_id=PLATE_ID,
+                sample_no=sample_no,
+                data_request_id=data_request.id,
+                params={},
+                seq_func=TEST_consecutive_noblocking,
+                seq_name="TEST_consecutive_noblocking",
+                seq_label="gcld-test",
+                param_defaults=TEST_defaults,
+            )
+            operator.add_sequence(seq2.get_seq())
+            print(f"Dispatching analysis sequence: {seq2.sequence_uuid}")
             operator.start()
-            time.sleep(2)
 
-            current_state = operator.orch_state()
+            time.sleep(5)
 
-            while current_state != "stopped":
-                print(
-                    f"{gen_ts()} Orchestrator loop status is {current_state}. Sleeping for 10s."
-                )
-                time.sleep(10)
-                current_state = operator.orch_state()
-                
+            # wait for analysis start (orch_state == "busy")
+            current_state, active_seq, last_seq = wait_for_orch(
+                operator, OrchStatus.busy
+            )
+            if current_state in [OrchStatus.error, OrchStatus.estopped]:
+                with CLIENT:
+                    output = CLIENT.set_status(
+                        Status.failed, data_request_id=data_request.id
+                    )
+                    input(
+                        "Press Enter to reset failed request to pending and exit operator..."
+                    )
+                    output = CLIENT.set_status(
+                        Status.pending, data_request_id=data_request.id
+                    )
+                    return -1
+            elif active_seq["sequence_uuid"] == seq.sequence_uuid:
+                # Acknowledge the data request
+                with CLIENT:
+                    output = CLIENT.acknowledge_data_request(data_request.id)
+                print(f"Data request status: {output.status}")
+
+            # wait for analysis end (orch_state == "idle")
+            current_state, active_seq, last_seq = wait_for_orch(
+                operator, OrchStatus.idle
+            )
+            if current_state in [OrchStatus.error, OrchStatus.estopped]:
+                with CLIENT:
+                    output = CLIENT.set_status(
+                        Status.failed, data_request_id=data_request.id
+                    )
+                    input(
+                        "Press Enter to reset failed request to pending and exit operator..."
+                    )
+                    output = CLIENT.set_status(
+                        Status.pending, data_request_id=data_request.id
+                    )
+                    return -1
+
+        with CLIENT:
+            print("Test mode: resetting data request status to pending.")
+            output = CLIENT.set_status(Status.pending, data_request_id=data_request.id)
         print(
             f"{gen_ts()} Orchestrator is idle. Checking for data requests in 15 seconds."
         )
         time.sleep(15)
+
+
+if __name__ == "__main__":
+    main()
