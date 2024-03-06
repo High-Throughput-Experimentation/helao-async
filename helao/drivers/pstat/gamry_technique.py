6fc9441edf56832ffc772402a899df2863a07aef
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-06 15:30:00 -0800

    wip: complete GamryTechnique (except EIS)

--- helao/drivers/pstat/gamry_technique.py
+++ helao/drivers/pstat/gamry_technique.py
@@ -2,8 +2,25 @@ from dataclasses import dataclass
 from typing import Optional
 from enum import StrEnum
 
-from .gamry_dtaq import GamryDtaq, DTAQ_CPIV
-from .gamry_signal import GamrySignal, SIGNAL_VRAMP
+from .gamry_dtaq import (
+    GamryDtaq,
+    DTAQ_CPIV,
+    DTAQ_CHRONOA,
+    DTAQ_CHRONOP,
+    DTAQ_RCV,
+    DTAQ_OCV,
+    DTAQ_UNIV,
+)
+from .gamry_signal import (
+    GamrySignal,
+    VSIGNAL_RAMP,
+    ISIGNAL_RAMP,
+    VSIGNAL_CONST,
+    ISIGNAL_CONST,
+    VSIGNAL_ARRAY,
+    VSIGNAL_RUPDN,
+    OCVSIGNAL_CONST,
+)
 
 # define enums to match GamryCOM
 
@@ -22,9 +39,47 @@ class GamryTechnique:
     set_ierangemode: Optional[bool] = None
 
 
-LSV = GamryTechnique(
+TECH_LSV = GamryTechnique(
     on_method=OnMethod.CellOn,
     dtaq=DTAQ_CPIV,
-    signal=SIGNAL_VRAMP,
+    signal=VSIGNAL_RAMP,
     set_vchrangemode=False,
+)
+
+TECH_LSA = GamryTechnique(
+    on_method=OnMethod.CellOn,
+    dtaq=DTAQ_CPIV,
+    signal=ISIGNAL_RAMP,
+    set_vchrangemode=False,
+)
+
+TECH_CA = GamryTechnique(
+    on_method=OnMethod.CellOn,
+    dtaq=DTAQ_CHRONOA,
+    signal=VSIGNAL_CONST,
+    set_vchrangemode=False,
+)
+TECH_CP = GamryTechnique(
+    on_method=OnMethod.CellOn,
+    dtaq=DTAQ_CHRONOP,
+    signal=ISIGNAL_CONST,
+    set_vchrangemode=False,
+)
+TECH_CV = GamryTechnique(
+    on_method=OnMethod.CellOn,
+    dtaq=DTAQ_RCV,
+    signal=VSIGNAL_RUPDN,
+    set_vchrangemode=False,
+)
+TECH_OCV = GamryTechnique(
+    on_method=OnMethod.CellMon,
+    dtaq=DTAQ_OCV,
+    signal=OCVSIGNAL_CONST,
+    set_vchrangemode=True,
+)
+TECH_RCA = GamryTechnique(
+    on_method=OnMethod.CellOn,
+    dtaq=DTAQ_UNIV,
+    signal=VSIGNAL_ARRAY,
+    set_vchrangemode=True,
 )
