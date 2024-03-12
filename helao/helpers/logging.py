16d756ea5bd1ed9be4a9f16559a347726d0923f9
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-11 20:43:03 -0700

    feat: add colored logging

--- helao/helpers/logging.py
+++ helao/helpers/logging.py
@@ -11,6 +11,7 @@ Usage:
 
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from colorlog import ColoredFormatter
 import tempfile
 import os
 
@@ -31,21 +32,36 @@ def make_logger(
     log_path = Path(os.path.join(log_dir, f"{logger_name}.log"))
     format_string = "%(asctime)s :: %(funcName)s @ %(filename)s:%(lineno)d - %(levelname)-8s %(message)s"
     formatter = logging.Formatter(format_string)
+    # for stream output
+    colored_format_string = "%(log_color)s%(asctime)s :: %(funcName)s @ %(filename)s:%(lineno)d - %(levelname)-8s%(reset)s %(blue)s%(message)s"
+    colored_formatter = ColoredFormatter(
+        colored_format_string,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+        secondary_log_colors={},
+        style='%'
+    )
 
     logger = logging.getLogger(logger_name)
     logger.setLevel(log_level)
 
     # create handlers
     console = logging.StreamHandler()
+    console.setFormatter(colored_formatter)
     timed_rotation = TimedRotatingFileHandler(
         filename=log_path, when="D", interval=1, backupCount=14
     )
+    timed_rotation.setFormatter(formatter)
 
-    # set log level, formatter, and attach handlers
+    # set log level and attach handlers
     handlers = [console, timed_rotation]
     for handler in handlers:
         handler.setLevel(log_level)
-        handler.setFormatter(formatter)
         logger.addHandler(handler)
 
     logger.info(f"writing log events to {log_path}")
