da01a6b624f1645571264bd1e35a0926d41d63bf
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-20 21:18:54 -0700

    fix: use combo commands for batch scripts

--- switch.bat
+++ switch.bat
@@ -3,21 +3,10 @@ call conda activate helao
 
 echo switching helao-core branch
 cd ..\helao-core
-
-git fetch --all
-git switch main
-
-git branch -D unstable
-git switch unstable
-
-git switch %1
-
+git fetch --all && git switch main && git branch -D unstable && git switch unstable && git switch %1
 echo[ 
 
 echo switching helao-async branch
 cd ..\helao-async
-
-git fetch --all
-git switch main && git branch -D unstable && git switch unstable && git switch %1
-
+git fetch --all && git switch main && git branch -D unstable && git switch unstable && git switch %1
 echo[ 
--- update.bat
+++ update.bat
@@ -3,18 +3,10 @@ call conda activate helao
 
 echo resetting helao-core and pulling updates
 cd ..\helao-core
-
-git fetch --prune --all
-git reset --hard
-
-git pull
+git fetch --prune --all && git reset --hard && git pull
 echo[ 
 
 echo resetting helao-async and pulling updates
 cd ..\helao-async
-
-git fetch --prune --all
-git reset --hard
-
-git pull
+git fetch --prune --all && git reset --hard && git pull
 echo[ 
