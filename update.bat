6fcec444b962306c990a21c8508287f7e8fdafdd
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-20 20:52:48 -0700

    wip: add newlines to update.bat

--- update.bat
+++ update.bat
@@ -1,20 +1,34 @@
 @echo off
 call conda activate helao
+
 echo resetting helao-core and pulling updates
 cd ..\helao-core
 git switch main
+
 git branch -D unstable
+
 git fetch --prune --all
+
 git reset --hard
+
 git pull
+
 git reset --hard
+
 echo[ 
+
 echo resetting helao-async and pulling updates
 cd ..\helao-async
 git switch main
+
 git branch -D unstable
+
 git fetch --prune --all
+
 git reset --hard
+
 git pull
+
 git reset --hard
+
 echo[ 
