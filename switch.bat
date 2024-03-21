5947005513e44e61af2713dc0202ec9d137a14b4
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-20 21:12:15 -0700

    wip: switch to main, reset unstable, then switch to intended branch

--- switch.bat
+++ switch.bat
@@ -4,15 +4,13 @@ call conda activate helao
 cd ..\helao-core
 
 git fetch --all
-if %~1=="main" (
-    git switch main
+git switch main
 
-) else (
-    git branch -D unstable
-    git switch unstable
+git branch -D unstable
+git switch unstable
 
-)
 git switch %1
+
 echo[ 
 
 cd ..\helao-async
