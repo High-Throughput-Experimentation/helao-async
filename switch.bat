a54980359383f9ac7eb046c735b1cefa8fc321ce
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-20 21:16:54 -0700

    wip: test combo command

--- switch.bat
+++ switch.bat
@@ -18,11 +18,6 @@ echo switching helao-async branch
 cd ..\helao-async
 
 git fetch --all
-git switch main
-
-git branch -D unstable
-git switch unstable
-
-git switch %1
+git switch main && git branch -D unstable && git switch unstable && git switch %1
 
 echo[ 
