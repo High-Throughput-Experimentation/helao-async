cf181dd1696dc76d49905b439a9c29fd3e2de58a
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-03-20 20:58:45 -0700

    chore: test commit

--- switch.bat
+++ switch.bat
@@ -1,13 +1,23 @@
 @echo off
 call conda activate helao
+
 cd ..\helao-core
 git fetch --all
+
 if %~1=="main" (
     git switch main
 ) else (
     git switch unstable
 )
+
 git switch %1
+
+echo[ 
+
 cd ..\helao-async
+
 git fetch --all
+
 git switch %1
+
+echo[ 
