f2f0a0db7eb4dec22ef474544fdca599462928c4
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-04-12 11:21:33 -0700

    feat: use time-sorted uuid7 instead of uuid4

--- helao/helpers/gen_uuid.py
+++ helao/helpers/gen_uuid.py
@@ -1,11 +1,12 @@
 __all__ = ["gen_uuid"]
 
 import uuid
+from uuid7 import uuid7
 
 
 def gen_uuid(input:str = None):
     "Generate a uuid, encode with larger character set, and trucate."
     if input is None:
-        return uuid.uuid4()
+        return uuid7()
     else:
         return uuid.uuid5(input)
