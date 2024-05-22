29053ba9abcd20cca3204f0d10406cc7c9c25658
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-05-22 11:51:24 -0700

    wip: Andor camera server endpoints

--- helao/servers/action/andor_server.py
+++ helao/servers/action/andor_server.py
@@ -12,10 +12,7 @@ __all__ = ["makeApp"]
 import asyncio
 import time
 from typing import Optional, List
-from collections import defaultdict, deque
 
-import numpy as np
-import pandas as pd
 from fastapi import Body
 
 from helaocore.error import ErrorCodes
@@ -27,7 +24,7 @@ from helao.helpers.premodels import Action
 from helao.helpers.config_loader import config_loader
 from helao.helpers.executor import Executor
 from helao.helpers import logging  # get LOGGER from BaseAPI instance
-from helao.drivers.spec.andor.driver import AndorDriver
+from helao.drivers.spec.andor.driver import AndorDriver, DriverResponse
 
 global LOGGER
 if logging.LOGGER is None:
@@ -41,12 +38,64 @@ class AndorCooling(Executor):
 
     driver: AndorDriver
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            self.poll_rate = 5  # pump events every 100 millisecond
+            self.start_time = time.time()
+
+            # link attrs for convenience
+            self.action_params = self.active.action.action_params
+            self.driver = self.active.base.fastapp.driver
+            self.cam = self.driver.cam
+
+            # no external timer, event sink signals end of measurement
+            self.duration = -1
+
+            self.timeout = self.action_params.get("timeout", 600)
+            self.cooldown = self.action_params.get("cooldown", True)
+
+            LOGGER.info("AndorCooling initialized.")
+        except Exception:
+            LOGGER.error("AndorCooling was not initialized.", exc_info=True)
+
+    async def _exec(self) -> dict:
+        """Set SensorCooling flag and wait for stabilization."""
+        LOGGER.debug(f"setting cam.SensorCooling = {self.cooldown}")
+        resp = self.cam.SensorCooling = self.cooldown
+        error = ErrorCodes.none if resp.response == "success" else ErrorCodes.critical
+        return {"error": error}
+
+    async def _poll(self) -> dict:
+        """Return data and status from dtaq event sink."""
+        sensor_temp = self.cam.SensorTemperature
+
+        status = HloStatus.active
+        if self.cam.TemperatureStatus == "Stabilised":
+            if (sensor_temp < 20 and self.cooldown) or (
+                sensor_temp >= 20 and not self.cooldown
+            ):
+                status = HloStatus.finished
+
+        error = ErrorCodes.none
+        return {
+            "error": error,
+            "status": status,
+            "data": {"sensor_temp__C": sensor_temp},
+        }
+
 
 class AndorAdjustND(Executor):
     """Auto-select ND filter with maximum optimality."""
 
     driver: AndorDriver
 
+    async def _exec(self) -> dict:
+        """Run ND filter adjustment routine."""
+        LOGGER.debug("Running driver.adjust_ND()")
+        resp = self.driver.adjust_ND()
+        error = ErrorCodes.none if resp.response == "success" else ErrorCodes.critical
+        return {"error": error, "data": resp.data}
 
 class AndorExtTrig(Executor):
     """Acquire data with external start trigger."""
@@ -66,7 +115,7 @@ class AndorExtTrig(Executor):
             # no external timer, event sink signals end of measurement
             self.duration = -1
 
-            self.frame_count = self.action_params["frame_count"]
+            self.max_frame_count = self.action_params["frame_count"]
             self.timeout = self.action_params["timeout"]
             self.buffer_count = self.action_params["buffer_count"]
 
@@ -74,10 +123,11 @@ class AndorExtTrig(Executor):
             self.framerate = self.action_params["framerate"]
 
             self.first_tick = None
+            self.frames_acquired = 0
 
-            LOGGER.info("AndorExec initialized.")
+            LOGGER.info("AndorExtTrig initialized.")
         except Exception:
-            LOGGER.error("AndorExec was not initialized.", exc_info=True)
+            LOGGER.error("AndorExtTrig was not initialized.", exc_info=True)
 
     async def _pre_exec(self) -> dict:
         """Setup potentiostat device for given technique."""
@@ -97,39 +147,22 @@ class AndorExtTrig(Executor):
     async def _poll(self) -> dict:
         """Return data and status from dtaq event sink."""
         resp = self.driver.get_data()
-        # populate executor buffer for output calculation
-        for k, v in resp.data.items():
-            self.data_buffer[k].extend(v)
+        if resp.data:
+            if self.first_tick is None:
+                self.first_tick = resp.data["tick_time"][0]
+            self.frames_acquired += len(resp.data["tick_time"])
         error = ErrorCodes.none if resp.response == "success" else ErrorCodes.critical
-        status = HloStatus.active if resp.message != "done" else HloStatus.finished
+        status = (
+            HloStatus.active
+            if resp.status == DriverResponse.busy
+            and self.frames_acquired < self.max_frame_count
+            else HloStatus.finished
+        )
         return {"error": error, "status": status, "data": resp.data}
 
     async def _post_exec(self):
         resp = self.driver.cleanup()
-        # may want to call disconnect if self.driver.connect() was used in setup or driver init
         self.driver.disconnect()
-
-        # # _post_exec can send one last data message and/or make final calculations and store in action_params
-        # # parse calculate outputs from data buffer:
-        # for k in ["t_s", "Ewe_V", "I_A"]:
-        #     if k in self.data_buffer:
-        #         meanv = np.nanmean(np.array(self.data_buffer[k])[-5:])
-        #         self.active.action.action_params[f"{k}__mean_final"] = meanv
-
-        # if self.active.action.action_name == "run_OCV":
-        #     data_df = pd.DataFrame(self.data_buffer)
-        #     rsd_thresh = self.action_params.get("RSD_threshold", 1)
-        #     simple_thresh = self.action_params.get("simple_threshold", 1)
-        #     signal_change_thresh = self.action_params.get("signal_change_threshold", 1)
-        #     amplitude_thresh = self.action_params.get("amplitude_threshold", 1)
-        #     has_bubble = bubble_detection(
-        #         data_df,
-        #         rsd_thresh,
-        #         simple_thresh,
-        #         signal_change_thresh,
-        #         amplitude_thresh,
-        #     )
-        #     self.active.action.action_params["has_bubble"] = has_bubble
 
         error = ErrorCodes.none if resp.response == "success" else ErrorCodes.critical
         return {"error": error, "data": {}}
@@ -144,18 +177,14 @@ class AndorExtTrig(Executor):
 async def andor_dyn_endpoints(app=None):
     server_key = app.base.server.server_name
 
-    # while not app.driver.ready:
-    #     LOGGER.info("waiting for andor init")
-    #     await asyncio.sleep(1)
-
-    # app.driver.connect()
-    # app.driver.disconnect()
-
     @app.post(f"/{server_key}/acquire_external_trig", tags=["action"])
     async def acquire_external_trig(
         action: Action = Body({}, embed=True),
         action_version: int = 2,
-        some_example_param: float = 0.0,
+        frame_count: int = 10,
+        buffer_count: int = 10,
+        exp_time: float = 0.0098,
+        framerate: float = 98,
     ):
         data_keys = ["elapsed_time_s"] + [
             f"ch_{i:04}" for i in range(len(app.driver.wl_arr.shape[0]))
@@ -163,6 +192,7 @@ async def andor_dyn_endpoints(app=None):
         active = await app.base.setup_and_contain_action(
             json_data_keys=data_keys,
             file_type="andor_helao__file",
+            # to reduce polling data size, we get the wl_arr directly from the driver
             hloheader=HloHeaderModel(
                 action_name=action.action_name,
                 column_headings=data_keys,
@@ -174,7 +204,6 @@ async def andor_dyn_endpoints(app=None):
         active.action.action_abbr = "ANDORSPEC"
         executor = AndorExtTrig(active=active, oneoff=False)
         active_action_dict = active.start_executor(executor)
-        #
 
         return active_action_dict
 
@@ -191,6 +220,27 @@ async def andor_dyn_endpoints(app=None):
         finished_action = await active.finish()
         return finished_action.as_dict()
 
+    @app.post(f"/{server_key}/cooling", tags=["action"])
+    async def cooling(
+        action: Action = Body({}, embed=True),
+        action_version: int = 1,
+        cooldown: bool = True,
+        timeout: int = 600,
+    ):
+        active = await app.base.setup_and_contain_action()
+        executor = AndorCooling(active=active, oneoff=True, cooldown=cooldown, timeout=timeout)
+        active_action_dict = active.start_executor(executor)
+        return active_action_dict
+    
+    @app.post(f"/{server_key}/adjust_nd", tags=["action"])
+    async def adjust_nd(
+        action: Action = Body({}, embed=True),
+        action_version: int = 1,
+    ):
+        active = await app.base.setup_and_contain_action()
+        executor = AndorAdjustND(active=active, oneoff=True)
+        active_action_dict = active.start_executor(executor)
+        return active_action_dict
 
 def makeApp(confPrefix, server_key, helao_root):
 
