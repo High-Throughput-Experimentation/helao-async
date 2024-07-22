56c94c1f3d0a4a44b7695d77732a5f6bcf24999e
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-07-22 16:29:38 -0700

    wip: setup logger in LeancatDriver

--- helao/drivers/test_station/leancat/logger/logger.py
+++ helao/drivers/test_station/leancat/logger/logger.py
@@ -6,19 +6,19 @@ from datetime import datetime
 
 log_level_acquired = True
 
-with open(arg_app_config_path, "r") as f:
-    app_config = json.loads(f.read())
-    log_level_str = app_config["app"]["logLevel"]["python"]
+# with open(arg_app_config_path, "r") as f:
+#     app_config = json.loads(f.read())
+#     log_level_str = app_config["app"]["logLevel"]["python"]
 
-match log_level_str:
-    case "info":
-        log_level = logging.INFO
-    case "debug":
-        log_level = logging.DEBUG
-    case _:
-        log_level = logging.INFO
-        # Set default level INFO
-        log_level_acquired = False
+# match log_level_str:
+#     case "info":
+#         log_level = logging.INFO
+#     case "debug":
+#         log_level = logging.DEBUG
+#     case _:
+#         log_level = logging.INFO
+#         # Set default level INFO
+#         log_level_acquired = False
 
 
 def setup_logger(log_name, log_folder, level) -> logging.Logger:
@@ -43,16 +43,16 @@ def setup_logger(log_name, log_folder, level) -> logging.Logger:
     return logging.getLogger(log_name)
 
 
-log_folder = os.path.join(os.getcwd(), arg_logs_folder_path)
+# log_folder = os.path.join(os.getcwd(), arg_logs_folder_path)
 
-main_log = setup_logger("main", log_folder, log_level)
-main_log_path = main_log.handlers[0].baseFilename
-main_log.info("Main log started")
-if not log_level_acquired:
-    main_log.info(f'Log level "{log_level_str}" not recognized, setting default level INFO')
-main_log.info(f"Log level: {main_log.level}")
+# main_log = setup_logger("main", log_folder, log_level)
+# main_log_path = main_log.handlers[0].baseFilename
+# main_log.info("Main log started")
+# if not log_level_acquired:
+#     main_log.info(f'Log level "{log_level_str}" not recognized, setting default level INFO')
+# main_log.info(f"Log level: {main_log.level}")
 
-script_log = setup_logger("script", log_folder, log_level)
-script_log_path = script_log.handlers[0].baseFilename
-script_log.info("Script log started")
-script_log.info(f"Log level: {main_log.level}")
+# script_log = setup_logger("script", log_folder, log_level)
+# script_log_path = script_log.handlers[0].baseFilename
+# script_log.info("Script log started")
+# script_log.info(f"Log level: {main_log.level}")
