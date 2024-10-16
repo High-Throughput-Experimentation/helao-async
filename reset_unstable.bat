a02ffb9330b5670654fe1def7d17a818f323b831
Dan Guevarra <onepunchdan@users.noreply.github.com>
2024-10-16 10:32:40 -0700

    feat: batch script for resetting unstable branch

---
+++ reset_unstable.bat
@@ -0,0 +1,18 @@
+@echo off
+call conda activate helao
+
+echo resetting helao-core and pulling updates
+cd ..\helao-core
+git switch main
+git branch -D unstable
+git fetch --prune --all && git reset --hard && git pull
+git switch unstable
+echo[
+
+echo resetting helao-async and pulling updates
+cd ..\helao-async
+git switch main
+git branch -D unstable
+git fetch --prune --all && git reset --hard && git pull
+git switch unstable
+echo[
