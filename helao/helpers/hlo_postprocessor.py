ee1d8d9f639e6becb4aadbb53cfb1da41fb135b2
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-01-17 10:12:14 -0800

    feat: exp and seq path attribs for post processing

--- helao/helpers/hlo_postprocessor.py
+++ helao/helpers/hlo_postprocessor.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from glob import glob
 from abc import ABC, abstractmethod
 from helao.core.models.file import FileInfo
 from helao.helpers.premodels import Action
@@ -9,6 +10,10 @@ class HloPostProcessor(ABC):
     def __init__(self, action: Action, save_root: str):
         self.action = action
         self.output_dir = os.path.join(save_root, action.action_output_dir)
+        exp_dir = os.path.dirname(self.output_dir)
+        self.exp_yml_path = glob(os.path.join(exp_dir, "*.yml"))[0]
+        seq_dir = os.path.dirname(exp_dir)
+        self.seq_yml_path = glob(os.path.join(seq_dir, "*.yml"))[0]
         self.files = action.files
     
     @abstractmethod
