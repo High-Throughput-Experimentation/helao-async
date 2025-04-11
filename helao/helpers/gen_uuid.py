fed87c16dd1592a2c5da6db8562ea05a596d2ea5
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-04-11 11:22:00 -0700

    fix: uuid5 requires namespace

--- helao/helpers/gen_uuid.py
+++ helao/helpers/gen_uuid.py
@@ -2,11 +2,12 @@ __all__ = ["gen_uuid"]
 
 import uuid
 from uuid_extensions import uuid7
+from typing import Optional
 
 
-def gen_uuid(input:str = None):
+def gen_uuid(input: Optional[str] = None) -> uuid.UUID:
     "Generate a uuid, encode with larger character set, and trucate."
     if input is None:
         return uuid7()
     else:
-        return uuid.uuid5(input)
+        return uuid.uuid5(uuid.NAMESPACE_URL, input)
