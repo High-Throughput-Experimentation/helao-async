c339a2b26ffb973bf3af8ad16e7322f08345fff8
Benjamin Moss <b.moss14@imperial.ac.uk>
2025-04-23 15:20:04 -0700

    fix calibrate downsample parq rounded time avoid unhandled cases

--- helao/helpers/HiSpEC_calibrate_downsample_parquet.py
+++ helao/helpers/HiSpEC_calibrate_downsample_parquet.py
@@ -334,16 +334,17 @@ def return_cycle_for_time(time:float, min_max_dict:dict)->int:
     cycle: the cycle number the time belongs to.
     """
     for cycle, min_max in min_max_dict.items():
+        time = round_10ms(time)
         if time == 0:
             return 0
         if time >= min_max[0] and time <= min_max[1]:
             return cycle
         # if the time is greater than the max time of the last cycle
         # it is in the last cycle
-        elif cycle == list(min_max_dict.keys())[-1] and time > min_max[1]:
+        elif cycle == sorted(min_max_dict.keys())[-1] and time > min_max[1]:
             return cycle
-    else:
-        raise ValueError(f"Time {time} not found in any cycle")
+        else:
+            raise ValueError(f"Time {time} not found in any cycle")
 
 def get_cycles_for_spec_times(calibration_df:pd.DataFrame, CV_data:pd.DataFrame, default_time_header1='t_s', default_cycle_header1='cycle')->pd.DataFrame:
     """
