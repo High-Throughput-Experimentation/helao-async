cd2393f89565c30f2bcd15c819e536a5d21c67b6
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-05-23 14:01:30 -0700

    chore: apply black formatting

--- helao/processors/HiSpEC_process_all.py
+++ helao/processors/HiSpEC_process_all.py
@@ -22,6 +22,8 @@ import json
 import pyarrow as pa
 import pyarrow.parquet as pq
 import pandas as pd
+
+
 class PostProcess(HloPostProcessor):
 
     def process(self) -> List[FileInfo]:
@@ -37,9 +39,11 @@ class PostProcess(HloPostProcessor):
                     ) as temp_file:
                         hlo_to_parquet(file_path, temp_file.name, HiSpEC=True)
                         df = fully_read_and_calibrate_parquet(
-                            cv_path=cvact.data_files[0],  # .data_files is now a property func that evaluates at call time, calling it here will get the freshest path since hlo_to_parquet might take some time
+                            cv_path=cvact.data_files[
+                                0
+                            ],  # .data_files is now a property func that evaluates at call time, calling it here will get the freshest path since hlo_to_parquet might take some time
                             spec_path=temp_file.name,
-                        write_file=False,
+                            write_file=False,
                         ).sort_values(by="t (s)")
                         hlo_meta = read_helao_metadata(temp_file.name)
                     action_comment = self.action.action_params.get("comment", "")
@@ -54,23 +58,27 @@ class PostProcess(HloPostProcessor):
                     df.rename(columns={"t (s)": "t_s"}, inplace=True)
                     # rename J(A) to J_A
                     df.rename(columns={"J (A)": "J_A"}, inplace=True)
-                    #reset the index
+                    # reset the index
                     df.reset_index(drop=True, inplace=True)
 
                     non_WL_cols = ["t_s", "U_V", "J_A", "cycle", "direction"]
-                    WL_map = {col: f"WL_{i}" for i, col in enumerate([x for x in df.columns if x not in non_WL_cols])}
-                    
+                    WL_map = {
+                        col: f"WL_{i}"
+                        for i, col in enumerate(
+                            [x for x in df.columns if x not in non_WL_cols]
+                        )
+                    }
+
                     df.rename(columns=WL_map, inplace=True)
-                    df.attrs["wl_dict"] = {v:k for k,v in WL_map.items()}  # Save inverse wl_dict for easy remapping
+                    df.attrs["wl_dict"] = {
+                        v: k for k, v in WL_map.items()
+                    }  # Save inverse wl_dict for easy remapping
                     # write the dataframe to a parquet file
-                    df.to_parquet(new_file_path, index=False, partition_cols=["cycle", "direction"])
-                    
-
-
-
-
-
-
+                    df.to_parquet(
+                        new_file_path,
+                        index=False,
+                        partition_cols=["cycle", "direction"],
+                    )
 
                     # table = pa.Table.from_pandas(df)
                     # schema = table.schema
@@ -80,14 +88,18 @@ class PostProcess(HloPostProcessor):
 
                     # table = table.replace_schema_metadata(metadata)
                     # schema = table.schema
-                    # pq.write_to_dataset(table, new_file_path, partition_cols=["cycle", "direction"], schema=schema)        
+                    # pq.write_to_dataset(table, new_file_path, partition_cols=["cycle", "direction"], schema=schema)
                     # glob through new file path to get all the parquet files to generate a list of paths
 
-                    file_list=Path(new_file_path).glob('**/*.parquet')
+                    file_list = Path(new_file_path).glob("**/*.parquet")
                     for file in file_list:
                         new_file = copy(act_file)
                         new_file.file_type = "andor_spec_parquet__file"
-                        relpath = Path(file).resolve().relative_to(Path(new_file_path).parent.resolve())
+                        relpath = (
+                            Path(file)
+                            .resolve()
+                            .relative_to(Path(new_file_path).parent.resolve())
+                        )
                         posixpath = str(relpath).replace("\\", "/")
                         new_file.file_name = posixpath
                         new_file.nosync = False
