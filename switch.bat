62580410d698ee50952262aea7c96fb91f6ec6c9
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-05-27 13:39:39 -0700

    fix: remove helao-core external deps

--- switch.bat
+++ switch.bat
@@ -1,11 +1,6 @@
 @echo off
 call conda activate helao
 
-echo switching helao-core branch
-cd ..\helao-core
-git fetch --all && git switch main && git branch -D unstable && git switch unstable && git switch %1
-echo[ 
-
 echo switching helao-async branch
 cd ..\helao-async
 git fetch --all && git switch main && git branch -D unstable && git switch unstable && git switch %1
--- update.bat
+++ update.bat
@@ -1,11 +1,6 @@
 @echo off
 call conda activate helao
 
-echo resetting helao-core and pulling updates
-cd ..\helao-core
-git fetch --prune --all && git reset --hard && git pull
-echo[ 
-
 echo resetting helao-async and pulling updates
 cd ..\helao-async
 git fetch --prune --all && git reset --hard && git pull
