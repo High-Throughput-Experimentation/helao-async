ba09efdae23ffe95e46eec7baf55449f8c3671b7
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-06-23 11:10:42 -0700

    fix: drop helao-core repo from update_env.bat

--- update_env.bat
+++ update_env.bat
@@ -1,11 +1,6 @@
 @echo off
 for %%A IN ("%~dp0.") do set REPO_PARENT_FOLDER=%%~dpA
 set HA_DIR=%REPO_PARENT_FOLDER%helao-async
-set HC_DIR=%REPO_PARENT_FOLDER%helao-core
-if not exist %HC_DIR% (
-    echo %HC_DIR% does not exist, cloning...
-    git clone https://github.com/High-Throughput-Experimentation/helao-core %HC_DIR%
-)
 conda env list | findstr /r /c:"^helao.*envs\\helao$"
 if ERRORLEVEL 1 (
     echo 'helao' conda environment was not found, creating it now...
@@ -15,7 +10,7 @@ conda env update -f helao_dev_win-64.yml
 echo.
 call conda activate helao
 echo setting PYTHONPATH in 'helao' conda environment vars
-call conda env config vars set PYTHONPATH=%HA_DIR%;%HC_DIR%
+call conda env config vars set PYTHONPATH=%HA_DIR%
 call conda env config vars set HELAOLAUNCH=%HA_DIR%\helao.bat
 call conda deactivate
 echo.
