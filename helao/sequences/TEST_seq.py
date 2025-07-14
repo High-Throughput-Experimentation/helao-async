dbe18ed3b4115a0b3024c8f0293b9978c83f37a9
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-07-14 11:16:30 -0700

    feat: add plate_sample_no_list param for testing

--- helao/sequences/TEST_seq.py
+++ helao/sequences/TEST_seq.py
@@ -16,21 +16,26 @@ def TEST_consecutive_noblocking(
     wait_time: float = 3.0,
     cycles: int = 5,
     dummy_list: List[List[float]] = [[0.0, 1.0], [2.0, 3.0]],
+    plate_sample_no_list: List[int] = [1, 2, 3],
     *args,
     **kwrags,
 ):
     epm = ExperimentPlanMaker()
 
-    for i in range(cycles):
-        if i == 0:
-            epm.add("TEST_sub_noblocking", {"wait_time": wait_time})
-        else:
-            epm.add(
-                "TEST_sub_noblocking",
-                {"wait_time": wait_time},
-                from_global_exp_params={"test_wait": "dummy_param"},
-            )
-        for i, l in enumerate(dummy_list):
-            print(f"dummy_list index {i}:  {l} has types {[type(x) for x in l]}")
+    for smp in plate_sample_no_list:
+        for i in range(cycles):
+            if i == 0:
+                epm.add(
+                    "TEST_sub_noblocking",
+                    {"wait_time": wait_time, "sample_no": smp},
+                )
+            else:
+                epm.add(
+                    "TEST_sub_noblocking",
+                    {"wait_time": wait_time, "sample_no": smp},
+                    from_global_exp_params={"test_wait": "dummy_param"},
+                )
+            for i, l in enumerate(dummy_list):
+                print(f"dummy_list index {i}:  {l} has types {[type(x) for x in l]}")
 
     return epm.planned_experiments  # returns complete experiment list
