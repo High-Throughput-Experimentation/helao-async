b4cf6e09c2d4d7f5e30ad80e36642845015c31f7
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-08-29 16:41:15 -0700

    fix: preserve file_type when converting to csv

--- helao/deploy/hte/processors/hlo_to_csv.py
+++ helao/deploy/hte/processors/hlo_to_csv.py
@@ -1,4 +1,5 @@
 from helao.helpers import helao_logging as logging
+
 if logging.LOGGER is None:
     LOGGER = logging.make_logger(__file__)
 else:
@@ -13,29 +14,37 @@ from helao.helpers.hlo_postprocessor import HloPostProcessor
 from helao.helpers.read_hlo import read_hlo
 import pandas as pd
 
+
 class PostProcess(HloPostProcessor):
 
     def process(self) -> List[FileInfo]:
         processed_file_list = []
         for act_file in self.files:
             try:
-                if act_file.file_type.endswith("helao__file") and act_file.file_name.endswith(".hlo"):
+                if act_file.file_type.endswith(
+                    "helao__file"
+                ) and act_file.file_name.endswith(".hlo"):
                     file_path = os.path.join(self.output_dir, act_file.file_name)
                     _, data = read_hlo(file_path)
                     df = pd.DataFrame(data)
                     action_comment = self.action.action_params.get("comment", "")
                     new_file_path = file_path.replace(".hlo", ".csv")
                     if action_comment:
-                        new_file_path = new_file_path.replace(".csv", f"_{action_comment}.csv")
+                        new_file_path = new_file_path.replace(
+                            ".csv", f"_{action_comment}.csv"
+                        )
                     df.to_csv(new_file_path, index=False)
                     new_file = copy(act_file)
-                    new_file.file_type = "csv__file"
+                    new_file.file_type = act_file.file_type.replace(
+                        "helao__file", "csv__file"
+                    )
                     new_file.file_name = os.path.basename(new_file_path)
                     processed_file_list.append(new_file)
-                    
-            except Exception: 
-                LOGGER.error(f"Error processing file: {act_file.file_name}", exc_info=True)
+
+            except Exception:
+                LOGGER.error(
+                    f"Error processing file: {act_file.file_name}", exc_info=True
+                )
             processed_file_list.append(act_file)
-        
+
         return processed_file_list
-        
