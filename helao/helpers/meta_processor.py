aaab3b268eba30d896eefda2170574302b082d31
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-09-02 17:12:44 -0700

    fix: access class name

--- helao/helpers/meta_processor.py
+++ helao/helpers/meta_processor.py
@@ -6,10 +6,10 @@ class MetaProcessor(ABC):
     def __init__(self, meta, core):
         self.core = core
         self.meta = meta
-        self.meta_type = meta.__class__.split(".")[-1].lower()
+        self.meta_type = meta.__class__.__name__.lower()
         self.global_params = (
             core.global_params
-            if core.__class__.split(".")[-1].lower() == "orch"
+            if core.__class__.__name__.lower() == "orch"
             else {}
         )
 
