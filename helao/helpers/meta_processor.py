193148bcd28eca540deb32f8a8e48849fd1cc589
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-09-02 17:10:33 -0700

    fix: parse class name

--- helao/helpers/meta_processor.py
+++ helao/helpers/meta_processor.py
@@ -6,8 +6,12 @@ class MetaProcessor(ABC):
     def __init__(self, meta, core):
         self.core = core
         self.meta = meta
-        self.meta_type = meta.__name__.lower()
-        self.global_params = core.global_params if core.__name__ == "Orch" else {}
+        self.meta_type = meta.__class__.split(".")[-1].lower()
+        self.global_params = (
+            core.global_params
+            if core.__class__.split(".")[-1].lower() == "orch"
+            else {}
+        )
 
     @abstractmethod
     def process(self) -> None:
