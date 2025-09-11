bb25e81806bba29f786cce4716d2cc6c3e25faf5
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-09-11 10:43:35 -0700

    wip: update deployments

--- update.bat
+++ update.bat
@@ -1,7 +1,20 @@
 @echo off
+HA_DIR=%~dp0
 call conda activate helao
 
 echo resetting helao-async and pulling updates
-cd ..\helao-async
+cd %HA_DIR%
 git fetch --prune --all && git reset --hard && git pull
+cd %HA_DIR%\helao\deploy
+for /d %%d in (*) do (
+    rem Check if the directory name is not "test" and not "hte"
+    if /i not "%%d"=="test" (
+        if /i not "%%d"=="hte" (
+            echo --- updating "%%d" ---
+            cd "%%d"
+            git fetch --prune --all && git reset --hard && git pull
+            cd ..
+        )
+    )
+)
 echo[ 
