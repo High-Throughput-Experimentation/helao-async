9a6ac958e00be7b8d56ebd58ef04e2f26831d473
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-09-11 10:57:46 -0700

    feat: update.sh bash script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+HA_DIR=$(readlink -f $0 | xargs -0 dirname)
+conda activate helao
+
+echo "resetting helao-async and pulling updates"
+cd "$HA_DIR" || exit
+git fetch --prune --all && git reset --hard && git pull
+cd "helao/deploy" || exit
+
+for dir in */; do
+    # Check if the directory is not "test" and not "hte"
+    if [[ "$dir" != "test/" && "$dir" != "hte/" && "$dir" != "__pycache__/" ]]; then
+        echo "--- updating ${dir%/} ---"
+        cd "$dir" || exit
+        git pull
+        cd ..
+        echo ""
+    fi
+done
+cd "$HA_DIR" || exit
