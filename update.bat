c245fdbecccd1a0bd0fd33d7641f99f6f10c3f0f
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-09-11 10:45:55 -0700

    fix: batch variable syntax

--- update.bat
+++ update.bat
@@ -3,9 +3,9 @@ HA_DIR=%~dp0
 call conda activate helao
 
 echo resetting helao-async and pulling updates
-cd %HA_DIR%
+cd %HA_DIR
 git fetch --prune --all && git reset --hard && git pull
-cd %HA_DIR%\helao\deploy
+cd helao\deploy
 for /d %%d in (*) do (
     rem Check if the directory name is not "test" and not "hte"
     if /i not "%%d"=="test" (
