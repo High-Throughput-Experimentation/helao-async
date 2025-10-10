ba6a0e11458fbd631be309ef042e3092decca789
Dan Guevarra <onepunchdan@users.noreply.github.com>
2025-10-10 13:23:01 -0700

    fix: correct region key

--- helao/core/drivers/data/sync_driver.py
+++ helao/core/drivers/data/sync_driver.py
@@ -1017,7 +1017,7 @@ class HelaoSyncer:
             self.aws_session = boto3.Session(
                 aws_access_key_id= self.config_dict["aws_access_key_id"],
                 aws_secret_access_key=self.config_dict["aws_secret_access_key"],
-                region_name=self.config_dict["region_name"],
+                region_name=self.config_dict["region"],
                 profile_name=self.config_dict["aws_profile"]
             )
             self.s3 = self.aws_session.client("s3")
