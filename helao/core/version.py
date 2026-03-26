7f9ecb66e2917a8fc246e621567c1fb2fe0c3945
Dan Guevarra <onepunchdan@users.noreply.github.com>
2026-03-26 11:53:14 -0700

    wip: return empty strings when unable to get git commit hash

--- helao/core/version.py
+++ helao/core/version.py
@@ -1,3 +1,4 @@
+from fsspec.implementations.http import ex
 import os
 import inspect
 import subprocess
@@ -9,36 +10,41 @@ __all__ = ["hlo_version", "get_hlo_version"]
 
 def get_branch_commithash():
     """Return current git branch and commit hash."""
-    command = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
-    branch = (
-        subprocess.check_output(command, stderr=subprocess.STDOUT)
-        .decode("utf8")
-        .strip()
-    )
-    command = ["git", "rev-parse", "--short", "HEAD"]
-    commit_hash = (
-        subprocess.check_output(command, stderr=subprocess.STDOUT)
-        .decode("utf8")
-        .strip()
-    )
-    return branch, commit_hash
+    try:
+        command = ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+        branch = (
+            subprocess.check_output(command, stderr=subprocess.STDOUT)
+            .decode("utf8")
+            .strip()
+        )
+        command = ["git", "rev-parse", "--short", "HEAD"]
+        commit_hash = (
+            subprocess.check_output(command, stderr=subprocess.STDOUT)
+            .decode("utf8")
+            .strip()
+        )
+        return branch, commit_hash
+    except Exception:
+        return "", ""
 
 
 def get_filehash(filename: str):
-    filename = os.path.abspath(filename)
-    parent_dir = os.path.dirname(filename)
-    command = ["git", "log", "-n", "1", "--pretty=format:%h", "--", filename]
-    response = (
-        subprocess.check_output(command, cwd=parent_dir, stderr=subprocess.STDOUT)
-        .decode("utf8")
-        .split()
-    )
-    if response:
-        short_hash = response[0]
-    else:
-        short_hash = ""
-    return short_hash
-
+    try:
+        filename = os.path.abspath(filename)
+        parent_dir = os.path.dirname(filename)
+        command = ["git", "log", "-n", "1", "--pretty=format:%h", "--", filename]
+        response = (
+            subprocess.check_output(command, cwd=parent_dir, stderr=subprocess.STDOUT)
+            .decode("utf8")
+            .split()
+        )
+        if response:
+            short_hash = response[0]
+        else:
+            short_hash = ""
+        return short_hash
+    except Exception:
+        return ""
 
 def get_hlo_version():
     """Return hard-coded HELAO release version."""
@@ -50,17 +56,23 @@ def get_hlo_version():
 
 def get_caller_filehash():
     """Return short git hash and filename of calling frame."""
-    caller_frame = inspect.stack()[1]
-    caller_filename_full = caller_frame.filename
-    short_hash = get_filehash(caller_filename_full)
-    return short_hash, caller_filename_full
+    try:
+        caller_frame = inspect.stack()[1]
+        caller_filename_full = caller_frame.filename
+        short_hash = get_filehash(caller_filename_full)
+        return short_hash, caller_filename_full
+    except Exception:
+        return "", ""
 
 
 def get_object_filehash(obj):
     """Return short git hash and source file of object."""
-    filename = inspect.getabsfile(obj)
-    short_hash = get_filehash(filename)
-    return short_hash, filename
+    try:
+        filename = inspect.getabsfile(obj)
+        short_hash = get_filehash(filename)
+        return short_hash, filename
+    except Exception:
+        return "", ""
 
 
 # version number, gets written into every exp/prg and hlo file
